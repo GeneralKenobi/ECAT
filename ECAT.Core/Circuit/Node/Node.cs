ef388c20f8f1e0cb50c4c825a7b3cefa3a557e59
Szymon Żogała <szymek.655@gmail.com>
2018-06-28 23:14:43 +0200

    Added, included some basic properties

---
+++ ECAT.Core/Circuit/Node/Node.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ECAT.Core
+{
+	/// <summary>
+	/// Node - connects one or more components and/or wires
+	/// </summary>
+	public class Node
+    {
+		#region Constructor
+
+		/// <summary>
+		/// Default Constructor
+		/// </summary>
+		public Node()
+		{
+			ConnectedComponents = new ReadOnlyCollection<BaseComponent>(_ConnectedComponents);
+		}
+
+		#endregion
+
+		#region Private properties
+
+		/// <summary>
+		/// Backing store for <see cref="ConnectedComponents"/>
+		/// </summary>
+		private List<BaseComponent> _ConnectedComponents { get; } = new List<BaseComponent>();
+
+		#endregion
+
+		#region Public properties
+
+		/// <summary>
+		/// Unique number representing the instance
+		/// </summary>
+		public int ID { get; }
+
+		/// <summary>
+		/// Collection of all parts connected to this node
+		/// </summary>
+		public ReadOnlyCollection<BaseComponent> ConnectedComponents { get; }
+
+		#endregion
+
+
+
+	}
+}
---
+++ ECAT.Core/Components/BaseComponent/BaseComponent.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace ECAT.Core
+{
+	/// <summary>
+	/// Base class for all components
+	/// </summary>
+	public class BaseComponent : INotifyPropertyChanged
+	{
+		#region Events
+
+		/// <summary>
+		/// Event fired whenever a property changes
+		/// </summary>
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		#endregion
+	}
+}
