a43f6f5f6a200caa1ebd0e190f659aef596d65ec
Szymon Żogała <szymek.655@gmail.com>
2018-06-29 13:17:42 +0200

    Construction can be only performed by a factory

--- ECAT.Core/Circuit/Node/Node.cs
+++ ECAT.Core/Circuit/Node/Node.cs
@@ -6,22 +6,24 @@ using System.Text;
 namespace ECAT.Core
 {
 	/// <summary>
-	/// Node - connects one or more components and/or wires
+	/// Node - connects one or more components and/or wires. Used in admittance matrix and simulation
 	/// </summary>
-	public class Node
+	public partial class Node
     {
 		#region Constructor
 
 		/// <summary>
-		/// Default Constructor
+		/// Default Constructor, hidden so as to force creation through <see cref=""/>
 		/// </summary>
-		public Node()
+		private Node(int id)
 		{
+			ID = id;
+
 			ConnectedComponents = new ReadOnlyCollection<BaseComponent>(_ConnectedComponents);
 		}
 
 		#endregion
-
+		
 		#region Private properties
 
 		/// <summary>
