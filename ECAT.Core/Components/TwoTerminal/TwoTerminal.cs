ba3d7ca59bbfcdeacb035965ba5477a99896484f
Szymon Żogała <szymek.655@gmail.com>
2018-06-29 13:17:08 +0200

    Added

---
+++ ECAT.Core/Components/TwoTerminal/TwoTerminal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECAT.Core
+{
+	/// <summary>
+	/// Base class for all two-terminal components
+	/// </summary>
+    public class TwoTerminal : BaseComponent
+    {
+		#region Public properties
+
+		/// <summary>
+		/// One of the terminals in this two-terminal
+		/// </summary>
+		public Node TerminalA { get; set; } = Node.Factory.Construct();
+
+		/// <summary>
+		/// One of the terminals in this two-terminal
+		/// </summary>
+		public Node TerminalB { get; set; } = Node.Factory.Construct();
+
+		#endregion
+	}
+}
