50716545b3d4d6a1940814f97048eb2caa9b50dd
Szymon Żogała <szymek.655@gmail.com>
2018-06-30 21:45:47 +0200

    Moved DesignManager implementation to Schematic, DesignManager will now serve as a collection of Schematics

--- ECAT.Design/DesignManager/DesignManager.cs
+++ ECAT.Design/DesignManager/DesignManager.cs
@@ -9,89 +9,12 @@ namespace ECAT.Design
 {
 	public class DesignManager : IDesignManager
 	{
-		#region Constructor
-
-		/// <summary>
-		/// Default Constructor
-		/// </summary>
-		public DesignManager()
-		{
-			Components = new ReadOnlyObservableCollection<IBaseComponent>(_Components);
-			Wires = new ReadOnlyObservableCollection<IWire>(_Wires);
-		}
-
-		#endregion
-
-		#region Private Properties
-
-		/// <summary>
-		/// Backing store for <see cref="Components"/>
-		/// </summary>
-		private ObservableCollection<IBaseComponent> _Components { get; } = new ObservableCollection<IBaseComponent>();
-
-		/// <summary>
-		/// Backing store for <see cref="Wires"/>
-		/// </summary>
-		private ObservableCollection<IWire> _Wires { get; } = new ObservableCollection<IWire>();
-
-		#endregion
-
-		#region Public Properties
-
-		/// <summary>
-		/// Collection of components added to the circuit
-		/// </summary>
-		public ReadOnlyObservableCollection<IBaseComponent> Components { get; }
-
-		/// <summary>
-		/// Collection of wires added to the circuit
-		/// </summary>
-		public ReadOnlyObservableCollection<IWire> Wires { get; }
-
-		#endregion
-
 		#region Events
 
 		/// <summary>
 		/// Event fired whenever a property changes its value
 		/// </summary>
 		public event PropertyChangedEventHandler PropertyChanged;
-
-		#endregion
-
-		#region Public Methods
-
-		/// <summary>
-		/// Method that adds a new component to the circuit
-		/// </summary>
-		/// <param name="component"></param>
-		public void AddNewComponent(IBaseComponent component) => _Components.Add(component);
-
-		/// <summary>
-		/// Method that adds a new wire to the circuit
-		/// </summary>
-		/// <param name="wire"></param>
-		public void AddNewWire(IWire wire) => _Wires.Add(wire);
-
-		/// <summary>
-		/// Method that removes a component from the circuit
-		/// </summary>
-		/// <param name="component"></param>
-		public void RemoveComponent(IBaseComponent component)
-		{
-			component.Dispose();
-			_Components.Remove(component);
-		}
-
-		/// <summary>
-		/// Method that removes a wire from the circuit
-		/// </summary>
-		/// <param name="wire"></param>
-		public void RemoveWire(IWire wire)
-		{
-			wire.Dispose();
-			_Wires.Remove(wire);
-		}
 
 		#endregion
 	}
