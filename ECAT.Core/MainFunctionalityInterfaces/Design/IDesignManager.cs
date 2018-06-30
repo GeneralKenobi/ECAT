4fa3c696983e4fe5e7512f645668f98c49615035
Szymon Żogała <szymek.655@gmail.com>
2018-06-30 21:43:50 +0200

    Moved the functionality of IDesignManager to ISchematic. IDesignManager is now a collection of ISchematics

--- ECAT.Core/MainFunctionalityInterfaces/Design/IDesignManager.cs
+++ ECAT.Core/MainFunctionalityInterfaces/Design/IDesignManager.cs
@@ -8,46 +8,6 @@ namespace ECAT.Core
 	/// </summary>
 	public interface IDesignManager : INotifyPropertyChanged
     {
-		#region Properties
-
-		/// <summary>
-		/// Collection of components added to the circuit
-		/// </summary>
-		ReadOnlyObservableCollection<IBaseComponent> Components { get; }
-
-		/// <summary>
-		/// Collection of wires added to the circuit
-		/// </summary>
-		ReadOnlyObservableCollection<IWire> Wires { get; }
-
-		#endregion
-
-		#region Methods
-
-		/// <summary>
-		/// Method that adds a new component to the circuit
-		/// </summary>
-		/// <param name="component"></param>
-		void AddNewComponent(IBaseComponent component);
-
-		/// <summary>
-		/// Method that adds a new wire to the circuit
-		/// </summary>
-		/// <param name="wire"></param>
-		void AddNewWire(IWire wire);
-
-		/// <summary>
-		/// Method that removes a component from the circuit
-		/// </summary>
-		/// <param name="component"></param>
-		void RemoveComponent(IBaseComponent component);
-
-		/// <summary>
-		/// Method that removes a wire from the circuit
-		/// </summary>
-		/// <param name="wire"></param>
-		void RemoveWire(IWire wire);
-
-		#endregion
+		
 	}
 }
--- ECAT.Core/MainFunctionalityInterfaces/Design/ISchematic.cs
+++ ECAT.Core/MainFunctionalityInterfaces/Design/ISchematic.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Text;
 
@@ -10,6 +11,46 @@ namespace ECAT.Core
 	/// </summary>
     public interface ISchematic : INotifyPropertyChanged
     {
+		#region Properties
 
-    }
+		/// <summary>
+		/// Collection of components added to the circuit
+		/// </summary>
+		ReadOnlyObservableCollection<IBaseComponent> Components { get; }
+
+		/// <summary>
+		/// Collection of wires added to the circuit
+		/// </summary>
+		ReadOnlyObservableCollection<IWire> Wires { get; }
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Method that adds a new component to the circuit
+		/// </summary>
+		/// <param name="component"></param>
+		void AddNewComponent(IBaseComponent component);
+
+		/// <summary>
+		/// Method that adds a new wire to the circuit
+		/// </summary>
+		/// <param name="wire"></param>
+		void AddNewWire(IWire wire);
+
+		/// <summary>
+		/// Method that removes a component from the circuit
+		/// </summary>
+		/// <param name="component"></param>
+		void RemoveComponent(IBaseComponent component);
+
+		/// <summary>
+		/// Method that removes a wire from the circuit
+		/// </summary>
+		/// <param name="wire"></param>
+		void RemoveWire(IWire wire);
+
+		#endregion
+	}
 }
