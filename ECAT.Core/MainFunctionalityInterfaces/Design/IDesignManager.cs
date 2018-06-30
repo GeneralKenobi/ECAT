1fa92dc2922dc28c44189a1d41ac3bf55ec5e2ca
Szymon Żogała <szymek.655@gmail.com>
2018-06-30 22:04:15 +0200

    Initial implementation

--- ECAT.Core/MainFunctionalityInterfaces/Design/IDesignManager.cs
+++ ECAT.Core/MainFunctionalityInterfaces/Design/IDesignManager.cs
@@ -8,6 +8,57 @@ namespace ECAT.Core
 	/// </summary>
 	public interface IDesignManager : INotifyPropertyChanged
     {
-		
+		#region Properties
+
+		/// <summary>
+		/// Collection of all active schematics
+		/// </summary>
+		ReadOnlyObservableCollection<ISchematic> Schematics { get; }
+
+		/// <summary>
+		/// The currently edited schematic
+		/// </summary>
+		ISchematic CurrentSchematic { get; }
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Creates and adds a new, empty schematic
+		/// </summary>
+		void AddSchematic();
+
+		/// <summary>
+		/// Adds the given schematic to <see cref="Schematics"/>
+		/// </summary>
+		/// <param name="schematic"></param>
+		void AddSchematic(ISchematic schematic);
+
+		/// <summary>
+		/// Removes the given schematic from <see cref="Schematics"/>
+		/// </summary>
+		/// <param name="schematic"></param>
+		void RemoveSchematic(ISchematic schematic);
+
+		/// <summary>
+		/// Removes schematic given by the argument
+		/// </summary>
+		/// <param name="index"></param>
+		void RemoveSchematic(int index);
+
+		/// <summary>
+		/// Changes the <see cref="CurrentSchematic"/> to the one given
+		/// </summary>
+		/// <param name="schematic"></param>
+		void ChangeCurrentSchematic(ISchematic schematic);
+
+		/// <summary>
+		/// Changes the <see cref="CurrentSchematic"/> to the one at <paramref name="index"/> in the <see cref="Schematics"/> collection
+		/// </summary>
+		/// <param name="index"></param>
+		void ChangeCurrentSchematic(int index);
+
+		#endregion
 	}
 }
--- ECAT.Design/DesignManager/DesignManager.cs
+++ ECAT.Design/DesignManager/DesignManager.cs
@@ -1,20 +1,135 @@
 ﻿using ECAT.Core;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Text;
 
 namespace ECAT.Design
 {
+	/// <summary>
+	/// Default implementation of <see cref="IDesignManager"/>
+	/// </summary>
 	public class DesignManager : IDesignManager
 	{
+		#region Constructor
+
+		/// <summary>
+		/// Default Constructor
+		/// </summary>
+		public DesignManager()
+		{
+			Schematics = new ReadOnlyObservableCollection<ISchematic>(_Schematics);
+		}
+
+		#endregion
+
+		#region Private Properties
+
+		/// <summary>
+		/// Backing store for <see cref="Schematic"/>
+		/// </summary>
+		private ObservableCollection<ISchematic> _Schematics { get; } = new ObservableCollection<ISchematic>();
+
+		#endregion
+
+		#region Public Properties
+
+		/// <summary>
+		/// Collection of all active schematics
+		/// </summary>
+		public ReadOnlyObservableCollection<ISchematic> Schematics { get; }
+
+		/// <summary>
+		/// The currently edited schematic
+		/// </summary>
+		public ISchematic CurrentSchematic { get; private set; }
+
+		#endregion
+
 		#region Events
 
 		/// <summary>
 		/// Event fired whenever a property changes its value
 		/// </summary>
 		public event PropertyChangedEventHandler PropertyChanged;
+
+		#endregion
+
+		#region Public methods
+
+		/// <summary>
+		/// Creates and adds a new, empty schematic
+		/// </summary>
+		public void AddSchematic() => _Schematics.Add(new Schematic());
+
+		/// <summary>
+		/// Adds the given schematic to <see cref="Schematics"/>
+		/// </summary>
+		/// <param name="schematic"></param>
+		public void AddSchematic(ISchematic schematic) => _Schematics.Add(schematic);
+
+		/// <summary>
+		/// Changes the <see cref="CurrentSchematic"/> to the one given
+		/// </summary>
+		/// <param name="schematic"></param>
+		/// <exception cref="ArgumentException">Thrown if <paramref name="schematic"/> is not a part of <see cref="Schematics"/>
+		/// </exception>
+		public void ChangeCurrentSchematic(ISchematic schematic)
+		{
+			if(_Schematics.Contains(schematic))
+			{
+				CurrentSchematic = schematic;
+			}
+			else
+			{
+				throw new ArgumentException(nameof(schematic) + " is not a part of this " + nameof(IDesignManager));
+			}
+		}
+
+		/// <summary>
+		/// Changes the <see cref="CurrentSchematic"/> to the one at <paramref name="index"/> in the <see cref="Schematics"/>
+		/// collection
+		/// </summary>
+		/// <param name="index"></param>
+		/// <exception cref="ArgumentOutOfRangeException"></exception>
+		public void ChangeCurrentSchematic(int index)
+		{
+			if(index < 0 || index >= _Schematics.Count)
+			{
+				throw new ArgumentOutOfRangeException(nameof(index));
+			}
+
+			CurrentSchematic = _Schematics[index];
+		}
+
+		/// <summary>
+		/// Removes the given schematic from <see cref="Schematics"/>
+		/// </summary>
+		/// <param name="schematic"></param>
+		public void RemoveSchematic(ISchematic schematic)
+		{
+			_Schematics.Remove(schematic);
+			
+			// If the removed schematic was the current one, set the current one to null
+			if(CurrentSchematic == schematic)
+			{
+				CurrentSchematic = null;
+			}
+		}
+
+		/// <summary>
+		/// Removes schematic given by the argument
+		/// </summary>
+		/// <param name="index"></param>
+		/// <exception cref="ArgumentOutOfRangeException"></exception>
+		public void RemoveSchematic(int index)
+		{
+			if (index < 0 || index >= _Schematics.Count)
+			{
+				throw new ArgumentOutOfRangeException(nameof(index));
+			}
+
+			RemoveSchematic(_Schematics[index]);
+		}
 
 		#endregion
 	}
