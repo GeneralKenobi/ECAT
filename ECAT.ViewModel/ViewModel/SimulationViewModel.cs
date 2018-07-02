8a9dbe5d9f29b37f1f06ffbb3777e73f9082f514
Szymon Żogała <szymek.655@gmail.com>
2018-07-02 17:03:53 +0200

    Added

---
+++ ECAT.ViewModel/ViewModel/SimulationViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Autofac;
+using ECAT.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECAT.ViewModel
+{
+	/// <summary>
+	/// ViewModel for simulation module
+	/// </summary>
+    public class SimulationViewModel : BaseViewModel
+    {
+		#region Constructor
+
+		/// <summary>
+		/// Default Constructor
+		/// </summary>
+		public SimulationViewModel()
+		{
+			if (IoC.Container.TryResolve(out ISimulationManager manager))
+			{
+				SimulationManager = manager;
+			}
+			else
+			{
+				throw new Exception();
+			}
+		}
+
+		#endregion
+
+		#region Public properties
+
+		/// <summary>
+		/// Provided implementation of the <see cref="IDesignManager"/> interface
+		/// </summary>
+		public ISimulationManager SimulationManager { get; private set; }
+
+		#endregion
+
+		#region Commands
+
+
+
+		#endregion
+	}
+}
