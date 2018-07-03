bac2343a453717cfdf8ad8f93f28c4d378fea5d2
Szymon Żogała <szymek.655@gmail.com>
2018-07-03 20:19:33 +0200

    Added

---
+++ ECAT.UWP/MainPage/IBaseComponentToComponentViewModelConverter.cs
@@ -0,0 +1,28 @@
+﻿using ECAT.Core;
+using ECAT.ViewModel;
+using System;
+using Windows.UI.Xaml.Data;
+
+namespace ECAT.UWP
+{
+	/// <summary>
+	/// Converts an <see cref="IBaseComponent"/> to a <see cref="ComponentViewModel"/>
+	/// </summary>
+	public class IBaseComponentToComponentViewModelConverter : IValueConverter
+    {
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			if(value is IBaseComponent component)
+			{
+				return new ComponentViewModel(component);
+			}
+
+			return null;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
