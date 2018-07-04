c0bd4147c275df85f51d3f1e517f2a6d7ded90ab
Szymon Żogała <szymek.655@gmail.com>
2018-07-04 23:10:24 +0200

    Added

---
+++ ECAT.UWP/Design/Components/Wire/PlanePositionCollectionToPointCollectionConverter.cs
@@ -0,0 +1,39 @@
+﻿using ECAT.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace ECAT.UWP
+{
+	public class PlanePositionCollectionToPointCollectionConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			if(value is ICollection<PlanePosition> collection)
+			{
+				var pointCollection = new PointCollection();
+
+				var transformedCollection = collection.Select((pos) => new Point((int)Math.Round(pos.X), (int)Math.Round(pos.Y)));
+
+				foreach(var item in transformedCollection)
+				{
+					pointCollection.Add(item);
+				}
+
+				return pointCollection;
+			}
+
+			return null;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ ECAT.UWP/Design/Components/Wire/WireTC.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+namespace ECAT.UWP
+{
+	public sealed class WireTC : Control
+	{
+		public WireTC()
+		{
+			this.DefaultStyleKey = typeof(WireTC);
+		}
+	}
+}
