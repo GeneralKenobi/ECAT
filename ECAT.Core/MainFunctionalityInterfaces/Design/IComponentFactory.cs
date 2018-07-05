59e7f2536c9f1f53bc4540fefe280a4bb4a30573
Szymon Żogała <szymek.655@gmail.com>
2018-07-05 19:31:32 +0200

    Added

---
+++ ECAT.Core/MainFunctionalityInterfaces/Design/IComponentFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECAT.Core
+{
+	/// <summary>
+	/// A Factory producing <see cref="IBaseComponent"/> 
+	/// </summary>
+    public interface IComponentFactory
+    {
+		#region Methods
+
+		/// <summary>
+		/// Constructs a new <see cref="IBaseComponent"/> based on the given <see cref="IComponentDeclaration"/>
+		/// </summary>
+		/// <param name="declaration"></param>
+		/// <returns></returns>
+		IBaseComponent Construct(IComponentDeclaration declaration);
+
+		/// <summary>
+		/// Constructs a new <see cref="IBaseComponent"/> based on the given component id
+		/// </summary>
+		/// <param name="componentID"></param>
+		/// <returns></returns>
+		IBaseComponent Construct(int componentID);
+
+		/// <summary>
+		/// Constructs a new <see cref="IWire"/>
+		/// </summary>
+		/// <returns></returns>
+		IWire ConstructWire();
+
+		#endregion
+	}
+}
---
+++ ECAT.Design/ComponentFactory/ComponentFactory.cs
@@ -0,0 +1,39 @@
+﻿using ECAT.Core;
+using System;
+
+namespace ECAT.Design
+{
+	public class ComponentFactory : IComponentFactory
+	{
+		/// <summary>
+		/// Constructs a new <see cref="IBaseComponent"/> based on the given <see cref="IComponentDeclaration"/>
+		/// </summary>
+		/// <param name="declaration"></param>
+		/// <returns></returns>
+		public IBaseComponent Construct(IComponentDeclaration declaration) => Construct(declaration.ID);
+
+		/// <summary>
+		/// Constructs a new <see cref="IBaseComponent"/> based on the given component id
+		/// </summary>
+		/// <param name="componentID"></param>
+		/// <returns></returns>
+		public IBaseComponent Construct(int componentID)
+		{
+			switch (componentID)
+			{
+				case 0: return new Resistor();
+
+				default:
+					{
+						throw new ArgumentException($"No component matches the ID: {componentID}");
+					}
+			}
+		}
+
+		/// <summary>
+		/// Constructs a new <see cref="IWire"/>
+		/// </summary>
+		/// <returns></returns>
+		public IWire ConstructWire() => new Wire();
+	}
+}
