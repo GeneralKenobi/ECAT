6b7f264ea1e5bbba6ccf5b42ecc6ed39ce3b61c2
Szymon Żogała <szymek.655@gmail.com>
2018-07-05 19:38:00 +0200

    Added

---
+++ ECAT.Core/GlobalEnums/AppState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECAT.Core
+{
+	/// <summary>
+	/// Defines states the app may be in
+	/// </summary>
+    public enum AppState
+    {
+		/// <summary>
+		/// App is in idle state - nothing is going on
+		/// </summary>
+		Idle = 0,
+
+		/// <summary>
+		/// User is currently adding new components
+		/// </summary>
+		AddingComponents = 1,
+
+		/// <summary>
+		/// User is currently placing a wire
+		/// </summary>
+		AddingWire = 2,
+    }
+}
