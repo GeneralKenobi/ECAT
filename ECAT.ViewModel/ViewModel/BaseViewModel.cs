c443f9b84ab9776665c6eab13b8d45ae32fbb2e9
Szymon Żogała <szymek.655@gmail.com>
2018-07-07 10:18:47 +0200

    Added protected InvokePropertyChanged method

--- ECAT.ViewModel/ViewModel/BaseViewModel.cs
+++ ECAT.ViewModel/ViewModel/BaseViewModel.cs
@@ -17,7 +17,23 @@ namespace ECAT.ViewModel
 		/// <summary>
 		/// The event that is fired when any child property changed its value
 		/// </summary>
-		public event PropertyChangedEventHandler PropertyChanged;		
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		#endregion
+
+		#region Protected methods
+
+		/// <summary>
+		/// Invokes Property Changed Event for each string parameter
+		/// </summary>
+		/// <param name="propertyName">Properties to invoke for. Null or string.Empty will result in notification for all properties</param>
+		protected void InvokePropertyChanged(params string[] propertyNames)
+		{
+			foreach (var item in propertyNames)
+			{
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(item));
+			}
+		}
 
 		#endregion
 	}
