8cca7ab109fbbe937a4e98cba97aadbc65279e8b
Szymon Żogała <szymek.655@gmail.com>
2018-07-07 10:22:27 +0200

    Added a callback for Wire DefiningPoints CollectionChanged to update the wire beginning and end

--- ECAT.ViewModel/ViewModel/WireViewModel.cs
+++ ECAT.ViewModel/ViewModel/WireViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using CSharpEnhanced.ICommands;
 using ECAT.Core;
 using System;
+using System.Collections.Specialized;
 using System.Windows.Input;
+using Autofac;
 
 namespace ECAT.ViewModel
 {
@@ -20,6 +22,8 @@ namespace ECAT.ViewModel
 			Wire = wire ?? throw new ArgumentNullException(nameof(wire));
 
 			RemoveWireCommand = new RelayCommand(RemoveWire);
+
+			(Wire.DefiningPoints as INotifyCollectionChanged).CollectionChanged += OnWireDefiningPointsChanged;			
 		}
 
 		#endregion
@@ -40,6 +44,18 @@ namespace ECAT.ViewModel
 		/// </summary>
 		public IWire Wire { get; private set; }
 
+		/// <summary>
+		/// Point that the wire begins on
+		/// </summary>
+		public IPlanePosition WireBeginning => Wire != null && Wire.DefiningPoints.Count > 0 ?
+			Wire.DefiningPoints[0] : IoC.Container.Resolve<IPlanePositionFactory>().Construct();
+
+		/// <summary>
+		/// Point the wire ends on
+		/// </summary>
+		public IPlanePosition WireEnding => Wire != null && Wire.DefiningPoints.Count > 0 ?
+			Wire.DefiningPoints[Wire.DefiningPoints.Count - 1] : IoC.Container.Resolve<IPlanePositionFactory>().Construct();
+
 		#endregion
 
 		#region Private methods
@@ -52,6 +68,16 @@ namespace ECAT.ViewModel
 			AppViewModel.Singleton.DesignVM.DesignManager.RemoveWire(Wire);
 			Wire = null;
 		}
+
+		/// <summary>
+		/// Method invoked when DefiningPoints of the Wire change; invkes PropertyChanged event for <see cref="WireBeginning"/> and
+		/// <see cref="WireEnding"/>
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void OnWireDefiningPointsChanged(object sender, NotifyCollectionChangedEventArgs e) =>
+			InvokePropertyChanged(nameof(WireEnding), nameof(WireBeginning));
+
 
 		#endregion
 	}
