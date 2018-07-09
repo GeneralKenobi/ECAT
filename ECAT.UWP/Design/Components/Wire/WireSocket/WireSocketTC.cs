c2f37d095940efd16f9638c2bb3fa7741437b4f1
Szymon Żogała <szymek.655@gmail.com>
2018-07-09 14:50:30 +0200

    Added PositionOnWire dependency property and added a command

--- ECAT.UWP/Design/Components/Wire/WireSocket/WireSocketTC.cs
+++ ECAT.UWP/Design/Components/Wire/WireSocket/WireSocketTC.cs
@@ -23,7 +23,7 @@ namespace ECAT.UWP
 		public WireSocketTC()
 		{
 			this.DefaultStyleKey = typeof(WireSocketTC);
-			this.Tapped += SocketTapped;
+			this.Tapped += SocketTapped;			
 		}
 
 		#endregion
@@ -56,6 +56,26 @@ namespace ECAT.UWP
 		public static readonly DependencyProperty PositionProperty =
 			DependencyProperty.Register(nameof(Position), typeof(IPlanePosition),
 			typeof(WireSocketTC), new PropertyMetadata(default(IPlanePosition)));
+
+		#endregion
+
+		#region PositionOnWire Dependency Property
+
+		/// <summary>
+		/// Indicates the position on the wire (true for ending, false for beginning)
+		/// </summary>
+		public bool PositionOnWire
+		{
+			get => (bool)GetValue(PositionOnWireProperty);
+			set => SetValue(PositionOnWireProperty, value);
+		}
+
+		/// <summary>
+		/// Backing store for <see cref="PositionOnWire"/>
+		/// </summary>
+		public static readonly DependencyProperty PositionOnWireProperty =
+			DependencyProperty.Register(nameof(PositionOnWire), typeof(bool),
+			typeof(WireSocketTC), new PropertyMetadata(default(bool)));
 
 		#endregion
 	}
