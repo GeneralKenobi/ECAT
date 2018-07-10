441794aa2c2e4d188b670c1dca0514343ec9fafc
Szymon Żogała <szymek.655@gmail.com>
2018-07-10 18:02:20 +0200

    Added

---
+++ ECAT.Core/MainFunctionalityInterfaces/Design/ITerminal.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace ECAT.Core
+{
+	/// <summary>
+	/// Interface for terminals - each terminal has a position and allows for 
+	/// </summary>
+	public interface ITerminal : INotifyPropertyChanged
+    {
+		/// <summary>
+		/// Position of the terminal on the design area used to connect the terminals
+		/// </summary>
+		IPlanePosition Position { get; }
+
+		/// <summary>
+		/// Node assigned to the element in the simulation, having it stored allows to display the results during real-time simulations
+		/// </summary>
+		INode Node { get; }
+    }
+}
---
+++ ECAT.Design/Components/Terminal/Terminal.cs
@@ -0,0 +1,42 @@
+﻿using ECAT.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ECAT.Design
+{
+    public class Terminal : ITerminal
+    {
+		#region Constructor
+
+		/// <summary>
+		/// Default Constructor
+		/// </summary>
+		public Terminal(IPlanePosition position)
+		{
+			Position = position;
+		}
+
+		#endregion
+
+		#region Events
+
+		/// <summary>
+		/// Event fired whenever a property changes its value
+		/// </summary>
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		#endregion
+		
+		/// <summary>
+		/// Position of the terminal on the design area used to connect the terminals
+		/// </summary>
+		public IPlanePosition Position { get; }
+
+		/// <summary>
+		/// Node assigned to the element in the simulation, having it stored allows to display the results during real-time simulations
+		/// </summary>
+		public INode Node { get; }		
+	}
+}
