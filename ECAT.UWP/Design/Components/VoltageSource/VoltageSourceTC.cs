d03f3877e43f345c91793b2aabc1302b410712ec
Szymon Żogała <szymek.655@gmail.com>
2018-07-15 21:43:59 +0200

    Added

---
+++ ECAT.UWP/Design/Components/VoltageSource/VoltageSourceTC.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml.Controls;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace ECAT.UWP
+{
+	public sealed class VoltageSourceTC : Control
+	{
+		public VoltageSourceTC()
+		{
+			this.DefaultStyleKey = typeof(VoltageSourceTC);
+		}
+	}
+}
