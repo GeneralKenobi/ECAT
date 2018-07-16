c20f6f1c2392126b7d77e20d9fc9e5e552011ee6
Szymon Żogała <szymek.655@gmail.com>
2018-07-16 15:52:12 +0200

    Added

---
+++ ECAT.UWP/Design/Components/CurrentSource/CurrentSourceTC.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml.Controls;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace ECAT.UWP
+{
+	public sealed class CurrentSourceTC : Control
+    {
+        public CurrentSourceTC()
+        {
+            this.DefaultStyleKey = typeof(CurrentSourceTC);
+        }
+    }
+}
