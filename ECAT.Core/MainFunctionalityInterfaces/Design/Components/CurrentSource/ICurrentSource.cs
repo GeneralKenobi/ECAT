72cc2bc2312bdfa02e35534edd1f414a3d32c537
Szymon Żogała <szymek.655@gmail.com>
2018-07-17 10:47:36 +0200

    Added

---
+++ ECAT.Core/MainFunctionalityInterfaces/Design/Components/CurrentSource/ICurrentSource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECAT.Core
+{
+	/// <summary>
+	/// Interface for current sources
+	/// </summary>
+    public interface ICurrentSource : ITwoTerminal
+    {
+    }
+}
