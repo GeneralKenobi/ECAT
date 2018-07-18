680c31839f93c07994d6af7a0f91892803e5a4f2
Szymon Żogała <szymek.655@gmail.com>
2018-07-18 12:31:06 +0200

    Added

---
+++ ECAT.Core/GlobalEnums/KeyModifiers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ECAT.Core
+{
+	/// <summary>
+	/// Flags that denote modifier keys that may have been present with a key press
+	/// </summary>
+	[Flags]
+	public enum KeyModifiers
+    {
+		/// <summary>
+		/// Shift key
+		/// </summary>
+		Shift = 1,
+
+		/// <summary>
+		/// Ctrl key
+		/// </summary>
+		Ctrl = 2,
+
+		/// <summary>
+		/// Alt key
+		/// </summary>
+		Alt = 4
+    }
+}
