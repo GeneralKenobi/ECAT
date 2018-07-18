562bef3230b8562d4f30c92b42061a4ba15d5407
Szymon Żogała <szymek.655@gmail.com>
2018-07-18 22:11:12 +0200

    Added

---
+++ ECAT.Core/GlobalEnums/ComponentIDEnumeration.cs
@@ -0,0 +1,14 @@
+﻿namespace ECAT.Core
+{
+	/// <summary>
+	/// Enumeration of components to be implemented in the app
+	/// </summary>
+	public enum ComponentIDEnumeration
+    {		
+		Resistor = 0,
+
+		VoltageSource = 1,
+
+		CurrentSource = 2,
+    }
+}
