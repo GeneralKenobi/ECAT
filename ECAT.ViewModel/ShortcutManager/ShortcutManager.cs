fbe5e7d820aa6f0934118b71364bd91f1df15321
Szymon Żogała <szymek.655@gmail.com>
2018-07-19 19:58:04 +0200

    Finished the implementation, tested, added documentation, added methods updating and resetting the bindings

--- ECAT.ViewModel/ShortcutManager/ShortcutManager.cs
+++ ECAT.ViewModel/ShortcutManager/ShortcutManager.cs
@@ -1,12 +1,14 @@
 ﻿using ECAT.Core;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
 
 namespace ECAT.ViewModel
 {
+	/// <summary>
+	/// Class acting as a manager for shortcuts - handles key presses, stores user settings locally, checks to make sure
+	/// all defined shortcuts are valid
+	/// </summary>
 	public class ShortcutManager
 	{
 		#region Constructor
@@ -44,59 +46,10 @@ namespace ECAT.ViewModel
 		#region Private methods
 
 		/// <summary>
-		/// Populates the <see cref="_RegisteredShortcuts"/> collection with <see cref="ShortcutActionDefinition"/>s,
-		/// should be called as soon as possible, preferably from the constructor
-		/// </summary>
-		private void PopulateRegisteredShortcuts()
-		{
-			// Clear the old shortcuts definitions
-			_RegisteredShortcuts.Clear();
-
-			_RegisteredShortcuts.Add(new ShortcutActionDefinition(
-				"Place Resistor", new ShortcutKey("R"), () => PlacePartHelper(ComponentIDEnumeration.Resistor)));
-
-			_RegisteredShortcuts.Add(new ShortcutActionDefinition(
-				"Place Voltage Source", new ShortcutKey("V"), () => PlacePartHelper(ComponentIDEnumeration.Resistor)));
-
-			_RegisteredShortcuts.Add(new ShortcutActionDefinition(
-				"Place Current Source", new ShortcutKey("C"), () => PlacePartHelper(ComponentIDEnumeration.Resistor)));
-
-			_RegisteredShortcuts.Add(new ShortcutActionDefinition(
-				"Stop Current Action", new ShortcutKey("Escape"), StopActions));
-
-			_RegisteredShortcuts.Add(new ShortcutActionDefinition(
-				"Place Loose Wire", new ShortcutKey("W"), PlaceLooseWire));
-
-			// TODO: Change the ShortcutKeys to ShortcutKey.Empty and load the saved combinations from a local file
-		}
-
-		/// <summary>
-		/// Method used as a helper when performing quick actions based on placing parts
-		/// </summary>
-		/// <param name="componentID"></param>
-		private void PlacePartHelper(ComponentIDEnumeration componentID) => AppViewModel.Singleton.DesignVM.ComponentToAdd =
-			IoC.Resolve<IComponentFactory>().ImplementedComponents.First((x) => x.ID == componentID);
-
-		/// <summary>
-		/// Helper method, stops currently performed action
-		/// </summary>
-		private void StopActions() => AppViewModel.Singleton.DesignVM.StopActionCommand?.Execute(null);
-
-		/// <summary>
-		/// Helper method, prepares the program to place a loose wire
-		/// </summary>
-		private void PlaceLooseWire() => AppViewModel.Singleton.DesignVM.PrepareToPlaceLooseWireCommand.Execute(null);
-
-		#endregion
-
-		#region Public methods
-
-		/// <summary>
-		/// Assigns new shortcuts (given as Item2 in the Tuple) to all <see cref="ShortcutActionDefinition"/>
-		/// with the same Name as Item1 of the Tuple
+		/// Updates the shortcuts without checking if they're correct
 		/// </summary>
 		/// <param name="newBindings"></param>
-		public void UpdateShortcuts(List<Tuple<string, ShortcutKey>> newBindings)
+		private void UpdateKeyShortcutsList(List<Tuple<string, ShortcutKey>> newBindings)
 		{
 			// For each new binding
 			newBindings.ForEach((newBinding) =>
@@ -110,6 +63,81 @@ namespace ECAT.ViewModel
 					oldBinding.DefinedKey = newBinding.Item2;
 				}
 			});
+		}
+
+		/// <summary>
+		/// Populates the <see cref="_RegisteredShortcuts"/> collection with <see cref="ShortcutActionDefinition"/>s,
+		/// should be called as soon as possible, preferably from the constructor
+		/// </summary>
+		private void PopulateRegisteredShortcuts()
+		{
+			// Clear the old shortcuts definitions
+			_RegisteredShortcuts.Clear();
+
+			_RegisteredShortcuts.Add(new ShortcutActionDefinition(
+				"Place Resistor", new ShortcutKey("R"), () => PlacePartHelper(ComponentIDEnumeration.Resistor)));
+
+			_RegisteredShortcuts.Add(new ShortcutActionDefinition(
+				"Place Voltage Source", new ShortcutKey("V"), () => PlacePartHelper(ComponentIDEnumeration.VoltageSource)));
+
+			_RegisteredShortcuts.Add(new ShortcutActionDefinition(
+				"Place Current Source", new ShortcutKey("C"), () => PlacePartHelper(ComponentIDEnumeration.CurrentSource)));
+
+			_RegisteredShortcuts.Add(new ShortcutActionDefinition(
+				"Stop Current Action", new ShortcutKey("Escape"), () => AppViewModel.Singleton.DesignVM.StopActionCommand?.Execute(null)));
+
+			_RegisteredShortcuts.Add(new ShortcutActionDefinition(
+				"Place Loose Wire", new ShortcutKey("W"), () => AppViewModel.Singleton.DesignVM.PrepareToPlaceLooseWireCommand.Execute(null)));
+
+			// TODO: Change the ShortcutKeys to ShortcutKey.Empty and load the saved combinations from a local file
+		}
+
+		/// <summary>
+		/// Method used as a helper when performing quick actions based on placing parts
+		/// </summary>
+		/// <param name="componentID"></param>
+		private void PlacePartHelper(ComponentIDEnumeration componentID) => AppViewModel.Singleton.DesignVM.ComponentToAdd =
+			IoC.Resolve<IComponentFactory>().ImplementedComponents.First((x) => x.ID == componentID);
+		
+		#endregion
+
+		#region Public methods
+
+		/// <summary>
+		/// Assigns new shortcuts (given as Item2 in the Tuple) to all <see cref="ShortcutActionDefinition"/>
+		/// with the same Name as Item1 of the Tuple. Checks if the new key mapping are correct (each key combination
+		/// occurs at most one time, if not reverts to the previous combination and throws an exception, if yes
+		/// saves the new key mapping to a file.
+		/// </summary>
+		/// <param name="newBindings"></param>
+		public void UpdateKeyShortcuts(List<Tuple<string, ShortcutKey>> newBindings)
+		{
+			// Get a backup collection
+			var backup = RegisteredShortcuts;
+
+			// Update the shortcut keys with the new bindings
+			UpdateKeyShortcutsList(newBindings);
+
+			// Get hashes of all ShortcutKeys except the empty ones
+			var hashes = new List<int>(_RegisteredShortcuts.Where((x) => !x.Equals(ShortcutKey.Empty)).Select((x) =>
+				x.DefinedKey.GetHashCode()));
+
+			// Get a distinct collection of the hashes and check if it's count is equal to the count of all hashes
+			if(hashes.Distinct().Count() == hashes.Count)
+			{
+				// If so, then it means that all key bingins are unique and the new values may be applied and saved
+
+				// TODO: Save the new bindings locally
+			}
+			else
+			{
+				// If not, then the new key bindings are not correct: they must be reverted and an exception must be thrown
+				UpdateKeyShortcutsList(backup);
+
+				throw new ArgumentException("After modifying the shortcut collection with new key bindings the " +
+					"bindings are no longer unique (there is at least one key combination that is associated with two " +
+					"or more actions (excluding " + nameof(ShortcutKey.Empty) + ")");
+			}
 		}		
 
 		/// <summary>
@@ -118,7 +146,15 @@ namespace ECAT.ViewModel
 		/// <param name="key"></param>
 		/// <param name="activeModifiers"></param>
 		public void ProcessKeyCombination(ShortcutKey pressedKey) =>
-			_RegisteredShortcuts.FindAll((x) => x.DefinedKey == pressedKey).ForEach((x) => x.Action.Invoke());
+			_RegisteredShortcuts.Find((x) => x.DefinedKey.Equals(pressedKey)).Action.Invoke();
+
+		/// <summary>
+		/// Loads the default key mappings from a file and saves them as current ones
+		/// </summary>
+		public void RevertToDefault()
+		{
+			// TODO: Load the default key mappings from a file and save them as the current ones
+		}
 
 		#endregion
 	}
