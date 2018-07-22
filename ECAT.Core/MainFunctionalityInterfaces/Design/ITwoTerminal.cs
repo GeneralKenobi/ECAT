66c76dd3ae424ecdeed3bc68ed244efe1965212f
Szymon Żogała <szymek.655@gmail.com>
2018-07-22 12:06:50 +0200

    Added

---
+++ ECAT.Core/MainFunctionalityInterfaces/Design/ITwoTerminal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace ECAT.Core
+{
+    public interface ITwoTerminal : IBaseComponent
+    {
+		/// <summary>
+		/// One of the terminals in this two-terminal
+		/// </summary>
+		ITerminal TerminalA { get; }
+
+		/// <summary>
+		/// One of the terminals in this two-terminal
+		/// </summary>
+		ITerminal TerminalB { get; }
+
+		/// <summary>
+		/// Admittance between terminals A and B
+		/// </summary>
+		Complex Admittance { get; }
+	}
+}
---
+++ ECAT.Core/MainFunctionalityInterfaces/Simulation/INode.cs
@@ -0,0 +1,32 @@
+﻿using CSharpEnhanced.CoreClasses;
+using System.Collections.Generic;
+
+namespace ECAT.Core
+{
+	public interface INode
+	{
+		#region Properties
+
+		/// <summary>
+		/// Potential present at the node with respect to ground
+		/// </summary>		
+		RefWrapper<double> Potential { get; }
+
+		/// <summary>
+		/// List with all components that are connected to the <see cref="INode"/>
+		/// </summary>
+		List<IBaseComponent> ConnectedComponents { get; set; }
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Merges <paramref name="node"/> into this instance (transfers over the associated components)
+		/// </summary>
+		/// <param name="node"></param>
+		void Merge(INode node);
+
+		#endregion
+	}
+}
