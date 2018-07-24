92050cc19a92690d95448def3fb820959819539e
Szymon Żogała <szymek.655@gmail.com>
2018-07-24 18:43:36 +0200

    Implementation of the interface

--- ECAT.Design/Components/OneTerminal/OneTerminal.cs
+++ ECAT.Design/Components/OneTerminal/OneTerminal.cs
@@ -6,18 +6,64 @@ using System.Text;
 
 namespace ECAT.Design
 {
+	/// <summary>
+	/// Default of implementation of <see cref="IOneTerminal"/>
+	/// </summary>
 	public abstract class OneTerminal : BaseComponent, IOneTerminal
 	{
-		public ITerminal Terminal { get; } = new Terminal();
+		#region Constructor
 
-		public Complex Admittance { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		/// <summary>
+		/// Default Constructor
+		/// </summary>
+		public OneTerminal()
+		{
+			Terminal = new Terminal(new PlanePosition(Complex.Zero, _TerminalShift));
+		}
+
+		#endregion
+
+		/// <summary>
+		/// The terminal of this <see cref="IOneTerminal"/>
+		/// </summary>
+		public ITerminal Terminal { get; }
+
+		/// <summary>
+		/// Input admittance of <see cref="Terminal"/>
+		/// </summary>
+		public Complex Admittance { get ; set; }
 
 		#region Protected properties
 
 		/// <summary>
-		/// The shift assigned to <see cref="TerminalA"/>, override to provide custom value
+		/// The shift assigned to <see cref="Terminal"/>, override to provide custom value
 		/// </summary>
-		protected virtual Complex _TerminalShift => new Complex(0, -Width/2);
+		protected virtual Complex _TerminalShift { get; } = Complex.Zero;
+
+		#endregion
+
+		#region Protected methods
+
+		/// <summary>
+		/// Rotates all terminals by <paramref name="degrees"/>
+		/// </summary>
+		/// <param name="degrees"></param>
+		protected override void RotateTerminals(double degrees) => Terminal.Position.RotationAngle += degrees;
+
+		/// <summary>
+		/// Assigns positions to all <see cref="ITerminal"/>s
+		/// </summary>
+		protected override void UpdateAbsoluteTerminalPositions() => Terminal.Position.Absolute = new Complex(Center.X, Center.Y);
+
+		#endregion
+
+		#region Public methods
+
+		/// <summary>
+		/// Returns a list with all terminals in this component
+		/// </summary>
+		/// <returns></returns>
+		public override List<ITerminal> GetTerminals() => new List<ITerminal>() { Terminal };
 
 		#endregion
 	}
