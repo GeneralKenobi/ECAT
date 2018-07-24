6a6cfdca7c088f5f6e95ab89a151a90fe5e00617
Szymon Żogała <szymek.655@gmail.com>
2018-07-24 18:42:32 +0200

    Added

---
+++ ECAT.UWP/Design/Components/Ground/GroundTC.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml.Controls;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace ECAT.UWP
+{
+	public sealed class GroundTC : Control
+	{
+		public GroundTC()
+		{
+			this.DefaultStyleKey = typeof(GroundTC);
+		}
+	}
+}
