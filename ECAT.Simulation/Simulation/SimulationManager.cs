a4365875b2655609159837fd9e5217f2e17904d5
Szymon Żogała <szymek.655@gmail.com>
2018-07-25 17:10:05 +0200

    Changed the method a little, works, now time for refactoring

--- ECAT.Simulation/Simulation/SimulationManager.cs
+++ ECAT.Simulation/Simulation/SimulationManager.cs
@@ -50,7 +50,7 @@ namespace ECAT.Simulation
 			// Assign the potentials from the nodes to the associated terminals
 			nodes.ForEach((node) => node.ConnectedTerminals.ForEach((terminal) => terminal.Potential = node.Potential));
 
-			NortonEquivalent(nodes);
+			//NortonEquivalent(nodes);
 
 			/////////////////////////////////////////////
 			// Temporary - remove when ground is added
@@ -58,23 +58,46 @@ namespace ECAT.Simulation
 			//	x.ConnectedTerminals.Contains(cs.TerminalA)));
 			/////////////////////////////////////////////
 
-			// nodes.Remove(referenceNode);
+			//nodes.Remove(referenceNode);
 
 			var referenceNodes =
 				new List<INode>(nodes.Where((node) => node.ConnectedComponents.Exists((component) => component is IGround)));
 
-			referenceNodes.ForEach((node) => node.Potential.Value = 0);
+			nodes.ForEach((node) => node.Potential.Value = 0);
 
-			nodes.RemoveAll((node) => referenceNodes.Contains(node));
+			nodes.RemoveAll((node) => referenceNodes.Contains(node));			
 
-			var admittanceMatrix = ConstructDCAdmittanceMatrix(nodes);
+			var voltageSources = new List<IVoltageSource>(schematic.Components.Where((component) =>
+				component is IVoltageSource).Select((component) => component as IVoltageSource));			
 
-			var result = LinearEquations.SimplifiedGaussJordanElimination(admittanceMatrix.Item1, admittanceMatrix.Item2);
+			var admittanceMatrix = ConstructDCAdmittanceMatrix(nodes, voltageSources);
 
-			for (int i = 0; i < result.Length; ++i)
+			var complexMatrix = new Complex[admittanceMatrix.Item1.GetLength(0), admittanceMatrix.Item1.GetLength(1)];
+			var complexFreeTerms = new Complex[admittanceMatrix.Item2.Length];
+
+			for (int i = 0; i < complexMatrix.GetLength(0); ++i)
 			{
-				nodes[i].Potential.Value = result[i].Evaluate().RoundTo(1e-3).Real;
+				for (int j = 0; j < complexMatrix.GetLength(1); ++j)
+				{
+					complexMatrix[i, j] = admittanceMatrix.Item1[i, j].Evaluate();
+				}
 			}
+
+			for (int i = 0; i < complexFreeTerms.Length; ++i)
+			{
+				complexFreeTerms[i] = admittanceMatrix.Item2[i].Evaluate();
+			}
+
+			try
+			{
+				var result = LinearEquations.SimplifiedGaussJordanElimination(complexMatrix, complexFreeTerms);
+
+				for (int i = 0; i < nodes.Count; ++i)
+				{
+					nodes[i].Potential.Value += result[i].RoundTo(1e-3).Real;
+				}
+			}
+			catch (Exception e) { }
 		}
 
 		#endregion
@@ -119,9 +142,9 @@ namespace ECAT.Simulation
 		/// <param name="nodes"></param>
 		/// <param name="admittances"></param>
 		/// <param name="currents"></param>
-		private void FillPassiveDCAdmittanceDiagonal(List<INode> nodes, IExpression[,] admittances)
+		private void FillPassiveDCAdmittanceMatrixADiagonal(List<INode> nodes, IExpression[,] admittances)
 		{
-			// For each node
+			// For each node (except node 0 - it's at 0 potential by definition so any admittence times u0 = 0 always - negligible)
 			for(int i=0; i<nodes.Count; ++i)
 			{
 				// For each component connected to that node
@@ -145,11 +168,11 @@ namespace ECAT.Simulation
 		/// </summary>
 		/// <param name="nodes"></param>
 		/// <param name="admittances"></param>
-		private void FillPassiveDCAdmittanceNonDiagonal(List<INode> nodes, IExpression[,] admittances)
+		private void FillPassiveDCAdmittanceMatrixANonDiagonal(List<INode> nodes, IExpression[,] admittances)
 		{
-			for(int i=0; i<nodes.Count; ++i)
+			for (int i = 0; i < nodes.Count; ++i)
 			{
-				for(int j=0; j<i; ++j)
+				for (int j = 0; j < i; ++j)
 				{
 					// Find all components located between node i and node j
 					var admittancesBetweenNodesij =
@@ -162,12 +185,12 @@ namespace ECAT.Simulation
 						if (component is ITwoTerminal twoTerminal && twoTerminal.Admittance.Imaginary == 0)
 						{
 							// Subtract its admittance to the matrix
-							admittances[i, j] = admittances[i, i].Subtract(
+							admittances[i, j] = admittances[i, j].Subtract(
 								new Variable.VariableSource(twoTerminal.Admittance.Real).Variable);
 
 							// And do the same to the entry j,i - admittances between node i,j are identical to admittances
 							// between nodes j,i
-							admittances[j, i] = admittances[i, i].Subtract(
+							admittances[j, i] = admittances[j, i].Subtract(
 								new Variable.VariableSource(twoTerminal.Admittance.Real).Variable);
 						}
 						// Currently components other than two terminals are not supported
@@ -177,9 +200,56 @@ namespace ECAT.Simulation
 			}
 		}
 
+		private void FillPassiveDCAdmittanceBMatrix(List<INode> nodes, IExpression[,] admittances,
+			List<IVoltageSource> voltageSources)
+		{
+			for(int i=0; i<voltageSources.Count; ++i)
+			{
+				if(nodes.Exists((node) => node.ConnectedTerminals.Contains(voltageSources[i].TerminalB)))
+				{					
+					admittances[nodes.FindIndex((node) => node.ConnectedTerminals.Contains(voltageSources[i].TerminalB)), nodes.Count + i] = Variable.One;
+				}
+
+				if (nodes.Exists((node) => node.ConnectedTerminals.Contains(voltageSources[i].TerminalA)))
+				{
+					admittances[nodes.FindIndex((node) => node.ConnectedTerminals.Contains(voltageSources[i].TerminalA)), nodes.Count + i] = Variable.NegativeOne;
+				}
+			}
+		}
+
+		private void FillPassiveDCAdmittanceCMatrix(List<INode> nodes, IExpression[,] admittances,
+			List<IVoltageSource> voltageSources)
+		{
+			for (int i = 0; i < voltageSources.Count; ++i)
+			{
+				if (nodes.Exists((node) => node.ConnectedTerminals.Contains(voltageSources[i].TerminalB)))
+				{
+					admittances[nodes.Count + i, nodes.FindIndex((node) => node.ConnectedTerminals.Contains(voltageSources[i].TerminalB))] = Variable.One;
+				}
+
+				if (nodes.Exists((node) => node.ConnectedTerminals.Contains(voltageSources[i].TerminalA)))
+				{
+					admittances[nodes.Count + i, nodes.FindIndex((node) => node.ConnectedTerminals.Contains(voltageSources[i].TerminalA))] = Variable.NegativeOne;
+				}
+			}
+		}
+
+		private void FillPassiveDCAdmittanceDMatrix(List<INode> nodes, IExpression[,] admittances)
+		{
+			// TODO: Fill accordingly when dependent sources are added
+			for (int i = nodes.Count; i < admittances.GetLength(0); ++i)
+			{
+				for (int j = nodes.Count; j < admittances.GetLength(1); ++j)
+				{
+					admittances[i, j] = Variable.Zero;
+				}
+			}
+		}
+
+
 		private void AddCurrents(List<INode> nodes, IExpression[] currents)
 		{
-			for (int i = 0; i < currents.Length; ++i)
+			for (int i = 0; i < nodes.Count; ++i)
 			{
 				nodes[i].ConnectedComponents.ForEach((component) =>
 				{
@@ -198,10 +268,19 @@ namespace ECAT.Simulation
 			}
 		}
 
-		private Tuple<IExpression[,], IExpression[]> ConstructDCAdmittanceMatrix(List<INode> nodes)
+		private void AddVoltages(List<INode> nodes, IExpression[] currents, List<IVoltageSource> voltageSources)
+		{
+			for (int i = 0; i < voltageSources.Count; ++i)
+			{
+				currents[nodes.Count + i] = new Variable.VariableSource(voltageSources[i].ProducedVoltage).Variable;
+			}
+		}
+
+		private Tuple<IExpression[,], IExpression[]> ConstructDCAdmittanceMatrix(List<INode> nodes,
+			List<IVoltageSource> independentVoltageSources)
 		{
 			// The size of the system
-			int size = nodes.Count;
+			int size = nodes.Count + independentVoltageSources.Count;
 
 			// Create arrays
 			var admittances = ArrayHelpers.CreateAndInitialize<IExpression>(Variable.Zero, size, size);
@@ -210,10 +289,45 @@ namespace ECAT.Simulation
 			// TODO: When added, handle the active components
 
 			// Fill the passive admittance matrix
-			FillPassiveDCAdmittanceDiagonal(nodes, admittances);
-			FillPassiveDCAdmittanceNonDiagonal(nodes, admittances);
+			FillPassiveDCAdmittanceMatrixADiagonal(nodes, admittances);
+			FillPassiveDCAdmittanceMatrixANonDiagonal(nodes, admittances);
+			FillPassiveDCAdmittanceBMatrix(nodes, admittances, independentVoltageSources);
+			FillPassiveDCAdmittanceCMatrix(nodes, admittances, independentVoltageSources);
+			FillPassiveDCAdmittanceDMatrix(nodes, admittances);
 
-			AddCurrents(nodes, currents);			
+			//inactiveNodePairs.ForEach((pair) =>
+			//{
+			//	for(int i=0; i<size; ++i)
+			//	{
+			//		admittances[pair.Item2, i] = Variable.Zero;
+			//	}
+			//	admittances[pair.Item1, pair.Item2] = Variable.Zero;				
+			//});
+
+			//List<int> lastRows = new List<int>() { size - 1, size - 2 };
+
+			//for (int i = 0; i < lastRows.Count; ++i)
+			//{
+			//	if (lastRows.Contains(activeVSNodes[i]))
+			//	{
+			//		lastRows.Remove(activeVSNodes[i]);
+			//		activeVSNodes.RemoveAt(i);
+			//		--i;
+			//	}
+			//}
+
+			//activeVSNodes.ForEach((nodeIndex) =>
+			//{
+			//	for(int i=0; i<size; ++i)
+			//	{
+			//		var temp = admittances[nodeIndex, i];
+			//		admittances[nodeIndex, i] = admittances[lastRows[0], i];
+			//		admittances[lastRows[0], i] = temp;
+			//	}
+			//});
+
+			AddCurrents(nodes, currents);
+			AddVoltages(nodes, currents, independentVoltageSources);
 
 			return new Tuple<IExpression[,], IExpression[]>(admittances, currents);
 		}
