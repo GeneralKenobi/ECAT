4f2005f7c1c2e96a4c21a20f66550caa61d554f3
Szymon Żogała <szymek.655@gmail.com>
2018-07-27 13:12:37 +0200

    Documentation

--- ECAT.UWP/Design/VoltageDrop/VoltageToArrowheadVisibilityConverter.cs
+++ ECAT.UWP/Design/VoltageDrop/VoltageToArrowheadVisibilityConverter.cs
@@ -4,6 +4,11 @@ using Windows.UI.Xaml.Data;
 
 namespace ECAT.UWP
 {
+	/// <summary>
+	/// If value is a double returns Visible for values >= 0, collapsed otherwise. If parameter is not null
+	/// returns collapes for values >=0, visible otherwise (opposite to null parameter).
+	/// If value is not a double always returns collapsed.
+	/// </summary>
 	public class VoltageToArrowheadVisibilityConverter : IValueConverter
 	{
 		public object Convert(object value, Type targetType, object parameter, string language)
