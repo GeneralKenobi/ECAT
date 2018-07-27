5c140bbc0a098f8227ab0c8f421042f1e2bc7d78
Szymon Żogała <szymek.655@gmail.com>
2018-07-27 11:48:14 +0200

    Added a PotentialValueChanged event

--- ECAT.Core/MainFunctionalityInterfaces/Design/ITerminal.cs
+++ ECAT.Core/MainFunctionalityInterfaces/Design/ITerminal.cs
@@ -1,4 +1,5 @@
 ﻿using CSharpEnhanced.CoreClasses;
+using System;
 using System.ComponentModel;
 
 namespace ECAT.Core
@@ -9,7 +10,9 @@ namespace ECAT.Core
 	/// to have a real-time updated value of potential at the terminal
 	/// </summary>
 	public interface ITerminal : INotifyPropertyChanged
-    {
+	{
+		#region Properties
+
 		/// <summary>
 		/// Position of the terminal on the design area used to connect the terminals
 		/// </summary>
@@ -19,5 +22,16 @@ namespace ECAT.Core
 		/// Reference to potential at <see cref="INode"/> that is associated with this <see cref="ITerminal"/>
 		/// </summary>
 		RefWrapperPropertyChanged<double> Potential { get; set; }
-    }
+
+		#endregion
+
+		#region Events
+
+		/// <summary>
+		/// Event fired whenever the value of the potential changes
+		/// </summary>
+		EventHandler PotentialValueChanged { get; set; }
+
+		#endregion
+	}
 }
