dc533e4a1160075bdbe19400b1df700abd7b1d8b
Szymon Żogała <szymek.655@gmail.com>
2018-07-27 22:09:40 +0200

    Added

---
+++ ECAT.UWP/Design/GeneralPurposeControls/CurrentFlow/CurrentFlowTC.cs
@@ -0,0 +1,45 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace ECAT.UWP
+{
+	/// <summary>
+	/// Control visualizing a current flow across a 2-terminal element
+	/// </summary>
+	public sealed class CurrentFlowTC : Control
+    {
+		#region Constructor
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public CurrentFlowTC()
+        {
+            this.DefaultStyleKey = typeof(CurrentFlowTC);
+        }
+
+		#endregion
+
+		#region Current Dependency Property
+
+		/// <summary>
+		/// The value of the current, flowing from right to left
+		/// </summary>
+		public double Current
+		{
+			get => (double)GetValue(CurrentProperty);
+			set => SetValue(CurrentProperty, value);
+		}
+
+		/// <summary>
+		/// Backing store for <see cref="Current"/>
+		/// </summary>
+		public static readonly DependencyProperty CurrentProperty =
+			DependencyProperty.Register(nameof(Current), typeof(double),
+			typeof(CurrentFlowTC), new PropertyMetadata(default(double)));
+
+		#endregion
+	}
+}
---
+++ ECAT.UWP/GeneralValueConverters/DoubleToChosenParameterConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace ECAT.UWP
+{
+	/// <summary>
+	/// If value is a double and it's positive: checks if parameter is a string, if it contains a backslash ("/"),
+	/// splits the parameter on the backslash, if there were at least two substrings created tries to parse both of them to ints.
+	/// If all this is successfull returns the int on the left of the backslash if value was nonnegative, otherwise returns the
+	/// int on the right of the backslash.
+	/// If any of the conditions described above is not met returns null.
+	/// </summary>
+	public class DoubleToChosenParameterConverter : IValueConverter
+    {
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			// Check if value is a double, if parameter is a string and if it contains a backslash
+			if (value is double d && parameter is string sParam && sParam.Contains("/"))
+			{
+				// Split the parameter
+				var split = sParam.Split("/");
+
+				// If it's long enough and the first two substrings can be parsed to int
+				if(split.Length >= 1 && int.TryParse(split[0], out int column1) && int.TryParse(split[1], out int column2))
+				{
+					// Return the first one if the value is nonnegative, otherwise return the other one
+					return d >= 0 ? column1 : column2;
+				}
+			}
+			
+			// If something went wrong return null
+			return null;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
