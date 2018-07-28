637084e1934e8fb596b444b8a8310419f0b0b290
Szymon Żogała <szymek.655@gmail.com>
2018-07-28 15:19:48 +0200

    Added

---
+++ ECAT.UWP/Design/GeneralPurposeControls/TwoTerminalComponentPackage/TwoTerminalComponentPackageTC.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml.Controls;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace ECAT.UWP
+{
+	/// <summary>
+	/// Control providing basic sub-controls to a two terminal component's control. Voltage drop across the component is displayed
+	/// at the top as an arrow, current flow (indicated by an arrow) is displayed on the component's wire (which is center by default),
+	/// components value is presented below the component
+	/// </summary>
+	public sealed class TwoTerminalComponentPackageTC : Control
+	{
+		#region Constructor
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public TwoTerminalComponentPackageTC()
+		{
+			this.DefaultStyleKey = typeof(TwoTerminalComponentPackageTC);
+		}
+
+		#endregion
+	}
+}
