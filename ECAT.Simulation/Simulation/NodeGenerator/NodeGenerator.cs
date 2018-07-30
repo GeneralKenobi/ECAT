46c7386a892041885ae775ebd8ee1f9468092773
Szymon Żogała <szymek.655@gmail.com>
2018-07-30 16:54:10 +0200

    Adjusted the methods to the new wire system which relies solely on the position of the wires and sockets

--- ECAT.Simulation/Simulation/NodeGenerator/NodeGenerator.cs
+++ ECAT.Simulation/Simulation/NodeGenerator/NodeGenerator.cs
@@ -9,71 +9,7 @@ namespace ECAT.Simulation
 	/// </summary>
 	public static class NodeGenerator
     {
-		#region Private static methods
-
-		/// <summary>
-		/// Helper of <see cref="MergeNodesConnectedByWires(List{INode}, List{IWire})"/>, goes through the passed list of wires and,
-		/// if it finds a wire that connects to at least one component, creates an <see cref="INode"/> that will be merged with nodes
-		/// at both ends of the selected wire. Removes the merged wires from the passed node list, removes the wire from the passed
-		/// wire list. Modifies <paramref name="nodes"/> and <paramref name="wires"/>, returns true if a wire connected to at least
-		/// one component was found (at least one end of the wire connects to a component), false otherwise.
-		/// </summary>
-		/// <param name="nodes"></param>
-		/// <param name="wires"></param>
-		/// <param name="createdNode"></param>
-		/// <param name="selectedWire"></param>
-		/// <returns></returns>
-		private static bool InitialWireMerge(List<INode> nodes, List<IWire> wires, out INode createdNode, out IWire selectedWire)
-		{
-			// Initially assign null to out parameters
-			createdNode = null;
-			selectedWire = null;
-
-			// A list for nodes of the selected wire
-			List<INode> mainWireNodes = null;
-
-			// Go through each wire to find the first wire that has nodes
-			foreach (var wire in wires)
-			{
-				// Get all nodes from the list that are positioned either at the beginning or ending of the wire
-				mainWireNodes = nodes.FindAll((node) => node.Position != null && (node.Position.Equals(wire.Beginning) ||
-					node.Position.Equals(wire.Ending)));
-
-				// If there was at least one
-				if (mainWireNodes.Count > 1)
-				{
-					// Assign the selected wire and finish the loop
-					selectedWire = wire;
-					break;
-				}
-			}
-
-			// If the selected wire was not assigned then it means the remaining wires don't connect with any components - nothing
-			// else to do
-			if (selectedWire == null)
-			{
-				return false;
-			}
-
-			// If the method got here then it means that it found a wire which connects to at least  one component
-			// Create a node for it
-			createdNode = new Node();
-
-			// And a copy to use in lambdas
-			var copyForLambdas = createdNode;
-
-			// Merge the nodes into the new Node
-			mainWireNodes.ForEach((node) => copyForLambdas.Merge(node));
-
-			// Remove them from the list of nodes
-			mainWireNodes.ForEach((node) => nodes.Remove(node));
-
-			// Remove the wire from the list of wires
-			wires.Remove(selectedWire);
-
-			// Return success
-			return true;
-		}
+		#region Private static methods		
 
 		/// <summary>
 		/// Merges all nodes that are connected by a wire network (modifies both lists)
@@ -86,29 +22,26 @@ namespace ECAT.Simulation
 			// Keep going until all wires were considered
 			while (wires.Count > 0)
 			{
-				// If the initial merge didn't succeed, stop the loop - there is no more merging to be done
-				if (!InitialWireMerge(nodes, wires, out INode finalNode, out IWire selectedWire))
-				{
-					break;
-				}
-
-				// If the method got here then it means that a suitable wire was found, get a list of all wires that are connected
+				// Get a list of all wires that are connected to the first wire
 				// to it
-				var wireNetwork = new List<IWire>(selectedWire.GetAllConnectedWires());
+				var wireNetwork = new List<IWire>(wires[0].GetAllConnectedWires(wires));
 
 				// Get all nodes that are associated with the ends of those wires (at this point nodes may have null positions which
 				// means those nodes are no longer position-based, so check for that)
 				var associatedNodes = new List<INode>(nodes.Where((node) => node.Position != null && wireNetwork.Exists((wire) =>
-					node.Position.Equals(wire.Beginning) || node.Position.Equals(wire.Ending))));
+					wire.BelongsToWire(node.Position))));
+
+				// Create a node which will cover the whole obtained wire network
+				var wireNode = new Node();
 
 				// Merge all found nodes
-				associatedNodes.ForEach((node) => finalNode.Merge(node));
+				associatedNodes.ForEach((node) => wireNode.Merge(node));
 
 				// Remove them from the collection
 				associatedNodes.ForEach((node) => nodes.Remove(node));
 
 				// Add the node that remained after merging to the collection
-				nodes.Add(finalNode);
+				nodes.Add(wireNode);
 
 				// Remove all wires that were handled in the process
 				wireNetwork.ForEach((wire) => wires.Remove(wire));
