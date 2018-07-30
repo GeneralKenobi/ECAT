c69b961be981da0030aa1cebb6a28f2d19bb83ed
Szymon Żogała <szymek.655@gmail.com>
2018-07-30 17:18:31 +0200

    Added

---
+++ ECAT.UWP/Design/EditMenu/VoltageSourceEdit/VoltageSourceEditUC.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace ECAT.UWP
+{
+	/// <summary>
+	/// Control for editing specific to an <see cref="IVoltageSource"/>
+	/// </summary>
+	public sealed partial class VoltageSourceEditUC : UserControl
+	{
+		#region Constructor
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public VoltageSourceEditUC()
+		{
+			this.InitializeComponent();
+		}
+
+		#endregion
+	}
+}
---
+++ ECAT.ViewModel/ViewModel/DesignViewModules/ComponentEditViewModels/SpecificComponentEditViewModels/VoltageSource/VoltageSourceEditViewModel.cs
@@ -0,0 +1,37 @@
+﻿using ECAT.Core;
+
+namespace ECAT.ViewModel
+{
+	/// <summary>
+	/// View model for edits specific to <see cref="IVoltageSource"/>
+	/// </summary>
+	public class VoltageSourceEditViewModel : SpecificComponentEditViewModel<IVoltageSource>
+    {
+		#region Constructor
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public VoltageSourceEditViewModel(ComponentViewModel viewModel) : base(viewModel) { }
+
+		#endregion
+
+		#region Public properties
+
+		/// <summary>
+		/// The header for the edit with produced voltage
+		/// </summary>
+		public string ProducedVoltageEditHeader { get; } = "Produced Voltage [V]";
+
+		/// <summary>
+		/// Accessor to the produced voltage on the edited <see cref="IVoltageSource"/>
+		/// </summary>
+		public double ProducedVoltage
+		{
+			get => _EditedComponent.ProducedVoltage;
+			set => _EditedComponent.ProducedVoltage = value; 
+		}
+
+		#endregion
+	}
+}
