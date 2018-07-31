f40788c5e6d7c2e2d2006cfa7d718b7f638caf98
Szymon Żogała <szymek.655@gmail.com>
2018-07-31 14:15:30 +0200

    Adjusted the shortuct method for logging messages to the new interface

--- ECAT.Core/IoC/IoC.cs
+++ ECAT.Core/IoC/IoC.cs
@@ -8,7 +8,32 @@ namespace ECAT.Core
 	/// </summary>
 	public static class IoC
 	{
-		#region Public methods
+		#region Private static methods
+
+		/// <summary>
+		/// Returns true if all the main (necessary) interfaces for the app are registered
+		/// </summary>
+		/// <param name="container"></param>
+		/// <returns></returns>
+		private static bool NecessaryElementsRegistered(IContainer container) => container.IsRegistered<IDesignManager>();
+
+		#endregion
+
+		#region Public static properties		
+
+		/// <summary>
+		/// Indicates whether the IoC was setup
+		/// </summary>
+		public static bool Ready => Container == null;
+
+		/// <summary>
+		/// Container with component registration
+		/// </summary>
+		public static IContainer Container { get; private set; } = null;
+
+		#endregion
+
+		#region Public static methods
 
 		/// <summary>
 		/// Builds the container for the given <see cref="ContainerBuilder"/>
@@ -40,33 +65,17 @@ namespace ECAT.Core
 		/// Shortcut to log messages through <see cref="IInfoLogger"/>
 		/// </summary>
 		/// <param name="message"></param>
+		/// <param name="loggerID">ID used to identify the caller. Messages may be removed only if the same ID is used later</param>
+		/// <param name="duration">Maximum lifetime of the message, if the message was still presented after the time expires,
+		/// it will be removed. By default it's infinite</param>
+		public static void Log(string message, int loggerID, InfoLoggerMessageDuration duration = InfoLoggerMessageDuration.Infinite) =>
+			Container.Resolve<IInfoLogger>().Log(message, loggerID, duration);
+
+		/// <summary>
+		/// Shortcut to remove logged messages through <see cref="IInfoLogger"/>
+		/// </summary>
 		/// <param name="loggerID"></param>
-		public static void Log(string message, int loggerID) => Container.Resolve<IInfoLogger>().Log(message);
-
-		#endregion
-
-		#region Private static methods
-
-		/// <summary>
-		/// Returns true if all the main (necessary) interfaces for the app are registered
-		/// </summary>
-		/// <param name="container"></param>
-		/// <returns></returns>
-		private static bool NecessaryElementsRegistered(IContainer container) => container.IsRegistered<IDesignManager>();
-
-		#endregion
-
-		#region Public static properties		
-
-		/// <summary>
-		/// Indicates whether the IoC was setup
-		/// </summary>
-		public static bool Ready => Container == null;
-
-		/// <summary>
-		/// Container with component registration
-		/// </summary>
-		public static IContainer Container { get; private set; } = null;
+		public static void RemoveLog(int loggerID) => Container.Resolve<IInfoLogger>().RemoveLog(loggerID);
 
 		#endregion
 	}
