99923924921bcc59655b2a1e920c51f22e0e2b3e
Szymon Żogała <szymek.655@gmail.com>
2018-07-31 14:14:38 +0200

    Added a method for removing a message if it was logged by the caller, modified the logging method to allow for messages with limited time duration

--- ECAT.Core/MainFunctionalityInterfaces/Design/InfoLogger/IInfoLogger.cs
+++ ECAT.Core/MainFunctionalityInterfaces/Design/InfoLogger/IInfoLogger.cs
@@ -17,10 +17,20 @@
 		#region Methods
 
 		/// <summary>
-		/// Logs a new message
+		/// Logs a new message. The message has to contain at least one non-white space character to be logged. To remove messages
+		/// use <see cref="RemoveLog(int)"/>
 		/// </summary>
 		/// <param name="message"></param>
-		void Log(string message);
+		/// <param name="loggerID">ID used to identify the caller. Messages may be removed only if the same ID is used later</param>
+		/// <param name="duration">Maximum lifetime of the message, if the message was still presented after the time expires,
+		/// it will be removed. By default it's infinite</param>
+		void Log(string message, int loggerID, InfoLoggerMessageDuration duration = InfoLoggerMessageDuration.Infinite);
+
+		/// <summary>
+		/// Removes the currently presented message if the given ID matches the one given while logging the message
+		/// </summary>
+		/// <param name="loggerID"></param>
+		void RemoveLog(int loggerID);
 
 		#endregion
 	}
