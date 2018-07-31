dc479b8b5d4ac8361d0d79ce8286890af111c657
Szymon Żogała <szymek.655@gmail.com>
2018-07-31 14:09:43 +0200

    Added

---
+++ ECAT.Core/GlobalEnums/InfoLoggerMessageDuration.cs
@@ -0,0 +1,28 @@
+﻿namespace ECAT.Core
+{
+	/// <summary>
+	/// Enumeration for duration of messages logged in an <see cref="IInfoLogger"/>
+	/// </summary>
+	public enum InfoLoggerMessageDuration
+    {
+		/// <summary>
+		/// The message will be presented until it is either removed or a new message is logged
+		/// </summary>
+		Infinite = 0,
+
+		/// <summary>
+		/// The message will be shown briefly (approximately 5 seconds), just to quickly signal something to the user
+		/// </summary>
+		Short = 1,
+
+		/// <summary>
+		/// The message will be shown for a moderate amount of time (approximately 10 seconds)
+		/// </summary>
+		Medium = 2,
+
+		/// <summary>
+		/// The message will be shown for a long amount of time (approximately 15 seconds)
+		/// </summary>
+		Long = 3,
+    }
+}
