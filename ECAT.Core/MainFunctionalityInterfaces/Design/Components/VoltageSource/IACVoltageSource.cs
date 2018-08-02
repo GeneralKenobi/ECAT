c6efdddc12e3a2b5d3448d853ccaf3c04f500d7e
Szymon Żogała <szymek.655@gmail.com>
2018-08-02 17:18:19 +0200

    Added

---
+++ ECAT.Core/MainFunctionalityInterfaces/Design/Components/VoltageSource/IACVoltageSource.cs
@@ -0,0 +1,24 @@
+﻿using CSharpEnhanced.Maths;
+
+namespace ECAT.Core
+{
+	/// <summary>
+	/// Interface for alternating voltage sources
+	/// </summary>
+	public interface IACVoltageSource : IVoltageSource
+    {
+		#region Properties
+
+		/// <summary>
+		/// Frequency of the produced voltage
+		/// </summary>
+		Variable FrequencyVar { get; }
+
+		/// <summary>
+		/// Accessor to the frequency of produced voltage
+		/// </summary>
+		double Frequency { get; set; }
+
+		#endregion
+	}
+}
