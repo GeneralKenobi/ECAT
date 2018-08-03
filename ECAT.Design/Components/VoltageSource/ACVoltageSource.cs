ec97f6d13cd88cd260eb7a9c6472d9d050ca28e3
Szymon Żogała <szymek.655@gmail.com>
2018-08-04 00:18:28 +0200

    Added

---
+++ ECAT.Design/Components/VoltageSource/ACVoltageSource.cs
@@ -0,0 +1,24 @@
+﻿using ECAT.Core;
+
+namespace ECAT.Design
+{
+	/// <summary>
+	/// Class implementing AC voltage sources, standard implementation of <see cref="IACVoltageSource"/>
+	/// </summary>
+	public class ACVoltageSource : VoltageSource, IACVoltageSource
+	{
+		#region Public properties
+
+		/// <summary>
+		/// Frequency of the AC voltage produced by this <see cref="ACVoltageSource"/>
+		/// </summary>
+		public double Frequency { get; set; }
+
+		/// <summary>
+		/// The positive peak value of the produced voltage sine wave
+		/// </summary>
+		public double PeakProducedVoltage { get; set; }
+
+		#endregion
+	}
+}
