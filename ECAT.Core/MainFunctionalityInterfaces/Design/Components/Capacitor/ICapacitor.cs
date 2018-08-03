a71c0c0dac2252b50945a1052c32b23ea1bc0500
Szymon Żogała <szymek.655@gmail.com>
2018-08-04 00:11:59 +0200

    Added

---
+++ ECAT.Core/MainFunctionalityInterfaces/Design/Components/Capacitor/ICapacitor.cs
@@ -0,0 +1,17 @@
+﻿namespace ECAT.Core
+{
+	/// <summary>
+	/// Interface for capacitors
+	/// </summary>
+	public interface ICapacitor : ITwoTerminal
+    {
+		#region Properties
+
+		/// <summary>
+		/// Capacitance of this <see cref="ICapacitor"/>
+		/// </summary>
+		double Capacitance { get; set; }
+
+		#endregion
+	}
+}
---
+++ ECAT.Design/Components/Capacitor/Capacitor.cs
@@ -0,0 +1,19 @@
+﻿using ECAT.Core;
+
+namespace ECAT.Design
+{
+	/// <summary>
+	/// Class implementing capacitors, standard implementation of <see cref="ICapacitor"/>
+	/// </summary>
+	public class Capacitor : TwoTerminal, ICapacitor
+    {
+		#region Public properties
+
+		/// <summary>
+		/// Capacitance of this <see cref="Capacitor"/>
+		/// </summary>
+		public double Capacitance { get; set; }
+
+		#endregion
+	}
+}
