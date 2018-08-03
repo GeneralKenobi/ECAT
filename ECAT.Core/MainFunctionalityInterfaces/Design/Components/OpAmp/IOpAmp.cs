2f8387404141a6d5ed533434ae20a67fa1f11c62
Szymon Żogała <szymek.655@gmail.com>
2018-08-03 17:31:18 +0200

    Documentation

--- ECAT.Core/MainFunctionalityInterfaces/Design/Components/OpAmp/IOpAmp.cs
+++ ECAT.Core/MainFunctionalityInterfaces/Design/Components/OpAmp/IOpAmp.cs
@@ -1,15 +1,23 @@
-﻿using CSharpEnhanced.Maths;
-
-namespace ECAT.Core
+﻿namespace ECAT.Core
 {
 	/// <summary>
 	/// Interface for operational amplifiers - 
 	/// </summary>
 	public interface IOpAmp : IThreeTerminal
     {
-		double PositiveSupplyVoltage { get; }
-		double NegativeSupplyVoltage { get; }
+		/// <summary>
+		/// Positive supply voltage - output cannot be greater than this value
+		/// </summary>
+		double PositiveSupplyVoltage { get; set; }
 
-		double OpenLoopGain { get; }
+		/// <summary>
+		/// Negative supply voltage - output cannot be smaller than this value
+		/// </summary>
+		double NegativeSupplyVoltage { get; set; }
+
+		/// <summary>
+		/// Open loop gain - voltage gain defined as output voltage divided by differential voltage (U+ - U-)
+		/// </summary>
+		double OpenLoopGain { get; set; }
 	}
 }
