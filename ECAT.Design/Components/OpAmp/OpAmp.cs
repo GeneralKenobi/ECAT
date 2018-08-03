4ee1f975b09b29b50de30bb9317db5903673f8f5
Szymon Żogała <szymek.655@gmail.com>
2018-08-03 17:37:56 +0200

    Documentation

--- ECAT.Design/Components/OpAmp/OpAmp.cs
+++ ECAT.Design/Components/OpAmp/OpAmp.cs
@@ -1,23 +1,61 @@
 ﻿using System.Numerics;
-using CSharpEnhanced.Maths;
 using ECAT.Core;
 
 namespace ECAT.Design
 {
+	/// <summary>
+	/// Class representing an operational amplifier, standard implementation of <see cref="IOpAmp"/>.
+	/// <see cref="ThreeTerminal.TerminalA"/> is the non-inverting input, <see cref="ThreeTerminal.TerminalB"/>
+	/// is the inverting input and <see cref="ThreeTerminal.TerminalC"/> is the output.
+	/// </summary>
 	public class OpAmp : ThreeTerminal, IOpAmp
 	{
-		public double PositiveSupplyVoltage { get; } = 15;
-		public double NegativeSupplyVoltage { get; } = -15;
-		public double OpenLoopGain { get; } = 1e6;
+		#region Protected properties
 
-		public override double Width { get; } = 300;
-
-		public override double Height { get; } = 200;
-
+		/// <summary>
+		/// Shift relative to center applied to terminal A (non-inverting input)
+		/// </summary>
 		protected override Complex _TerminalAShift { get; } = new Complex(-150, 50);
 
+		/// <summary>
+		/// Shift relative to center applied to terminal B (inverting input)
+		/// </summary>
 		protected override Complex _TerminalBShift { get; } = new Complex(-150, -50);
 
+		/// <summary>
+		/// Shift relative to center applied to terminal C (output)
+		/// </summary>
 		protected override Complex _TerminalCShift { get; } = new Complex(150, 0);
+
+		#endregion
+
+		#region Public properties
+
+		/// <summary>		
+		/// Positive supply voltage - output cannot be greater than this value
+		/// </summary>
+		public double PositiveSupplyVoltage { get; set; } = 15;
+
+		/// <summary>
+		/// Negative supply voltage - output cannot be smaller than this value
+		/// </summary>
+		public double NegativeSupplyVoltage { get; set; } = -15;
+
+		/// <summary>
+		/// Open loop gain - voltage gain defined as output voltage divided by differential voltage (U+ - U-)
+		/// </summary>
+		public double OpenLoopGain { get; set; } = 1e6;
+
+		/// <summary>
+		/// Width of the <see cref="OpAmp"/> in horizontal position
+		/// </summary>
+		public override double Width { get; } = 300;
+
+		/// <summary>
+		/// Height of the <see cref="OpAmp"/> in horizontal position
+		/// </summary>
+		public override double Height { get; } = 200;
+
+		#endregion
 	}
 }
