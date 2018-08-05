de71bb3901d3c5665f0f50aada83ffcfe98a2d53
Szymon Żogała <szymek.655@gmail.com>
2018-08-05 23:49:23 +0200

    Added

---
+++ ECAT.UWP/Design/EditMenu/CapacitorEdit/CapacitorEditUC.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace ECAT.UWP
+{
+	/// <summary>
+	/// Control for editing <see cref="ICapacitor"/>
+	/// </summary>
+	public sealed partial class CapacitorEditUC : UserControl
+	{
+		#region Constructor
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public CapacitorEditUC()
+		{
+			this.InitializeComponent();
+		}
+
+		#endregion
+	}
+}
