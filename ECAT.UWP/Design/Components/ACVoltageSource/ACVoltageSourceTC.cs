80c7127b600a32379a63496a70ba8770ffb8b91a
Szymon Żogała <szymek.655@gmail.com>
2018-08-05 21:45:15 +0200

    Added

---
+++ ECAT.UWP/Design/Components/ACVoltageSource/ACVoltageSourceTC.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace ECAT.UWP
+{
+	/// <summary>
+	/// Control for an <see cref="IACVoltageSource"/>
+	/// </summary>
+	public sealed class ACVoltageSourceTC : Control
+	{
+		#region Constructor
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public ACVoltageSourceTC()
+		{
+			this.DefaultStyleKey = typeof(ACVoltageSourceTC);
+		}
+
+		#endregion
+	}
+}
---
+++ ECAT.UWP/Design/Components/Capacitor/CapacitorTC.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace ECAT.UWP
+{
+	/// <summary>
+	/// Control for an <see cref="ICapacitor"/>
+	/// </summary>
+	public sealed class CapacitorTC : Control
+	{
+		#region Constructor
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public CapacitorTC()
+		{
+			this.DefaultStyleKey = typeof(CapacitorTC);
+		}
+
+		#endregion
+	}
+}
