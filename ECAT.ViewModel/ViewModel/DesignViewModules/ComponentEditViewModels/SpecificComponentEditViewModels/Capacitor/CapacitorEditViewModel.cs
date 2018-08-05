008c327a1b3096130c34b88be4019e467b47e0c0
Szymon Żogała <szymek.655@gmail.com>
2018-08-05 23:43:32 +0200

    Added

---
+++ ECAT.ViewModel/ViewModel/DesignViewModules/ComponentEditViewModels/SpecificComponentEditViewModels/Capacitor/CapacitorEditViewModel.cs
@@ -0,0 +1,38 @@
+﻿using ECAT.Core;
+
+namespace ECAT.ViewModel
+{
+	/// <summary>
+	/// Edit view model for <see cref="ICapacitor"/>s
+	/// </summary>
+	public class CapacitorEditViewModel : SpecificComponentEditViewModel<ICapacitor>
+	{
+		#region Constructors
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		/// <param name="componentViewModel"></param>
+		public CapacitorEditViewModel(ComponentViewModel componentViewModel) : base(componentViewModel) { }
+
+		#endregion
+
+		#region Public Properties		
+
+		/// <summary>
+		/// Accessor to capacitance
+		/// </summary>
+		public double Capacitance
+		{
+			get => _EditedComponent.Capacitance;
+			set => _EditedComponent.Capacitance = value;
+		}
+
+		/// <summary>
+		/// Capacitance edit field header
+		/// </summary>
+		public string CapacitanceEditHeader { get; } = "Capacitance [F]";
+
+		#endregion
+	}
+}
