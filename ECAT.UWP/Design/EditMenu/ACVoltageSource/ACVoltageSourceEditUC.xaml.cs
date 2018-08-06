b6606d32a5d74f8b6288f446bbf9d37d891e497f
Szymon Żogała <szymek.655@gmail.com>
2018-08-06 23:18:10 +0200

    Added

---
+++ ECAT.UWP/Design/EditMenu/ACVoltageSource/ACVoltageSourceEditUC.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace ECAT.UWP
+{
+	/// <summary>
+	/// Control for editing <see cref="IACVoltageSource"/>
+	/// </summary>
+	public sealed partial class ACVoltageSourceEditUC : UserControl
+	{
+		#region Constructor
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public ACVoltageSourceEditUC()
+		{
+			this.InitializeComponent();
+		}
+
+		#endregion
+	}
+}
---
+++ ECAT.ViewModel/ViewModel/DesignViewModules/ComponentEditViewModels/SpecificComponentEditViewModels/ACVoltageSource/ACVoltageSourceEditViewModel.cs
@@ -0,0 +1,66 @@
+﻿using ECAT.Core;
+
+namespace ECAT.ViewModel
+{
+	/// <summary>
+	/// Viewmodel for editing <see cref="IACVoltageSource"/>
+	/// </summary>
+	public class ACVoltageSourceEditViewModel : SpecificComponentEditViewModel<IACVoltageSource>
+	{
+		#region Constructors
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		/// <param name="componentViewModel"></param>
+		public ACVoltageSourceEditViewModel(ComponentViewModel componentViewModel) : base(componentViewModel) { }
+
+		#endregion
+
+		#region Public Properties
+
+		/// <summary>
+		/// Accessor to the peak produced AC voltage
+		/// </summary>
+		public double PeakProducedVoltage
+		{
+			get => _EditedComponent.PeakProducedVoltage;
+			set => _EditedComponent.PeakProducedVoltage = value;
+		}
+
+		/// <summary>
+		/// Capacitance edit field header
+		/// </summary>
+		public string PeakProducedVoltageEditHeader { get; } = "Peak AC voltage [V]";
+
+		/// <summary>
+		/// Accessor to the frequency of the produced AC voltage
+		/// </summary>
+		public double Frequency
+		{
+			get => _EditedComponent.Frequency;
+			set => _EditedComponent.Frequency = value;
+		}
+
+		/// <summary>
+		/// Capacitance edit field header
+		/// </summary>
+		public string FrequencyEditHeader { get; } = "Frequency [Hz]";
+
+		/// <summary>
+		/// Accessor to the DC offset
+		/// </summary>
+		public double DCOffset
+		{
+			get => _EditedComponent.ProducedDCVoltage;
+			set => _EditedComponent.ProducedDCVoltage = value;
+		}
+
+		/// <summary>
+		/// Capacitance edit field header
+		/// </summary>
+		public string DCOffsetEditHeader { get; } = "DC Offset [V]";
+
+		#endregion
+	}
+}
