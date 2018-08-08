ebd3290721f3527148111d43851a79d1b8d0eb18
Szymon Żogała <szymek.655@gmail.com>
2018-08-08 22:23:03 +0200

    Reworked for the upcoming UI update

--- ECAT.UWP/Design/GeneralPurposeControls/CurrentFlow/CurrentFlowTC.cs
+++ ECAT.UWP/Design/GeneralPurposeControls/CurrentFlow/CurrentFlowTC.cs
@@ -1,6 +1,4 @@
-﻿using System.Numerics;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml.Controls;
 
 namespace ECAT.UWP
 {
@@ -18,26 +16,6 @@ namespace ECAT.UWP
         {
             this.DefaultStyleKey = typeof(CurrentFlowTC);
         }
-
-		#endregion
-
-		#region Current Dependency Property
-
-		/// <summary>
-		/// The value of the current, flowing from left to right
-		/// </summary>
-		public Complex Current
-		{
-			get => (Complex)GetValue(CurrentProperty);
-			set => SetValue(CurrentProperty, value);
-		}
-
-		/// <summary>
-		/// Backing store for <see cref="Current"/>
-		/// </summary>
-		public static readonly DependencyProperty CurrentProperty =
-			DependencyProperty.Register(nameof(Current), typeof(Complex),
-			typeof(CurrentFlowTC), new PropertyMetadata(default(Complex)));
 
 		#endregion
 	}
