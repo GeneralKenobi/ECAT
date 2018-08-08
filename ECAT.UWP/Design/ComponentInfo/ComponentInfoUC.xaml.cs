80702d39b05d8dae6ca7b95a16f2877dbdf2d5fb
Szymon Żogała <szymek.655@gmail.com>
2018-08-08 22:33:55 +0200

    Added

---
+++ ECAT.UWP/Design/ComponentInfo/ComponentInfoUC.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace ECAT.UWP
+{
+	public sealed partial class ComponentInfoUC : UserControl
+	{
+		public ComponentInfoUC()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
