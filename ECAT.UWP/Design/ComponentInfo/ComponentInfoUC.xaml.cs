fca5e6bee1166713abfb4714a82e01e4f031411d
Szymon Żogała <szymek.655@gmail.com>
2018-08-09 14:02:34 +0200

    Implemented

--- ECAT.UWP/Design/ComponentInfo/ComponentInfoUC.xaml.cs
+++ ECAT.UWP/Design/ComponentInfo/ComponentInfoUC.xaml.cs
@@ -2,11 +2,21 @@
 
 namespace ECAT.UWP
 {
+	/// <summary>
+	/// Control displaying the component info in the bottom left corner
+	/// </summary>
 	public sealed partial class ComponentInfoUC : UserControl
 	{
+		#region Constructor
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
 		public ComponentInfoUC()
 		{
 			this.InitializeComponent();
 		}
+
+		#endregion
 	}
 }
