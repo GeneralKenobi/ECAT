48f5eafc5576dda43b271e1bc091462a3474a8d2
Szymon Żogała <szymek.655@gmail.com>
2018-08-13 11:16:38 +0200

    Reformed the setup to make use of IManager

--- ECAT.UWP/IoCSetup/IoCSetup.cs
+++ ECAT.UWP/IoCSetup/IoCSetup.cs
@@ -10,6 +10,30 @@ namespace ECAT.UWP
 	/// </summary>
 	public static class IoCSetup
 	{
+		#region Private static methods
+
+		/// <summary>
+		/// Registers <see cref="IManager.GetTypesToRegister"/> as types and <see cref="IManager.GetInstancesToRegister"/> as instances
+		/// </summary>
+		/// <param name="builder"></param>
+		/// <param name="manager"></param>
+		private static void RegisterManagersImplementations(ContainerBuilder builder, IManager manager)
+		{
+			// Register instances
+			foreach(var instance in manager.GetInstancesToRegister())
+			{
+				builder.RegisterInstance(instance.Item2).As(instance.Item1);
+			}
+
+			// Register types
+			foreach(var type in manager.GetTypesToRegister())
+			{
+				builder.RegisterType(type.Item2).As(type.Item1);
+			}
+		}
+
+		#endregion
+
 		#region Public static methods
 
 		/// <summary>
@@ -20,14 +44,17 @@ namespace ECAT.UWP
 			// Create the builder
 			ContainerBuilder builder = new ContainerBuilder();
 
-			// Register necessary types
-			builder.RegisterInstance(new DesignManager()).As<IDesignManager>();
-			builder.RegisterInstance(new SimulationManager()).As<ISimulationManager>();			
-			builder.RegisterInstance(new ComponentFactory()).As<IComponentFactory>();
-			builder.RegisterInstance(new PlanePositionFactory()).As<IPlanePositionFactory>();
-			builder.RegisterInstance(new DefaultValues()).As<IDefaultValues>();
-			builder.RegisterInstance(new InfoLogger()).As<IInfoLogger>();
+			// Create managers
+			var designManager = new DesignManager();
+			var simulationManager = new SimulationManager();
 
+			// Register them
+			builder.RegisterInstance(designManager).As<IDesignManager>();
+			builder.RegisterInstance(simulationManager).As<ISimulationManager>();
+
+			// Let them make their registrations
+			RegisterManagersImplementations(builder, designManager);
+			RegisterManagersImplementations(builder, simulationManager);
 
 			IoC.Build(builder);
 		}
