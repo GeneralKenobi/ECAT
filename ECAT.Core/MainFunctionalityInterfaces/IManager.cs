df6cb7ecc332b8cd6f322ba6e6874b599a6aed3c
Szymon Żogała <szymek.655@gmail.com>
2018-08-13 10:59:07 +0200

    Added a method for registering instances

--- ECAT.Core/MainFunctionalityInterfaces/IManager.cs
+++ ECAT.Core/MainFunctionalityInterfaces/IManager.cs
@@ -12,8 +12,15 @@ namespace ECAT.Core
 		#region Methods
 
 		/// <summary>
-		/// Returns an enumeration of types to register in IoC. Item1 is the interface to register as, Item2 is the implementation
-		/// type, instance will be created using <see cref="Activator"/>
+		/// Returns an enumeration of types to register in IoC. Item1 is the interface to register as, Item2 is the object to register
+		/// as an instance.
+		/// </summary>
+		/// <returns></returns>
+		IEnumerable<Tuple<Type, object>> GetInstancesToRegister();
+
+		/// <summary>
+		/// Returns an enumeration of types to register in IoC. Item1 is the interface to register as, Item2 is the type to register
+		/// as implementation of the interface
 		/// </summary>
 		/// <returns></returns>
 		IEnumerable<Tuple<Type, Type>> GetTypesToRegister();
