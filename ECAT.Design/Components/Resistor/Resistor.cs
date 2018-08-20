61b9e6e29035aceb7b9c75a9d8d4b41ae7ca0146
Szymon Żogała <szymek.655@gmail.com>
2018-08-20 14:05:05 +0200

    Constructor provids own info headers, added a method calculating power info

--- ECAT.Design/Components/Resistor/Resistor.cs
+++ ECAT.Design/Components/Resistor/Resistor.cs
@@ -1,4 +1,9 @@
-﻿using System.Numerics;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using CSharpEnhanced.Helpers;
+using CSharpEnhanced.Maths;
 using ECAT.Core;
 
 namespace ECAT.Design
@@ -8,6 +13,15 @@ namespace ECAT.Design
 	/// </summary>
 	public class Resistor : TwoTerminal, IResistor
     {
+		#region Constructors
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public Resistor() : base(new string[] { "Voltage", "Current", "Power" }) { }
+
+		#endregion
+
 		#region Private members
 
 		/// <summary>
@@ -44,6 +58,34 @@ namespace ECAT.Design
 		/// <param name="frequency"></param>
 		/// <returns></returns>
 		protected override Complex CalculateAdmittance(double frequency) => 1 / Resistance;
+
+		/// <summary>
+		/// Returns info related to power
+		/// </summary>
+		/// <returns></returns>
+		protected IEnumerable<string> GetPowerInfo()
+		{
+			var dcPower = _VoltageDrop.Type.HasFlag(VoltageDropType.DC) ? Math.Pow(_VoltageDrop.DC, 2) * GetAdmittance(0) : 0;
+
+			//var maxPower = maxCurrent * _VoltageDrop.Maximum;
+			var maxPower = Math.Pow(_VoltageDrop.Maximum, 2) * GetAdmittance(0);
+
+			var rmsPower = _VoltageDrop.ComposingACWaveforms.Sum((voltage) =>
+				(Math.Pow(voltage.Value.Magnitude, 2) * GetAdmittance(voltage.Key)).Magnitude) / 2 + dcPower;
+
+			// Return characteristic power information
+			yield return "Maximum instantenous power: " +
+				SIHelpers.ToSIStringExcludingSmallPrefixes(maxPower.RoundToDigit(4), "W");
+
+			yield return "Average power: " + SIHelpers.ToSIStringExcludingSmallPrefixes(rmsPower.RoundToDigit(4), "W");
+		}
+
+		/// <summary>
+		/// Returns complete info for the component (basic two terminal plus power)
+		/// </summary>
+		/// <returns></returns>
+		protected override IEnumerable<IEnumerable<string>> GetComponentInfo() =>
+			base.GetComponentInfo().Concat(new IEnumerable<string>[] { GetPowerInfo() });
 
 		#endregion
 	}
