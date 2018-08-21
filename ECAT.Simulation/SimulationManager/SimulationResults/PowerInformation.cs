a2b6bd454c618978303a7310baa05e2ce115b87c
Szymon Żogała <szymek.655@gmail.com>
2018-08-21 14:19:50 +0200

    Added

---
+++ ECAT.Simulation/SimulationManager/SimulationResults/PowerInformation.cs
@@ -0,0 +1,32 @@
+﻿using ECAT.Core;
+
+namespace ECAT.Simulation
+{
+	public partial class SimulationManager
+	{
+		/// <summary>
+		/// Standard implementation of <see cref="IPowerInformation"/>, presents information about a power loss/delivery
+		/// </summary>
+		private class PowerInformation : IPowerInformation
+		{
+			#region Public properties
+
+			/// <summary>
+			/// Average value
+			/// </summary>
+			public double Average { get; set; }
+
+			/// <summary>
+			/// Maximum instantenous power loss
+			/// </summary>
+			public double MaximumLost { get; set; }
+
+			/// <summary>
+			/// Maximum instantenous power supplied
+			/// </summary>
+			public double MaximumSupplied { get; set; }
+
+			#endregion
+		}
+	}
+}
