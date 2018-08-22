e20b18257b3204445c7ce938b91fe3c8be05751b
Szymon Żogała <szymek.655@gmail.com>
2018-08-22 14:18:13 +0200

    Added

---
+++ ECAT.Core/GlobalEnums/PowerType.cs
@@ -0,0 +1,18 @@
+﻿namespace ECAT.Core.GlobalEnums
+{
+	/// <summary>
+	/// Type of power dissipation
+	/// </summary>
+	public enum PowerType
+    {
+		/// <summary>
+		/// Power is dissipated - lost on the element
+		/// </summary>
+		Dissipated = 0,
+
+		/// <summary>
+		/// Power is supplied by the element
+		/// </summary>
+		Supplied = 1,
+    }
+}
