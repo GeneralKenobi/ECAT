bd6b07566e2c48f9edb5ab34449cb4fbb2845622
Szymon Żogała <szymek.655@gmail.com>
2018-08-23 13:50:58 +0200

    Added

---
+++ ECAT.Core/MainFunctionalityInterfaces/Design/Components/BasicTypes/IActiveComponent.cs
@@ -0,0 +1,18 @@
+﻿namespace ECAT.Core
+{
+	/// <summary>
+	/// Interface for active components that produce current which is calculated during the simulation rather than after simulation
+	/// using component's parameters and voltage drop across the component.
+	/// </summary>
+	public interface IActiveComponent
+	{
+		#region Properties
+
+		/// <summary>
+		/// Index used to query <see cref="ISimulationResults"/> for produced current
+		/// </summary>
+		int ActiveComponentIndex { get; set; }
+
+		#endregion
+	}
+}
