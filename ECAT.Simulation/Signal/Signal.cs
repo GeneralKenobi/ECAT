7bcdc57e7414723936442f67a28b334383d65223
Szymon Żogała <szymek.655@gmail.com>
2018-08-23 18:12:18 +0200

    Added

---
+++ ECAT.Simulation/Signal/Signal.cs
@@ -0,0 +1,28 @@
+﻿using ECAT.Core;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace ECAT.Simulation.Signal
+{
+	/// <summary>
+	/// Standard implementation of <see cref="ISignal"/>, represents a single signal that may be measured in a circuit
+	/// </summary>
+	public class Signal : ISignal
+    {
+		#region Public properties
+
+		/// <summary>
+		/// DC component
+		/// </summary>
+		public double DC { get; set; }
+
+		/// <summary>
+		/// List with all phasors adding to the total signal
+		/// </summary>
+		public IEnumerable<KeyValuePair<double, Complex>> ComposingPhasors { get; set; } =
+			Enumerable.Empty<KeyValuePair<double, Complex>>();
+
+		#endregion
+	}
+}
