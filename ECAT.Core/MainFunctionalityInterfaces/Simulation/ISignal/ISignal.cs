db2a2f0a97e29674cf0da986e2c9712d582e6637
Szymon Żogała <szymek.655@gmail.com>
2018-08-23 15:37:39 +0200

    Added

---
+++ ECAT.Core/MainFunctionalityInterfaces/Simulation/ISignal/ISignal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace ECAT.Core
+{
+	/// <summary>
+	/// Interface for classes defining a signal - voltage drop or current flow
+	/// </summary>
+	public interface ISignal
+	{
+		#region Properties
+
+		/// <summary>
+		/// DC component of the signal
+		/// </summary>
+		double DC { get; }
+
+		/// <summary>
+		/// List with phasors adding to the signal
+		/// </summary>
+		IEnumerable<KeyValuePair<double, Complex>> ComposingPhasors { get; }
+
+		#endregion
+	}
+}
