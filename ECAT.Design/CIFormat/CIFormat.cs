269747cf48ac55802df10d9c27ed758852b971b4
Szymon Żogała <szymek.655@gmail.com>
2018-08-24 22:37:59 +0200

    Added

---
+++ ECAT.Design/CIFormat/CIFormat.cs
@@ -0,0 +1,35 @@
+﻿using CSharpEnhanced.Helpers;
+
+namespace ECAT.Design
+{
+	/// <summary>
+	/// Class containing methods formating text for display in component info section
+	/// </summary>
+	public static class CIFormat
+    {
+		#region Private static properties
+		
+		/// <summary>
+		/// Round all values to this digit
+		/// </summary>
+		private static int _RoundToDigit { get; } = 4;
+		
+		#endregion
+
+		#region Public static methods
+
+		/// <summary>
+		/// Returns a string for display in component info section which will be in the form: 
+		/// "{<paramref name="description"/>} : {<paramref name="value"/>}{<paramref name="unit"/>}"
+		/// If <paramref name="value"/> is a <see cref="double.NaN"/> it is instead displayed as "unavailable" and the unit is omitted
+		/// </summary>
+		/// <param name="description"></param>
+		/// <param name="value"></param>
+		/// <param name="unit"></param>
+		/// <returns></returns>
+		public static string LineInfo(string description, double value, string unit) =>	description + " : " + 
+			(double.IsNaN(value) ? "unavailable" : SIHelpers.ToSIStringExcludingSmallPrefixes(value, unit, _RoundToDigit));
+
+		#endregion
+	}
+}
