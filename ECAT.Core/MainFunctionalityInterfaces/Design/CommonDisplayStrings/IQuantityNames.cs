bdeb05febb5f4d384aee5badc449ffdd9788bfad
Szymon Żogała <szymek.655@gmail.com>
2018-08-25 12:14:43 +0200

    Added

---
+++ ECAT.Core/MainFunctionalityInterfaces/Design/CommonDisplayStrings/IQuantityNames.cs
@@ -0,0 +1,51 @@
+﻿namespace ECAT.Core
+{
+	/// <summary>
+	/// Interface for a class providing unified display names of common quantities, eg. voltage
+	/// </summary>
+	public interface IQuantityNames
+    {
+		#region Properties
+
+		/// <summary>
+		/// Display name for voltage
+		/// </summary>
+		string Voltage { get; }
+
+		/// <summary>
+		/// Display name for voltage with first letter capitalized
+		/// </summary>
+		string VoltageCap { get; }
+		/// <summary>
+		/// Display name for current
+		/// </summary>
+		string Current { get; }
+
+		/// <summary>
+		/// Display name for current with first letter capitalized
+		/// </summary>
+		string CurrentCap { get; }
+
+		/// <summary>
+		/// Display name for power
+		/// </summary>
+		string Power { get; }
+
+		/// <summary>
+		/// Display name for power with first letter capitalized
+		/// </summary>
+		string PowerCap { get; }
+
+		/// <summary>
+		/// Display name for frequency
+		/// </summary>
+		string Frequency { get; }
+
+		/// <summary>
+		/// Display name for frequency with first letter capitalized
+		/// </summary>
+		string FrequencyCap { get; }
+
+		#endregion
+	}
+}
---
+++ ECAT.Design/CommonDisplayStrings/QuantityNames/QuantityNames.cs
@@ -0,0 +1,72 @@
+﻿using ECAT.Core;
+
+namespace ECAT.Design
+{
+	/// <summary>
+	/// Standard implementation of <see cref="IQuantityNames"/>
+	/// </summary>
+	public class QuantityNames : IQuantityNames
+    {
+		#region Constructors
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		private QuantityNames() { }
+
+		#endregion
+
+		#region Public properties
+
+		/// <summary>
+		/// Display name for voltage
+		/// </summary>
+		public string Voltage { get; } = "voltage";
+
+		/// <summary>
+		/// Display name for voltage with first letter capitalized
+		/// </summary>
+		public string VoltageCap { get; } = "Voltage";
+
+		/// <summary>
+		/// Display name for current
+		/// </summary>
+		public string Current { get; } = "current";
+
+		/// <summary>
+		/// Display name for current with first letter capitalized
+		/// </summary>
+		public string CurrentCap { get; } = "Current";
+
+		/// <summary>
+		/// Display name for power
+		/// </summary>
+		public string Power { get; } = "power";
+
+		/// <summary>
+		/// Display name for power with first letter capitalized
+		/// </summary>
+		public string PowerCap { get; } = "Power";
+
+		/// <summary>
+		/// Display name for frequency
+		/// </summary>
+		public string Frequency { get; } = "frequency";
+
+		/// <summary>
+		/// Display name for frequency with first letter capitalized
+		/// </summary>
+		public string FrequencyCap { get; } = "Frequency";
+
+		#endregion
+
+		#region Singleton
+
+		/// <summary>
+		/// Singleton instance of this class
+		/// </summary>
+		public static QuantityNames Singleton = new QuantityNames();
+
+		#endregion
+	}
+}
