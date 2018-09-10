7bc6a117740b4cabc87c02b27d94d81908439f46
Szymon Żogała <szymek.655@gmail.com>
2018-09-10 23:03:21 +0200

    Added as a bridge between ISignalData and all consumers that wish to obtain characteristic values of some ISignalData

---
+++ ECAT.Core/MainFunctionalityInterfaces/Simulation/ISignal/ISignalDataInterpreter.cs
@@ -0,0 +1,30 @@
+﻿namespace ECAT.Core
+{
+	/// <summary>
+	/// Interface for classes exposing methods returning characteristic values of some <see cref="ISignalData"/>.
+	/// </summary>
+	public interface ISignalDataInterpreter
+    {
+		#region Properties
+
+		/// <summary>
+		/// Calculates and returns the maximum instantenous value of the signal
+		/// </summary>
+		/// <returns></returns>
+		double Maximum();
+
+		/// <summary>
+		/// Calculates and returns the minimum instantenous value of the signal
+		/// </summary>
+		/// <returns></returns>
+		double Minimum();
+
+		/// <summary>
+		/// Calculates and returns the root-mean-square value of the signal
+		/// </summary>
+		/// <returns></returns>
+		double RMS();
+
+		#endregion
+	}
+}
