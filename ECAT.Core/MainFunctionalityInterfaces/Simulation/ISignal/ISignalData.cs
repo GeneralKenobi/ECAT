7b4ef36830082537702437dc29ca62a94d90d57e
Szymon Żogała <szymek.655@gmail.com>
2018-09-10 22:57:19 +0200

    Added

---
+++ ECAT.Core/MainFunctionalityInterfaces/Simulation/ISignal/ISignalData.cs
@@ -0,0 +1,17 @@
+﻿namespace ECAT.Core
+{
+	/// <summary>
+	/// Interface for classes containing raw data about a signal
+	/// </summary>
+	public interface ISignalData
+	{
+		#region Properties
+
+		/// <summary>
+		/// Object capable of interpreting the particular <see cref="ISignalData"/>
+		/// </summary>
+		ISignalDataInterpreter Interpreter { get; }
+
+		#endregion
+	}
+}
