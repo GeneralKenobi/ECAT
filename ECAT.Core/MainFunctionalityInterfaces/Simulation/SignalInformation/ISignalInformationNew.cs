c11676deea6d68f5c29781d0a0518e32d2353ef6
Szymon Żogała <szymek.655@gmail.com>
2018-09-13 22:07:42 +0200

    Added

---
+++ ECAT.Core/MainFunctionalityInterfaces/Simulation/SignalInformation/ISignalInformationNew.cs
@@ -0,0 +1,40 @@
+﻿using CSharpEnhanced.CoreInterfaces;
+
+namespace ECAT.Core
+{
+	/// <summary>
+	/// Interface for classes presenting information about some <see cref="ISignalData"/>
+	/// </summary>
+	public interface ISignalInformationNew : IDeepCopy<ISignalInformationNew>
+	{
+		#region Properties
+
+		/// <summary>
+		/// Raw data of the signal
+		/// </summary>
+		ISignalData Data { get; }
+
+		/// <summary>
+		/// True if the direction of signal was inverted (with respect to assumed directions) to present <see cref="Maximum"/> as a
+		/// positive number
+		/// </summary>
+		bool InvertedDirection { get; }
+
+		/// <summary>
+		/// The maximum instantenous signal value that may occur
+		/// </summary>
+		double Maximum { get; }
+
+		/// <summary>
+		/// The minimum instantenous signal value that may occur
+		/// </summary>
+		double Minimum { get; }
+
+		/// <summary>
+		/// RMS value of this signal
+		/// </summary>
+		double RMS { get; }
+
+		#endregion
+	}
+}
