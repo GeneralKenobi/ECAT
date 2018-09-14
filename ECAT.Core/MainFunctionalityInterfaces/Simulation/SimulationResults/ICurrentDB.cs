b80dc6d92e995dea2c3004f02428b682ce2467ec
Szymon Żogała <szymek.655@gmail.com>
2018-09-14 14:15:56 +0200

    Added methods

--- ECAT.Core/MainFunctionalityInterfaces/Simulation/SimulationResults/ICurrentDB.cs
+++ ECAT.Core/MainFunctionalityInterfaces/Simulation/SimulationResults/ICurrentDB.cs
@@ -5,5 +5,34 @@
 	/// </summary>
 	public interface ICurrentDB
     {
-    }
+		#region Methods
+
+		/// <summary>
+		/// Gets information about current flowing through an <see cref="IResistor"/> or null if unsuccessful
+		/// </summary>
+		/// <param name="voltageDrop"></param>
+		/// <param name="resistor"></param>
+		/// <returns></returns>
+		ISignalInformation GetCurrent(IResistor resistor, bool reverseDirection);
+
+		/// <summary>
+		/// Gets information about current flowing through an <see cref="ICapacitor"/> or null if unsuccessful
+		/// </summary>
+		/// <param name="voltageDrop"></param>
+		/// <param name="capacitor"></param>
+		/// <returns></returns>
+		ISignalInformation GetCurrent(ICapacitor capacitor, bool reverseDirection);
+
+		/// <summary>
+		/// Returns current produced by some <see cref="IActiveComponent"/>. If simulation was not yet performed or the current can't be
+		/// found returns null
+		/// </summary>
+		/// <param name="activeComponentIndex">Index of the <see cref="IActiveComponent"/> whose current to query</param>
+		/// <param name="reverseDirection">True if the direction of current should be reversed with respect to the one given
+		/// by convention for the specific element</param>
+		/// <returns></returns>
+		ISignalInformation GetCurrent(int activeComponentIndex, bool reverseDirection);
+
+		#endregion
+	}
 }
--- ECAT.Core/MainFunctionalityInterfaces/Simulation/SimulationResults/IVoltageDB.cs
+++ ECAT.Core/MainFunctionalityInterfaces/Simulation/SimulationResults/IVoltageDB.cs
@@ -4,6 +4,25 @@
 	/// Interface for classes holding information about voltage drops calculated in simulation
 	/// </summary>
 	public interface IVoltageDB
-    {
-    }
+	{
+		#region Methods
+
+		/// <summary>
+		/// Gets a voltage drop of a node with respect to ground or returns null if unsuccessful
+		/// </summary>
+		/// <param name="nodeIndex"></param>
+		/// <returns></returns>
+		ISignalInformation GetVoltageDrop(int nodeIndex);
+
+		/// <summary>
+		/// Gets information on voltage drop between two nodes (with node A being treated as the reference node) or returns null
+		/// if unsuccessful
+		/// </summary>
+		/// <param name="nodeAIndex"></param>
+		/// <param name="nodeBIndex"></param>
+		/// <returns></returns>
+		ISignalInformation GetVoltageDrop(int nodeAIndex, int nodeBIndex);
+
+		#endregion
+	}
 }
