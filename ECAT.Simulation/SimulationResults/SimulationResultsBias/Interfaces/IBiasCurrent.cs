1cdcbde7942a06237c20c778682490e652243b1e
Szymon Żogała <szymek.655@gmail.com>
2018-09-14 14:35:11 +0200

    Added methods

--- ECAT.Simulation/SimulationResults/SimulationResultsBias/Interfaces/IBiasCurrent.cs
+++ ECAT.Simulation/SimulationResults/SimulationResultsBias/Interfaces/IBiasCurrent.cs
@@ -1,13 +1,44 @@
-﻿namespace ECAT.Simulation
+﻿using ECAT.Core;
+
+namespace ECAT.Simulation
 {
 	public partial class SimulationResultsBias
 	{
 		/// <summary>
-		/// Interface for classes capable of computing currents through components
+		/// Private interface for classes capable of computing and caching currents through components, the purpose of the interface is
+		/// to work with other private interfaces/classes computing and caching values of voltage, power, etc.
 		/// </summary>
 		private interface IBiasCurrent
 		{
+			#region Methods
 
+			/// <summary>
+			/// Gets current flowing through an <see cref="IResistor"/> or null if unsuccessful and stores it in
+			/// <paramref name="current"/>. Returns true on success, false otherwise.
+			/// </summary>
+			/// <param name="resistor"></param>
+			/// <param name="current"></param>
+			/// <returns></returns>
+			bool TryGetCurrent(IResistor resistor, out IPhasorDomainSignal current);
+
+			/// <summary>
+			/// Gets current flowing through an <see cref="ICapacitor"/> or null if unsuccessful and stores it in
+			/// <paramref name="current"/>. Returns true on success, false otherwise.
+			/// <param name="capacitor"></param>
+			/// <paramref name="current"></paramref>
+			/// <returns></returns>
+			bool GetCurrent(ICapacitor capacitor, out IPhasorDomainSignal current);
+
+			/// <summary>
+			/// Gets current produced by some <see cref="IActiveComponent"/> or null if unsuccessful and stores it in
+			/// <paramref name="current"/>. Returns true on success, false otherwise.
+			/// </summary>
+			/// <param name="activeComponentIndex">Index of the <see cref="IActiveComponent"/> whose current to query</param>
+			/// <paramref name="current"></paramref>
+			/// <returns></returns>
+			bool GetCurrent(int activeComponentIndex, out IPhasorDomainSignal current);
+
+			#endregion
 		}
 	}
 }
