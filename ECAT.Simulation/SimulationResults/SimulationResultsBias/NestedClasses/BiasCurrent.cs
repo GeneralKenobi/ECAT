a7425b880e5f96bf172458a13b710985a8ca5209
Szymon Żogała <szymek.655@gmail.com>
2018-09-14 23:12:44 +0200

    Added a constructor and private properties

--- ECAT.Simulation/SimulationResults/SimulationResultsBias/NestedClasses/BiasCurrent.cs
+++ ECAT.Simulation/SimulationResults/SimulationResultsBias/NestedClasses/BiasCurrent.cs
@@ -1,4 +1,7 @@
 ﻿using ECAT.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ECAT.Simulation
 {
@@ -10,7 +13,57 @@ namespace ECAT.Simulation
 		/// </summary>
 		private class BiasCurrent : ICurrentDB, IBiasCurrent
 		{
+			#region Constructors
 
+			/// <summary>
+			/// Default constructor, requires two parameters, if either is null, an exception will be thrown
+			/// </summary>
+			/// <param name="voltageDrops">Object contain information about voltage drops calculated in simulation, can't be null</param>
+			/// <param name="activeComponentCurrents">Currents produced by active components, can't be null</param>
+			public BiasCurrent(IBiasVoltage voltageDrops, IEnumerable<KeyValuePair<int, IPhasorDomainSignal>> activeComponentCurrents)
+			{
+				_VoltageDrops = voltageDrops ?? throw new ArgumentNullException(nameof(voltageDrops));
+
+				_ActiveComponentsCurrentCache = new Dictionary<int, Tuple<IPhasorDomainSignal, SignalInformation, SignalInformation>>(
+					activeComponentCurrents?.ToDictionary(
+					// Key remains the same
+					(x) => x.Key,
+					// Value is constructor based on value in enumation's elements
+					(x) => new Tuple<IPhasorDomainSignal, SignalInformation, SignalInformation>(
+						// First tuple element is the signal
+						x.Value,
+						// Second is info constructed based on the signal
+						new SignalInformation(x.Value),
+						// Third is info constructed based on negation of the signal
+						new SignalInformation(x.Value.CopyAndNegate())))
+						// Finally if the activeComponentsCurrents was null, the ? operator included above will return null which will
+						// be caught here and an exception will be thrown
+						?? throw new ArgumentNullException(nameof(activeComponentCurrents)));
+			}
+
+			#endregion
+
+			#region Private properties
+
+			/// <summary>
+			/// Contains information about voltage drops calculated in simulation
+			/// </summary>
+			private IBiasVoltage _VoltageDrops { get; }
+
+			/// <summary>
+			/// Cache with currents produced by <see cref="IVoltageSource"/>s, <see cref="IACVoltageSource"/>s and <see cref="IOpAmp"/>s
+			/// Item1 is the data on which the signal is based, Item2 is information about the signal data, Item3 is information
+			/// about negated signal data.
+			/// </summary>
+			private Dictionary<int, Tuple<IPhasorDomainSignal, SignalInformation, SignalInformation>> _ActiveComponentsCurrentCache { get; }
+
+			/// <summary>
+			/// Contains already computed currents, Item1 is in the standard direction, Item2 is in the reverse direction
+			/// </summary>
+			private Dictionary<IBaseComponent, Tuple<SignalInformation, SignalInformation>> _CurrentCache { get; } =
+				new Dictionary<IBaseComponent, Tuple<SignalInformation, SignalInformation>>();
+
+			#endregion
 		}
 	}
 }
