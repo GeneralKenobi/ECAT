cc360445c0d977ce2f788f322975c1a06885b3af
Szymon Żogała <szymek.655@gmail.com>
2018-09-14 22:38:21 +0200

    Added

---
+++ ECAT.Simulation/SimulationResults/SimulationResultsBias/NestedClasses/BiasCurrent.cs
@@ -0,0 +1,16 @@
+﻿using ECAT.Core;
+
+namespace ECAT.Simulation
+{
+	public partial class SimulationResultsBias : ISimulationResults
+	{
+		/// <summary>
+		/// Provides functionality connected with storing, calculating and exposing currents and information about them in
+		/// form of <see cref="IPhasorDomainSignal"/>s and <see cref="ISignalInformation"/>
+		/// </summary>
+		private class BiasCurrent : ICurrentDB, IBiasCurrent
+		{
+
+		}
+	}
+}
