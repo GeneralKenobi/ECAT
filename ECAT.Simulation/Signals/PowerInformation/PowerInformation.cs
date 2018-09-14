4a598923e0bd8c8233fc1617a72499a22a54b5d9
Szymon Żogała <szymek.655@gmail.com>
2018-09-14 12:56:08 +0200

    No longer nested classes of SimulationManager

--- ECAT.Simulation/Signals/PowerInformation/PowerInformation.cs
+++ ECAT.Simulation/Signals/PowerInformation/PowerInformation.cs
@@ -2,38 +2,35 @@
 
 namespace ECAT.Simulation
 {
-	public partial class SimulationManager
+	/// <summary>
+	/// Standard implementation of <see cref="IPowerInformation"/>, presents information about a power loss/delivery
+	/// </summary>
+	public class PowerInformation : IPowerInformation
 	{
+		#region Public properties
+
 		/// <summary>
-		/// Standard implementation of <see cref="IPowerInformation"/>, presents information about a power loss/delivery
+		/// Average value, negative power is supplied, positive power is dissipated
 		/// </summary>
-		private class PowerInformation : IPowerInformation
-		{
-			#region Public properties
+		public double Average { get; set; }
 
-			/// <summary>
-			/// Average value, negative power is supplied, positive power is dissipated
-			/// </summary>
-			public double Average { get; set; }
+		/// <summary>
+		/// Determines whether power is dissipated/supplied by the element
+		/// </summary>
+		public PowerType AveragePowerType =>
+			// Average equal to 0 is no power, average greater than 0 is dissipated and average smaller than 0 is supplied
+			Average == 0 ? PowerType.None : Average > 0 ? PowerType.Dissipated : PowerType.Supplied;
 
-			/// <summary>
-			/// Determines whether power is dissipated/supplied by the element
-			/// </summary>
-			public PowerType AveragePowerType =>
-				// Average equal to 0 is no power, average greater than 0 is dissipated and average smaller than 0 is supplied
-				Average == 0 ? PowerType.None : Average > 0 ? PowerType.Dissipated : PowerType.Supplied;
+		/// <summary>
+		/// Maximum instantenous power value
+		/// </summary>
+		public double Maximum { get; set; }
 
-			/// <summary>
-			/// Maximum instantenous power value
-			/// </summary>
-			public double Maximum { get; set; }
+		/// <summary>
+		/// Minimum instantenous power value 
+		/// </summary>
+		public double Minimum { get; set; }
 
-			/// <summary>
-			/// Minimum instantenous power value 
-			/// </summary>
-			public double Minimum { get; set; }
-
-			#endregion
-		}
-	}
+		#endregion
+	}	
 }
--- ECAT.Simulation/Signals/SignalInformation/SignalInformation.cs
+++ ECAT.Simulation/Signals/SignalInformation/SignalInformation.cs
@@ -4,145 +4,142 @@ using System;
 
 namespace ECAT.Simulation
 {
-	public partial class SimulationManager
+	/// <summary>
+	/// Standard implementation of <see cref="ISignalInformation"/>, presents information about a signal - voltage drop or current flow		
+	/// </summary>
+	public class SignalInformation : ISignalInformation
 	{
+		#region Constructors
+
 		/// <summary>
-		/// Standard implementation of <see cref="ISignalInformation"/>, presents information about a signal - voltage drop or current flow		
+		/// Default constructor
 		/// </summary>
-		private class SignalInformation : ISignalInformation
+		public SignalInformation() { }
+
+		/// <summary>
+		/// Constructor with parameter
+		/// </summary>
+		public SignalInformation(ISignalData data)
 		{
-			#region Constructors
-
-			/// <summary>
-			/// Default constructor
-			/// </summary>
-			public SignalInformation() { }
-
-			/// <summary>
-			/// Constructor with parameter
-			/// </summary>
-			public SignalInformation(ISignalData data)
-			{
-				Data = data;
-			}
-
-			/// <summary>
-			/// Copy constructor
-			/// </summary>
-			public SignalInformation(ISignalInformation source)
-			{
-				Copy(source);
-			}
-
-			#endregion
-
-			#region Private members
-
-			/// <summary>
-			/// Backing store for <see cref="Data"/>
-			/// </summary>
-			ISignalData mData;
-
-			#endregion
-
-			#region Public properties
-
-			/// <summary>
-			/// Raw data of the signal
-			/// </summary>
-			public ISignalData Data
-			{
-				get => mData;
-				private set
-				{
-					if(mData != value)
-					{
-						mData = value;
-						DataChanged();
-					}
-				}
-			}
-
-			/// <summary>
-			/// True if the direction of signal was inverted (with respect to assumed directions) to present <see cref="Maximum"/> as a
-			/// positive number
-			/// </summary>
-			public bool InvertedDirection { get; set; }
-
-			/// <summary>
-			/// The maximum signal value that may occur
-			/// </summary>
-			public double Maximum { get; set; }
-
-			/// <summary>
-			/// The minimum signal that may occur
-			/// </summary>
-			public double Minimum { get; set; }
-
-			/// <summary>
-			/// RMS value of this signal
-			/// </summary>
-			public double RMS { get; set; }
-
-			#endregion
-
-			#region Private methods
-
-			private void DataChanged()
-			{
-				if (Data != null)
-				{
-					Maximum = Data.Interpreter.Maximum();
-					Minimum = Data.Interpreter.Minimum();
-					RMS = Data.Interpreter.RMS();
-				}
-				else
-				{
-					Maximum = 0;
-					Minimum = 0;
-					RMS = 0;
-				}
-			}
-
-			#endregion
-
-			#region Public methods
-
-			/// <summary>
-			/// Copies all contents of <paramref name="signalInformation"/> to this object.
-			/// </summary>
-			/// <param name="signalInformation"></param>
-			public void Copy(ISignalInformation signalInformation)
-			{
-				if(signalInformation == null)
-				{
-					throw new ArgumentNullException(nameof(signalInformation));
-				}
-
-				Data = signalInformation.Data;
-
-				Maximum = signalInformation.Maximum;
-
-				Minimum = signalInformation.Minimum;
-
-				RMS = signalInformation.RMS;
-
-				InvertedDirection = signalInformation.InvertedDirection;
-			}
-
-			/// <summary>
-			/// Returns a copy of this instance
-			/// </summary>
-			/// <returns></returns>
-			ISignalInformation IDeepCopyTo<ISignalInformation>.Copy() => CopySignalInformation();
-
-			/// <summary>
-			/// Returns a copy of this instance
-			/// </summary>
-			/// <returns></returns>
-			public SignalInformation CopySignalInformation() =>	new SignalInformation(this);
-
-			#endregion
+			Data = data;
 		}
+
+		/// <summary>
+		/// Copy constructor
+		/// </summary>
+		public SignalInformation(ISignalInformation source)
+		{
+			Copy(source);
+		}
+
+		#endregion
+
+		#region Private members
+
+		/// <summary>
+		/// Backing store for <see cref="Data"/>
+		/// </summary>
+		ISignalData mData;
+
+		#endregion
+
+		#region Public properties
+
+		/// <summary>
+		/// Raw data of the signal
+		/// </summary>
+		public ISignalData Data
+		{
+			get => mData;
+			private set
+			{
+				if(mData != value)
+				{
+					mData = value;
+					DataChanged();
+				}
+			}
+		}
+
+		/// <summary>
+		/// True if the direction of signal was inverted (with respect to assumed directions) to present <see cref="Maximum"/> as a
+		/// positive number
+		/// </summary>
+		public bool InvertedDirection { get; set; }
+
+		/// <summary>
+		/// The maximum signal value that may occur
+		/// </summary>
+		public double Maximum { get; set; }
+
+		/// <summary>
+		/// The minimum signal that may occur
+		/// </summary>
+		public double Minimum { get; set; }
+
+		/// <summary>
+		/// RMS value of this signal
+		/// </summary>
+		public double RMS { get; set; }
+
+		#endregion
+
+		#region Private methods
+
+		private void DataChanged()
+		{
+			if (Data != null)
+			{
+				Maximum = Data.Interpreter.Maximum();
+				Minimum = Data.Interpreter.Minimum();
+				RMS = Data.Interpreter.RMS();
+			}
+			else
+			{
+				Maximum = 0;
+				Minimum = 0;
+				RMS = 0;
+			}
+		}
+
+		#endregion
+
+		#region Public methods
+
+		/// <summary>
+		/// Copies all contents of <paramref name="signalInformation"/> to this object.
+		/// </summary>
+		/// <param name="signalInformation"></param>
+		public void Copy(ISignalInformation signalInformation)
+		{
+			if(signalInformation == null)
+			{
+				throw new ArgumentNullException(nameof(signalInformation));
+			}
+
+			Data = signalInformation.Data;
+
+			Maximum = signalInformation.Maximum;
+
+			Minimum = signalInformation.Minimum;
+
+			RMS = signalInformation.RMS;
+
+			InvertedDirection = signalInformation.InvertedDirection;
+		}
+
+		/// <summary>
+		/// Returns a copy of this instance
+		/// </summary>
+		/// <returns></returns>
+		ISignalInformation IDeepCopyTo<ISignalInformation>.Copy() => CopySignalInformation();
+
+		/// <summary>
+		/// Returns a copy of this instance
+		/// </summary>
+		/// <returns></returns>
+		public SignalInformation CopySignalInformation() =>	new SignalInformation(this);
+
+		#endregion
 	}
 }
