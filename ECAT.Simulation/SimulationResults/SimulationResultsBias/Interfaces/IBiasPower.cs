0d20723ebc02122af1f4cc5ad02222a8976ef31d
Szymon Żogała <szymek.655@gmail.com>
2018-09-14 13:20:40 +0200

    Added

---
+++ ECAT.Simulation/SimulationResults/SimulationResultsBias/Interfaces/IBiasPower.cs
@@ -0,0 +1,13 @@
+﻿namespace ECAT.Simulation
+{
+	public partial class SimulationResultsBias
+	{
+		/// <summary>
+		/// Interface for classes capable of computing and caching power dissipated/delivered by components
+		/// </summary>
+		private interface IBiasPower
+		{
+
+		}
+	}
+}
---
+++ ECAT.Simulation/SimulationResults/SimulationResultsBias/Interfaces/IBiasVoltage.cs
@@ -0,0 +1,13 @@
+﻿namespace ECAT.Simulation
+{
+	public partial class SimulationResultsBias
+	{
+		/// <summary>
+		/// Interface for classes capable of computing and caching voltages across components
+		/// </summary>
+		private interface IBiasVoltage
+		{
+
+		}
+	}
+}
