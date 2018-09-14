fc8044ecee996a1a6322ff9cb10210466c064eae
Szymon Żogała <szymek.655@gmail.com>
2018-09-14 14:25:32 +0200

    Added a method

--- ECAT.Core/MainFunctionalityInterfaces/Simulation/SimulationResults/IVoltageDB.cs
+++ ECAT.Core/MainFunctionalityInterfaces/Simulation/SimulationResults/IVoltageDB.cs
@@ -23,6 +23,16 @@
 		/// <returns></returns>
 		ISignalInformation GetVoltageDrop(int nodeAIndex, int nodeBIndex);
 
+		/// <summary>
+		/// Gets information on voltage drop across a <see cref="ITwoTerminal"/> component
+		/// </summary>
+		/// <param name="component"></param>
+		/// <param name="voltageBA">If true, voltage drop is calculated from <see cref="ITwoTerminal.TerminalB"/> to
+		/// <see cref="ITwoTerminal.TerminalA"/>, if false from <see cref="ITwoTerminal.TerminalA"/> to
+		/// <see cref="ITwoTerminal.TerminalB"/></param>
+		/// <returns></returns>
+		ISignalInformation GetVoltageDrop(ITwoTerminal component, bool voltageBA = true);
+
 		#endregion
 	}
 }
