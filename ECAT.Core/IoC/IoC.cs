033aeda47646a87745effed7bbdc3cc4f492c0ba
Szymon Żogała <szymek.655@gmail.com>
2018-09-15 23:32:16 +0200

    Removed no longer necessary methods and properties

--- ECAT.Core/IoC/IoC.cs
+++ ECAT.Core/IoC/IoC.cs
@@ -1,5 +1,4 @@
 ﻿using Autofac;
-using System;
 
 namespace ECAT.Core
 {
@@ -8,24 +7,8 @@ namespace ECAT.Core
 	/// </summary>
 	public static class IoC
 	{
-		#region Private static methods
-
-		/// <summary>
-		/// Returns true if all the main (necessary) interfaces for the app are registered
-		/// </summary>
-		/// <param name="container"></param>
-		/// <returns></returns>
-		private static bool NecessaryElementsRegistered(IContainer container) => container.IsRegistered<IDesignManager>();
-
-		#endregion
-
 		#region Public static properties		
-
-		/// <summary>
-		/// Indicates whether the IoC was setup
-		/// </summary>
-		public static bool Ready => Container == null;
-
+		
 		/// <summary>
 		/// Container with component registration
 		/// </summary>
@@ -34,24 +17,6 @@ namespace ECAT.Core
 		#endregion
 
 		#region Public static methods
-
-		/// <summary>
-		/// Builds the container for the given <see cref="ContainerBuilder"/>
-		/// </summary>
-		/// <param name="builder"></param>
-		public static void Build(ContainerBuilder builder)
-		{
-			var container = builder.Build();
-
-			if (NecessaryElementsRegistered(container))
-			{
-				Container = container;
-			}
-			else
-			{
-				throw new Exception("The builder container does not have all necessary interfaces registered");
-			}
-		}
 
 		/// <summary>
 		/// Calls and returns the result of <see cref="IoC.Container.Resolve{T}"/> (without getting a lifetime scope;
