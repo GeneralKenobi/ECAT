db5da75c95dc9a229efa1397ed7cc7c5899a8ee8
Szymon Żogała <szymek.655@gmail.com>
2018-09-16 21:44:54 +0200

    Added

---
+++ ECAT.Core/MainFunctionalityInterfaces/Simulation/SimulationResults/ISimulationResultsProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace ECAT.Core
+{
+	/// <summary>
+	/// Interface for classes providing results from simulation
+	/// </summary>
+	public interface ISimulationResultsProvider
+	{
+		#region Properties
+
+		/// <summary>
+		/// Results from the last simulation
+		/// </summary>
+		ISimulationResults Value { get; }
+
+		#endregion
+	}
+}
