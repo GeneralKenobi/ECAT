21a607fc9f44b218054f9f445ca0e94968d9dfe9
Szymon Żogała <szymek.655@gmail.com>
2018-09-16 22:00:13 +0200

    Added

---
+++ ECAT.Simulation/SimulationResults/SimulationResultsProvider/SimulationResultsProvider.cs
@@ -0,0 +1,20 @@
+﻿using ECAT.Core;
+
+namespace ECAT.Simulation
+{
+	/// <summary>
+	/// Provides results from the latest simulation
+	/// </summary>
+	[RegisterAsInstance(typeof(ISimulationResultsProvider))]
+	public class SimulationResultsProvider : ISimulationResultsProvider
+	{
+		#region Public methods
+
+		/// <summary>
+		/// Results from the last simulation
+		/// </summary>
+		public ISimulationResults Value { get; set; }
+
+		#endregion
+	}
+}
