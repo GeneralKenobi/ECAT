d92c7d843872c6541a5c68d7138645c888b9fb2e
Szymon Żogała <szymek.655@gmail.com>
2018-09-17 19:36:55 +0200

    Added

---
+++ ECAT.Core/MainFunctionalityInterfaces/Simulation/Signal/SignalInformation/ISignalInformationFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace ECAT.Core
+{
+	/// <summary>
+	/// Interface for factories of <see cref="ISignalInformation"/>
+	/// </summary>
+	public interface ISignalInformationFactory
+    {
+		#region Methods
+
+		/// <summary>
+		/// Constructs an <see cref="ISignalInformation"/> based on <paramref name="data"/>
+		/// </summary>
+		/// <param name="data"></param>
+		/// <returns></returns>
+		/// <exception cref="System.ArgumentNullException"></exception>
+		ISignalInformation Construct(ISignalData data);
+
+		#endregion
+	}
+}
