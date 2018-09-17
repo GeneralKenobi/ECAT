7d8ec6fe73b2c331f5befbc1dea86783e707a8a9
Szymon Żogała <szymek.655@gmail.com>
2018-09-17 19:31:57 +0200

    Now derives from two generic factory interfaces

--- ECAT.Core/MainFunctionalityInterfaces/Simulation/Signal/SignalData/SignalDataBase/ISignalDataFactory.cs
+++ ECAT.Core/MainFunctionalityInterfaces/Simulation/Signal/SignalData/SignalDataBase/ISignalDataFactory.cs
@@ -4,24 +4,10 @@
 	/// Base interface for factories of <see cref="ISignalData"/>s
 	/// </summary>
 	/// <typeparam name="T"></typeparam>
-	public interface ISignalDataFactory<T>
+	public interface ISignalDataFactory<T> : IFactoryParameterless<T>, IFactoryShallowCopying<T>
 		where T : ISignalData
     {
 		#region Methods
-
-		/// <summary>
-		/// Constructs a <see cref="T"/> equal to 0
-		/// </summary>
-		/// <returns></returns>
-		T Construct();
-
-		/// <summary>
-		/// Constructs a shallow copy of <paramref name="signal"/>
-		/// </summary>
-		/// <param name="signal"></param>
-		/// <returns></returns>
-		/// <exception cref="ArgumentNullException"></exception>
-		T Construct(T signal);
 
 		/// <summary>
 		/// Returns a negation (shallow copy) of <paramref name="signal"/>
