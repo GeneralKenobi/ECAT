c1b7dbc181a0d835e1e153ceea3ffba007237477
Szymon Żogała <szymek.655@gmail.com>
2018-09-17 15:06:58 +0200

    Added

---
+++ ECAT.Core/MainFunctionalityInterfaces/Simulation/Signal/SignalData/TimeDomainSignal/ITimeDomainSignalFactory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace ECAT.Core
+{
+	/// <summary>
+	/// Interface for factories of <see cref="ITimeDomainSignal"/>
+	/// </summary>
+	public interface ITimeDomainSignalFactory : ISignalDataFactory<ITimeDomainSignal>
+	{
+		#region Methods
+
+		/// <summary>
+		/// Constructs a new <see cref="ITimeDomainSignal"/>
+		/// </summary>
+		/// <param name="instantenousValues">Values occuring at specific time moments</param>
+		/// <param name="timeStep">Time step between two subsequent values</param>
+		/// <param name="startTime">Start time of the signal</param>
+		/// <returns></returns>
+		ITimeDomainSignal Construct(IEnumerable<double> instantenousValues, double timeStep, double startTime = 0);
+
+		#endregion
+	}
+}
