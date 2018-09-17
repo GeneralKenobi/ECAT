8325c6f62895df30dc850cb5cf9f1bc13926e6fa
Szymon Żogała <szymek.655@gmail.com>
2018-09-17 19:30:20 +0200

    Added

---
+++ ECAT.Core/GenericFactories/IFactoryDeepCopying.cs
@@ -0,0 +1,21 @@
+﻿namespace ECAT.Core.GenericFactories
+{
+	/// <summary>
+	/// Interface for factories allowing for construction of an instance of <see cref="T"/> by making a deep copy of another instance
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public interface IFactoryDeepCopying<T>
+    {
+		#region Methods
+
+		/// <summary>
+		/// Constructs a deep copy of <paramref name="source"/>
+		/// </summary>
+		/// <param name="source"></param>
+		/// <returns></returns>
+		/// <exception cref="ArgumentNullException"></exception>
+		T Construct(T source);
+
+		#endregion
+	}
+}
---
+++ ECAT.Core/GenericFactories/IFactoryParameterless.cs
@@ -0,0 +1,19 @@
+﻿namespace ECAT.Core.GenericFactories
+{
+	/// <summary>
+	/// Interface for factories allowing for parameterless construction of an instance of <see cref="T"/>
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public interface IFactoryParameterless<T>
+	{
+		#region Methods
+
+		/// <summary>
+		/// Constructs a <see cref="T"/>
+		/// </summary>
+		/// <returns></returns>
+		T Construct();
+
+		#endregion
+	}
+}
