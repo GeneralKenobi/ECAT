a15113ed4489b08e6212ef1ec2702e8ef6db552c
Szymon Żogała <szymek.655@gmail.com>
2018-09-17 12:38:49 +0200

    Added

---
+++ ECAT.Core/MainFunctionalityInterfaces/Simulation/Signal/SignalData/ISignalDataFactory.cs
@@ -0,0 +1,34 @@
+﻿namespace ECAT.Core
+{
+	/// <summary>
+	/// Base interface for factories of <see cref="ISignalData"/>s
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public interface ISignalDataFactory<T>
+		where T : ISignalData
+    {
+		#region Methods
+
+		/// <summary>
+		/// Constructs a <see cref="T"/> equal to 0
+		/// </summary>
+		/// <returns></returns>
+		T Construct();
+
+		/// <summary>
+		/// Constructs a copy of <paramref name="signal"/>
+		/// </summary>
+		/// <param name="signal"></param>
+		/// <returns></returns>
+		T Construct(T signal);
+
+		/// <summary>
+		/// Returns a negation (copy) of <paramref name="signal"/>
+		/// </summary>
+		/// <param name="signal"></param>
+		/// <returns></returns>
+		T ConstructNegation(T signal);
+
+		#endregion
+	}
+}
