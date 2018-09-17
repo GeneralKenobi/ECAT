5dfb82236f4c51570dab301696fa4e40d7228b7a
Szymon Żogała <szymek.655@gmail.com>
2018-09-17 19:44:43 +0200

    Added

---
+++ ECAT.Simulation/Signals/SignalInformation/SignalInformationFactory.cs
@@ -0,0 +1,23 @@
+﻿using ECAT.Core;
+
+namespace ECAT.Simulation
+{
+	/// <summary>
+	/// Factory of <see cref="ISignalInformation"/>s.
+	/// </summary>
+	[RegisterAsInstance(typeof(ISignalInformationFactory))]
+	public class SignalInformationFactory : ISignalInformationFactory
+    {
+		#region Public methods
+
+		/// <summary>
+		/// Constructs an <see cref="ISignalInformation"/> based on <paramref name="data"/>
+		/// </summary>
+		/// <param name="data"></param>
+		/// <returns></returns>
+		/// <exception cref="System.ArgumentNullException"></exception>
+		public ISignalInformation Construct(ISignalData data) => new SignalInformation(data);
+
+		#endregion
+	}
+}
