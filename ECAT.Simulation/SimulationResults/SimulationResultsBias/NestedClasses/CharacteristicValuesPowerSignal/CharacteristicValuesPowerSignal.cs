40779de6c19b527e9dfc4046d88026ed93db2f1d
Szymon Żogała <szymek.655@gmail.com>
2018-09-18 22:28:04 +0200

    Added

---
+++ ECAT.Simulation/SimulationResults/SimulationResultsBias/NestedClasses/CharacteristicValuesPowerSignal/CharacteristicValuesPowerSignal.cs
@@ -0,0 +1,60 @@
+﻿using ECAT.Core;
+
+namespace ECAT.Simulation
+{
+	public partial class SimulationResultsBias
+	{
+		/// <summary>
+		/// Class for power signals that can't be fully constructed, instead only some characteristic values may be computed, for
+		/// example maximum value.
+		/// </summary>
+		private class CharacteristicValuesPowerSignal : ISignalData
+		{
+			#region Constructors
+
+			/// <summary>
+			/// Default constructor
+			/// </summary>
+			public CharacteristicValuesPowerSignal()
+			{
+				Interpreter = new CharacteristicValuesPowerSignalInterpreter(this);
+			}
+
+			/// <summary>
+			/// Constructor with parameters
+			/// </summary>
+			public CharacteristicValuesPowerSignal(double maximum, double minimum, double average) : this()
+			{
+				Maximum = maximum;
+				Minimum = minimum;
+				Average = average;
+			}
+
+			#endregion
+
+			#region Public properties
+
+			/// <summary>
+			/// The maximum instantenous value
+			/// </summary>
+			public double Maximum { get; private set; }
+
+			/// <summary>
+			/// The minimum instantenous value
+			/// </summary>
+			public double Minimum { get; private set; }
+
+			/// <summary>
+			/// The average value
+			/// </summary>
+			public double Average { get; private set; }
+
+			/// <summary>
+			/// Interpreter of this <see cref="ISignalData"/>
+			/// </summary>
+			public ISignalDataInterpreter Interpreter { get; }
+
+			#endregion
+		}
+	}
+}
