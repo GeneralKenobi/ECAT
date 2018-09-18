18928858661d38b7f650740fa792de886e67ccf1
Szymon Żogała <szymek.655@gmail.com>
2018-09-18 13:58:58 +0200

    Added methods

--- ECAT.Core/MainFunctionalityInterfaces/Simulation/SimulationResults/IPowerDB.cs
+++ ECAT.Core/MainFunctionalityInterfaces/Simulation/SimulationResults/IPowerDB.cs
@@ -4,6 +4,44 @@
 	/// Interface for classes holding information about power dissipated/delivered by components
 	/// </summary>
 	public interface IPowerDB
-    {
-    }
+	{
+		#region Methods
+
+		/// <summary>
+		/// Gets information about power dissipated on an <see cref="IResistor"/>
+		/// </summary>
+		/// <param name="voltageDrop"></param>
+		/// <param name="resistor"></param>
+		/// <returns></returns>
+		IPowerInformation GetPower(IResistor resistor);
+
+		/// <summary>
+		/// Gets information about power on an <see cref="ICurrentSource"/>
+		/// </summary>
+		/// <param name="voltageDrop"></param>
+		/// <param name="currentSource"></param>
+		/// <returns></returns>
+		IPowerInformation GetPower(ICurrentSource currentSource);
+
+		/// <summary>
+		/// Gets information about power on an <see cref="IVoltageSource"/>
+		/// </summary>
+		/// <param name="current"></param>
+		/// <param name="voltageSource"></param>
+		/// <returns></returns>
+		IPowerInformation GetPower(IVoltageSource voltageSource);
+
+		/// <summary>
+		/// Gets information about power on an <see cref="IACVoltageSource"/>. If the <paramref name="current"/> is composed of
+		/// phasors with different frequency than that of <paramref name="voltageSource"/> the average power will be assigned
+		/// <see cref="Double.NaN"/> (it's impossible to calculate it using only phasors). Doesn't compute maximum/minimum
+		/// instantenous power.
+		/// </summary>
+		/// <param name="current"></param>
+		/// <param name="voltageSource"></param>
+		/// <returns></returns>
+		IPowerInformation GetPower(IACVoltageSource voltageSource);
+
+		#endregion
+	}
 }
