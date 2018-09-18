cbd838bf4233e0294073596292eafa13313489b1
Szymon Żogała <szymek.655@gmail.com>
2018-09-18 23:01:35 +0200

    Now only contains I*DB interfaces which took over specific methods

--- ECAT.Core/MainFunctionalityInterfaces/Simulation/SimulationResults/ResultsInterfaces/ISimulationResults.cs
+++ ECAT.Core/MainFunctionalityInterfaces/Simulation/SimulationResults/ResultsInterfaces/ISimulationResults.cs
@@ -6,112 +6,22 @@
 	/// </summary>
 	public interface ISimulationResults
     {
-		#region Methods
-
-		#region Voltage related
+		#region Properties
 
 		/// <summary>
-		/// Gets a voltage drop of a node with respect to ground
+		/// Contains information about power
 		/// </summary>
-		/// <param name="nodeIndex"></param>
-		/// <returns></returns>
-		bool TryGetVoltageDrop(int nodeIndex, out ISignalInformation voltageDrop);
+		IVoltageDB Voltage { get; }
 
 		/// <summary>
-		/// Gets information on voltage drop between two nodes (with node A being treated as the reference node). If the node
-		/// indexes exceed currently held nodes count null is assigned to <paramref name="voltageDrop"/> and false is returned
+		/// Contains information about power
 		/// </summary>
-		/// <param name="nodeAIndex"></param>
-		/// <param name="nodeBIndex"></param>
-		/// <returns></returns>
-		bool TryGetVoltageDrop(int nodeAIndex, int nodeBIndex, out ISignalInformation voltageDrop);
+		ICurrentDB Current { get; }
 
 		/// <summary>
-		/// Gets a voltage drop of a node with respect to ground or returns a drop equal to zero if unsuccessful
+		/// Contains information about power
 		/// </summary>
-		/// <param name="nodeIndex"></param>
-		/// <returns></returns>
-		ISignalInformation GetVoltageDropOrZero(int nodeIndex);
-
-		/// <summary>
-		/// Gets information on voltage drop between two nodes (with node A being treated as the reference node) or returns a drop
-		/// equal to zero if unsuccessful
-		/// </summary>
-		/// <param name="nodeAIndex"></param>
-		/// <param name="nodeBIndex"></param>
-		/// <returns></returns>
-		ISignalInformation GetVoltageDropOrZero(int nodeAIndex, int nodeBIndex);
-
-		#endregion
-
-		#region Current related
-
-		/// <summary>
-		/// Gets information about current flowing through an <see cref="IResistor"/>
-		/// </summary>
-		/// <param name="voltageDrop"></param>
-		/// <param name="resistor"></param>
-		/// <returns></returns>
-		ISignalInformation GetCurrent(IResistor resistor, bool reverseDirection);
-
-		/// <summary>
-		/// Gets information about current flowing through an <see cref="ICapacitor"/>
-		/// </summary>
-		/// <param name="voltageDrop"></param>
-		/// <param name="capacitor"></param>
-		/// <returns></returns>
-		ISignalInformation GetCurrent(ICapacitor capacitor, bool reverseDirection);
-
-		/// <summary>
-		/// Returns current produced by some <see cref="IActiveComponent"/>. If simulation was not yet performed or the current can't be
-		/// found returns zero
-		/// </summary>
-		/// <param name="activeComponentIndex">Index of the <see cref="IActiveComponent"/> whose current to query</param>
-		/// <param name="reverseDirection">True if the direction of current should be reversed with respect to the one given
-		/// by convention for the specific element</param>
-		/// <returns></returns>
-		ISignalInformation GetCurrentOrZero(int activeComponentIndex, bool reverseDirection);
-
-		#endregion
-
-		#region Power related
-
-		/// <summary>
-		/// Gets information about power dissipated on an <see cref="IResistor"/>
-		/// </summary>
-		/// <param name="voltageDrop"></param>
-		/// <param name="resistor"></param>
-		/// <returns></returns>
-		IPowerInformation GetPower(IResistor resistor);
-
-		/// <summary>
-		/// Gets information about power on an <see cref="ICurrentSource"/>
-		/// </summary>
-		/// <param name="voltageDrop"></param>
-		/// <param name="currentSource"></param>
-		/// <returns></returns>
-		IPowerInformation GetPower(ICurrentSource currentSource);
-
-		/// <summary>
-		/// Gets information about power on an <see cref="IVoltageSource"/>
-		/// </summary>
-		/// <param name="current"></param>
-		/// <param name="voltageSource"></param>
-		/// <returns></returns>
-		IPowerInformation GetPower(IVoltageSource voltageSource);
-
-		/// <summary>
-		/// Gets information about power on an <see cref="IACVoltageSource"/>. If the <paramref name="current"/> is composed of
-		/// phasors with different frequency than that of <paramref name="voltageSource"/> the average power will be assigned
-		/// <see cref="Double.NaN"/> (it's impossible to calculate it using only phasors). Doesn't compute maximum/minimum
-		/// instantenous power.
-		/// </summary>
-		/// <param name="current"></param>
-		/// <param name="voltageSource"></param>
-		/// <returns></returns>
-		IPowerInformation GetPower(IACVoltageSource voltageSource);
-
-		#endregion
+		IPowerDB Power { get; }
 
 		#endregion
 	}
