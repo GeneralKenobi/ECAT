3811e86ed687b486fc965a50f991a81a886e1042
Szymon Żogała <szymek.655@gmail.com>
2018-09-19 22:56:21 +0200

    Added dummy results which are returned if the assigned results are null

--- ECAT.Simulation/SimulationResults/SimulationResultsProvider/SimulationResultsProvider.cs
+++ ECAT.Simulation/SimulationResults/SimulationResultsProvider/SimulationResultsProvider.cs
@@ -7,14 +7,36 @@ namespace ECAT.Simulation
 	/// </summary>
 	// Register as self so that SimulationManager can use the public setter for Value
 	[RegisterAsInstance(typeof(SimulationResultsProvider), typeof(ISimulationResultsProvider))]
-	public class SimulationResultsProvider : ISimulationResultsProvider
+	public partial class SimulationResultsProvider : ISimulationResultsProvider
 	{
+		#region Private members
+
+		/// <summary>
+		/// Backing store for <see cref="Value"/>
+		/// </summary>
+		private ISimulationResults mValue;
+
+		#endregion
+
+		#region Private properties
+		
+		/// <summary>
+		/// Dummy results that should be returned by <see cref="Value"/> if <see cref="mValue"/> is null
+		/// </summary>
+		private ISimulationResults _DummySimulationResults { get; } = new DummySimulationResults();
+
+		#endregion
+
 		#region Public properties
 
 		/// <summary>
 		/// Results from the last simulation
 		/// </summary>
-		public ISimulationResults Value { get; set; }
+		public ISimulationResults Value
+		{
+			get => mValue ?? _DummySimulationResults;
+			set => mValue = value;
+		}
 
 		#endregion
 	}
