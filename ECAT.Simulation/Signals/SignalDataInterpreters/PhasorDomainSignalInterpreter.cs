7685cc1f96b29f8e6e81703e3ccd92c5956ff0bf
Szymon Żogała <szymek.655@gmail.com>
2018-09-20 15:46:00 +0200

    It is now a nested class of PhasorDomainSignal

--- ECAT.Simulation/Signals/SignalDataInterpreters/PhasorDomainSignalInterpreter.cs
+++ ECAT.Simulation/Signals/SignalDataInterpreters/PhasorDomainSignalInterpreter.cs
@@ -4,62 +4,65 @@ using System.Linq;
 
 namespace ECAT.Simulation
 {
-	/// <summary>
-	/// Class capable of interpreting <see cref="IPhasorDomainSignal"/> and calculating its characteristic values
-	/// </summary>
-	public class PhasorDomainSignalInterpreter : ISignalDataInterpreter
+	public partial class PhasorDomainSignal
 	{
-		#region Constructors
-
 		/// <summary>
-		/// Default constructor
+		/// Class capable of interpreting <see cref="IPhasorDomainSignal"/> and calculating its characteristic values
 		/// </summary>
-		/// <param name="Signal">Signal being interpreted</param>
-		/// <exception cref="ArgumentNullException">Thrown if <paramref name="Signal"/> is null</exception>
-		public PhasorDomainSignalInterpreter(IPhasorDomainSignal Signal)
+		private class PhasorDomainSignalInterpreter : ISignalDataInterpreter
 		{
-			_Signal = Signal ?? throw new ArgumentNullException(nameof(Signal));
+			#region Constructors
+
+			/// <summary>
+			/// Default constructor
+			/// </summary>
+			/// <param name="Signal">Signal being interpreted</param>
+			/// <exception cref="ArgumentNullException">Thrown if <paramref name="Signal"/> is null</exception>
+			public PhasorDomainSignalInterpreter(IPhasorDomainSignal Signal)
+			{
+				_Signal = Signal ?? throw new ArgumentNullException(nameof(Signal));
+			}
+
+			#endregion
+
+			#region Private properties
+
+			/// <summary>
+			/// Signal that is interpreted
+			/// </summary>
+			private IPhasorDomainSignal _Signal { get; }
+
+			#endregion
+
+			#region Public methods
+
+			/// <summary>
+			/// Calculates and returns the maximum instantenous value of the signal
+			/// </summary>
+			/// <returns></returns>
+			public double Maximum() => _Signal.DC + _Signal.Phasors.Sum((phasor) => phasor.Value.Magnitude);
+
+			/// <summary>
+			/// Calculates and returns the minimum instantenous value of the signal
+			/// </summary>
+			/// <returns></returns>
+			public double Minimum() => _Signal.DC - _Signal.Phasors.Sum((phasor) => phasor.Value.Magnitude);
+
+			/// <summary>
+			/// Calculates and returns the root-mean-square value of the signal
+			/// </summary>
+			/// <returns></returns>
+			public double RMS() =>
+				Math.Sqrt(Math.Pow(_Signal.DC, 2) + _Signal.Phasors.Sum((x) => Math.Pow(x.Value.Magnitude / Math.Sqrt(2), 2)));
+
+			/// <summary>
+			/// Returns the average value of the signal
+			/// </summary>
+			/// <returns></returns>
+			public double Average() => _Signal.DC;
+			// Average value is equal to DC (sine waves have average value equal to 0)
+
+			#endregion
 		}
-
-		#endregion
-
-		#region Private properties
-
-		/// <summary>
-		/// Signal that is interpreted
-		/// </summary>
-		private IPhasorDomainSignal _Signal { get; }
-
-		#endregion
-
-		#region Public methods
-
-		/// <summary>
-		/// Calculates and returns the maximum instantenous value of the signal
-		/// </summary>
-		/// <returns></returns>
-		public double Maximum() => _Signal.DC + _Signal.Phasors.Sum((phasor) => phasor.Value.Magnitude);
-
-		/// <summary>
-		/// Calculates and returns the minimum instantenous value of the signal
-		/// </summary>
-		/// <returns></returns>
-		public double Minimum() => _Signal.DC - _Signal.Phasors.Sum((phasor) => phasor.Value.Magnitude);
-
-		/// <summary>
-		/// Calculates and returns the root-mean-square value of the signal
-		/// </summary>
-		/// <returns></returns>
-		public double RMS() =>
-			Math.Sqrt(Math.Pow(_Signal.DC, 2) + _Signal.Phasors.Sum((x) => Math.Pow(x.Value.Magnitude / Math.Sqrt(2), 2)));
-
-		/// <summary>
-		/// Returns the average value of the signal
-		/// </summary>
-		/// <returns></returns>
-		public double Average() => _Signal.DC;
-		// Average value is equal to DC (sine waves have average value equal to 0)
-
-		#endregion
 	}
 }
