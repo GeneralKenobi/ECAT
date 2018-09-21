b9701c0f318a7698c4ebb4de8f036c323641f0ae
Szymon Żogała <szymek.655@gmail.com>
2018-09-21 20:17:46 +0200

    Renamed TypeScanPredicates method to GetTypeScanPredicates

--- ECAT.Core/Initialization/IInitializationTypeScan.cs
+++ ECAT.Core/Initialization/IInitializationTypeScan.cs
@@ -6,14 +6,14 @@ namespace ECAT.Core
 	/// <summary>
 	/// Interface for classes that want to be called at start-up to perform initialization routine. One instance is created using
 	/// <see cref="System.Activator"/> and <see cref="InitializationTypeScan(IEnumerable{Type})"/> is called using types that match
-	/// <see cref="TypeScanPredicates"/>.
+	/// <see cref="GetTypeScanPredicates"/>.
 	/// </summary>
 	public interface IInitializationTypeScan
     {
 		#region Public methods
 
 		/// <summary>
-		/// Method called at app's startup with <see cref="Type"/>s that match <see cref="TypeScanPredicates"/>
+		/// Method called at app's startup with <see cref="Type"/>s that match <see cref="GetTypeScanPredicates"/>
 		/// </summary>
 		void InitializationTypeScan(IEnumerable<Type> foundTypes);
 
@@ -22,7 +22,7 @@ namespace ECAT.Core
 		/// <see cref="InitializationTypeScan(IEnumerable{Type})"/>
 		/// </summary>
 		/// <returns></returns>
-		IEnumerable<Predicate<Type>> TypeScanPredicates();
+		IEnumerable<Predicate<Type>> GetTypeScanPredicates();
 
 		#endregion
 	}
