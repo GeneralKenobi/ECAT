b10d59a8e3b1e8a51060b78fe3591dc584efb6ac
Szymon Żogała <szymek.655@gmail.com>
2018-09-21 19:27:03 +0200

    Added

---
+++ ECAT.Core/Initialization/IInitializationRoutine.cs
@@ -0,0 +1,17 @@
+﻿namespace ECAT.Core
+{
+	/// <summary>
+	/// Interface for classes that want to be called at start-up to perform initialization routine
+	/// </summary>
+	public interface IInitializationRoutine
+    {
+		#region Public methods
+
+		/// <summary>
+		/// Method called at app's startup
+		/// </summary>
+		void InitializationRoutine();
+
+		#endregion
+	}
+}
