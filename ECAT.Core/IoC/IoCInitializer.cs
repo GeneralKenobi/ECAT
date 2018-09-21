fce3ce21181e977149f1f7d22553931692bdb63b
Szymon Żogała <szymek.655@gmail.com>
2018-09-21 20:32:44 +0200

    Added

---
+++ ECAT.Core/IoC/IoCInitializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ECAT.Core
+{
+	/// <summary>
+	/// Finds types that are used for IoC registration and calls <see cref="IoC.Build(IEnumerable{Type})"/>
+	/// </summary>
+	public class IoCInitializer : IInitializationTypeScan
+	{
+		#region Public methods
+
+		/// <summary>
+		/// Returns all types that will be used for IoC registration
+		/// </summary>
+		/// <returns></returns>
+		public IEnumerable<Predicate<Type>> GetTypeScanPredicates() => new Predicate<Type>[]
+		{
+			// All types that are marked with an IoCRegistration attribute are valid
+			(type) => Attribute.IsDefined(type, typeof(IoCRegistration)),
+			// All types that derive Autofac.Module are valid
+			(type) => typeof(Autofac.Module).IsAssignableFrom(type),
+		};
+
+		/// <summary>
+		/// Passes the valid types to <see cref="IoC.Build(IEnumerable{Type})"/>
+		/// </summary>
+		/// <param name="foundTypes"></param>
+		public void InitializationTypeScan(IEnumerable<Type> foundTypes) => IoC.Build(foundTypes);
+
+		#endregion
+	}
+}
