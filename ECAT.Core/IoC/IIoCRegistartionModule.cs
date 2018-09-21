3878385cc8eac7fc7c2dbff06173d814023d3b1f
Szymon Żogała <szymek.655@gmail.com>
2018-09-21 22:25:50 +0200

    Added

---
+++ ECAT.Core/IoC/IIoCRegistartionModule.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+
+namespace ECAT.Core
+{
+	/// <summary>
+	/// Interface for classes that wish to perform a custom registration with <see cref="IoC"/>. At start-up an instance of all
+	/// classes that implement this interface is created using an <see cref="System.Activator"/> and
+	/// <see cref="Register(ContainerBuilder)"/> method is called.
+	/// </summary>
+	public interface IIoCRegistartionModule
+	{
+		#region Public methods
+
+		/// <summary>
+		/// Method called on start-up, implementing classes should perform their registration with <paramref name="builder"/> inside it.
+		/// </summary>
+		/// <param name="builder"></param>
+		void Register(ContainerBuilder builder);
+
+		#endregion
+	}
+}
