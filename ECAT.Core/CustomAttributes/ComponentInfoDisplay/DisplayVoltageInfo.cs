233ebed7a7eea99e58b197036f7d93369534c1be
Szymon Żogała <szymek.655@gmail.com>
2018-09-21 12:36:54 +0200

    Defined public properties, constructor, added documentation

--- ECAT.Core/CustomAttributes/ComponentInfoDisplay/DisplayVoltageInfo.cs
+++ ECAT.Core/CustomAttributes/ComponentInfoDisplay/DisplayVoltageInfo.cs
@@ -1,12 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ECAT.Core
 {
-	
-    public class DisplayVoltageInfo : Attribute
+	/// <summary>
+	/// Attribute used to register an <see cref="IBaseComponent"/>'s voltage drop information between two terminals.
+	/// Using it on classes that don't implement <see cref="IBaseComponent"/> will not do anything.
+	/// <see cref="TerminalA"/> and <see cref="TerminalB"/> are names of public properties of type <see cref="ITerminal"/> and both
+	/// have to obtainable from the target type (otherwise exception will be thrown during initalization).
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+	public class DisplayVoltageInfo : Attribute
     {
+		#region Constructors
 
-    }
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public DisplayVoltageInfo(string header, string terminalA, string terminalB)
+		{
+			Header = header ?? throw new ArgumentNullException(nameof(header));
+			TerminalA = terminalA ?? throw new ArgumentNullException(nameof(terminalA));
+			TerminalB = terminalB ?? throw new ArgumentNullException(nameof(terminalB));
+		}
+
+		#endregion
+
+		#region Public properties
+
+		/// <summary>
+		/// Header to display for that info section
+		/// </summary>
+		public string Header { get; }
+
+		/// <summary>
+		/// First (reference) terminal taken for the voltage drop query (name of the property)
+		/// </summary>
+		public string TerminalA { get; }
+
+		/// <summary>
+		/// Second terminal taken for the voltage drop query (name of the property)
+		/// </summary>
+		public string TerminalB { get; }
+
+		#endregion
+	}
 }
