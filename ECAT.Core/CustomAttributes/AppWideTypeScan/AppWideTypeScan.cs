d0e1f2866518e590c68f42fe8405979d02365de3
Szymon Żogała <szymek.655@gmail.com>
2018-09-21 19:27:24 +0200

    Referenced method was renamed

--- ECAT.Core/CustomAttributes/AppWideTypeScan/AppWideTypeScan.cs
+++ ECAT.Core/CustomAttributes/AppWideTypeScan/AppWideTypeScan.cs
@@ -5,8 +5,8 @@ namespace ECAT.Core
 	/// <summary>
 	/// Base class for attributes that are used with app wide type scanning (for example for <see cref="IoC"/> registration).
 	/// All types that have this attribute (or a derived attribute) defined will be passed to
-	/// <see cref="Initialization.Initialize(System.Collections.Generic.IEnumerable{Type})"/>.
-	/// Remark: types passed to <see cref="Initialization.Initialize(System.Collections.Generic.IEnumerable{Type})"/> are not
+	/// <see cref="Initialization.Run(System.Collections.Generic.IEnumerable{Type})"/>.
+	/// Remark: types passed to <see cref="Initialization.Run(System.Collections.Generic.IEnumerable{Type})"/> are not
 	/// limited only to those with <see cref="AppWideTypeScan"/> attribute and are ultimately determined by app's Main method.
 	/// </summary>
 	public abstract class AppWideTypeScan : Attribute { }
