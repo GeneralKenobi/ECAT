bb848cde218c855438b6ff18321e112c3462f27f
Szymon Żogała <szymek.655@gmail.com>
2018-09-22 23:06:10 +0200

    Added

---
+++ ECAT.DataDisplay/ComponentInfoDisplay/NestedClasses/Interfaces/ISignalInformationResolver.cs
@@ -0,0 +1,25 @@
+﻿using ECAT.Core;
+
+namespace ECAT.DataDisplay
+{
+	public partial class ComponentInfoDisplay
+	{
+		/// <summary>
+		/// Interface for classes that resolve some signal information for base components
+		/// </summary>
+		private interface ISignalInformationResolver
+		{
+			#region Public methods
+
+			/// <summary>
+			/// Gets some signal information associated with <paramref name="target"/>
+			/// </summary>
+			/// <param name="target"></param>
+			/// <returns></returns>
+			/// <exception cref="System.ArgumentNullException"></exception>
+			ISignalInformation Get(IBaseComponent target);
+
+			#endregion
+		}
+	}
+}
