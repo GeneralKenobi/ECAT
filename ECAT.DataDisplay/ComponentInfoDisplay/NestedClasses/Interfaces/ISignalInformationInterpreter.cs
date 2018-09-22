198e50c5581fa8f3f426752a336739056f8c98e6
Szymon Żogała <szymek.655@gmail.com>
2018-09-22 23:09:25 +0200

    Added

---
+++ ECAT.DataDisplay/ComponentInfoDisplay/NestedClasses/Interfaces/ISignalInformationInterpreter.cs
@@ -0,0 +1,27 @@
+﻿using ECAT.Core;
+using System.Collections.Generic;
+
+namespace ECAT.DataDisplay
+{
+	public partial class ComponentInfoDisplay
+	{
+		/// <summary>
+		/// Interprets an <see cref="ISignalInformation"/> (converts from <see cref="ISignalInformation"/> to a sequence of strings
+		/// that can be displayed on screen)
+		/// </summary>
+		private interface ISignalInformationInterpreter
+		{
+			#region Methods
+
+			/// <summary>
+			/// Converts the <see cref="ISignalInformation"/> to a sequence of strings that can be displayed on screen.
+			/// For null <paramref name="info"/> returns an empty sequence.
+			/// </summary>
+			/// <param name="info"></param>
+			/// <returns></returns>
+			IEnumerable<string> Get(ISignalInformation info);
+
+			#endregion
+		}
+	}
+}
