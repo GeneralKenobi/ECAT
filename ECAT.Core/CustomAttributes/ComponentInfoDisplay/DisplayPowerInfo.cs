ef9d7269feaefd66162f83e174fd11bb7d9b8479
Szymon Żogała <szymek.655@gmail.com>
2018-09-23 21:55:05 +0200

    Defined

--- ECAT.Core/CustomAttributes/ComponentInfoDisplay/DisplayPowerInfo.cs
+++ ECAT.Core/CustomAttributes/ComponentInfoDisplay/DisplayPowerInfo.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ECAT.Core
 {
-	
-    public class DisplayPowerInfo : Attribute
+	/// <summary>
+	/// Attribute used to register an <see cref="IBaseComponent"/>'s power dissipation information.
+	/// Using it on classes that don't implement <see cref="IBaseComponent"/> will not do anything.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+	public class DisplayPowerInfo : DisplayInfo
 	{
+		#region Constructors
 
-    }
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		/// <param name="header">Header displayed above the info section</param>
+		/// <param name="sectionIndex">Final position of the section, nonnegative, default value is <see cref="int.MaxValue"/></param>
+		/// <exception cref="ArgumentNullException"></exception>
+		/// <exception cref="ArgumentOutOfRangeException"></exception>
+		public DisplayPowerInfo(string header = "Power", int sectionIndex = int.MaxValue) : base(sectionIndex, header) { }
+
+		#endregion
+	}
 }
