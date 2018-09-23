c4511172194bd32f35634cf5cfb6c0a5d8114be9
Szymon Żogała <szymek.655@gmail.com>
2018-09-23 12:35:30 +0200

    Added

---
+++ ECAT.DataDisplay/ComponentInfoDisplay/NestedClasses/VoltageInfo/VoltageInfoInterpreter.cs
@@ -0,0 +1,20 @@
+﻿namespace ECAT.DataDisplay
+{
+	public partial class ComponentInfoDisplay
+	{
+		/// <summary>
+		/// Interprets signals related to voltage
+		/// </summary>
+		private class VoltageInfoInterpreter : GenericSignalInformationInterpreter
+		{
+			#region Constructors
+
+			/// <summary>
+			/// Default constructor
+			/// </summary>
+			public VoltageInfoInterpreter() : base("Voltage", "V") { }
+
+			#endregion
+		}
+	}
+}
