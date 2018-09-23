236ab44daddc959118d9c7106570871f8f0331fe
Szymon Żogała <szymek.655@gmail.com>
2018-09-23 23:23:57 +0200

    Added

---
+++ ECAT.DataDisplay/ComponentInfoDisplay/NestedClasses/CurrentInfo/CurrentInfoInterpreter.cs
@@ -0,0 +1,20 @@
+﻿namespace ECAT.DataDisplay
+{
+	public partial class ComponentInfoDisplay
+	{
+		/// <summary>
+		/// Interprets signals related to current
+		/// </summary>
+		private class CurrentInfoInterpreter : GenericSignalInformationInterpreter
+		{
+			#region Constructors
+
+			/// <summary>
+			/// Default constructor
+			/// </summary>
+			public CurrentInfoInterpreter() : base("Current", "A") { }
+
+			#endregion
+		}
+	}
+}
