83b76419a862db01128116f7d77ced4a4f30a8a7
Szymon Żogała <szymek.655@gmail.com>
2018-09-23 23:22:14 +0200

    Added

---
+++ ECAT.DataDisplay/ComponentInfoDisplay/NestedClasses/CurrentInfo/CurrentInfoResolver.cs
@@ -0,0 +1,53 @@
+﻿using CSharpEnhanced.CoreClasses;
+using ECAT.Core;
+using System;
+
+namespace ECAT.DataDisplay
+{
+	public partial class ComponentInfoDisplay
+	{
+		/// <summary>
+		/// Resolves currents on components
+		/// </summary>
+		private class CurrentInfoResolver : ResolverBase
+		{
+			#region Constructors
+
+			/// <summary>
+			/// Default constructor
+			/// </summary>
+			/// <param name="targetType"></param>
+			/// <exception cref="ArgumentNullException"></exception>
+			/// <exception cref="ArgumentException"></exception>
+			public CurrentInfoResolver(Type targetType) : base(targetType) { }
+
+			#endregion			
+
+			#region Public Methods
+
+			/// <summary>
+			/// Gets a current flow
+			/// </summary>
+			/// <param name="target"></param>
+			/// <returns></returns>
+			protected override ISignalInformation GetSignalInformation(IBaseComponent target)
+			{
+				// Get currents
+				var results = IoC.Resolve<ISimulationResultsProvider>().Value.Current;
+
+				// The info to get
+				ISignalInformation info = null;
+
+				// Get the results from provider and return its return value (lazy cases because we operate on interfaces)
+				var typeSwitch = new TypeSwitch().
+					LazyCase<IResistor>((x) => info = results.Get(x, false)).
+					LazyCase<IResistor>((x) => info = results.Get(x, false)).
+					LazyCase<IActiveComponent>((x) => info = results.Get(x.ActiveComponentIndex, false));
+
+				return info;
+			}
+
+			#endregion
+		}
+	}
+}
