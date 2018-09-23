dc9c1bdc81e6328bf3d1124263cc8ed071793115
Szymon Żogała <szymek.655@gmail.com>
2018-09-23 21:53:44 +0200

    Defined

--- ECAT.Core/CustomAttributes/ComponentInfoDisplay/DisplayCurrentInfo.cs
+++ ECAT.Core/CustomAttributes/ComponentInfoDisplay/DisplayCurrentInfo.cs
@@ -1,12 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ECAT.Core
 {
-	
-    public class DisplayCurrentInfo : Attribute
+	/// <summary>
+	/// Attribute used to register an <see cref="IBaseComponent"/>'s current flow information. It is aimed for all components that only
+	/// have one notable current flow (all <see cref="ITwoTerminal"/>, <see cref="IOpAmp"/> (only output current)). Components that
+	/// have more (eg. transistors) should use use <see cref="DisplayCurrentInfoCustom"/>.
+	/// Using it on classes that don't implement <see cref="IBaseComponent"/> will not do anything.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+	public class DisplayCurrentInfo : DisplayInfo
 	{
+		#region Constructors
 
-    }
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		/// <param name="header">Header displayed above the info section</param>
+		/// <param name="sectionIndex">Final position of the section, nonnegative, default value is <see cref="int.MaxValue"/></param>
+		/// <exception cref="ArgumentNullException"></exception>
+		/// <exception cref="ArgumentOutOfRangeException"></exception>
+		public DisplayCurrentInfo(string header = "Current", int sectionIndex = int.MaxValue) : base(sectionIndex, header) { }
+
+		#endregion
+	}
 }
