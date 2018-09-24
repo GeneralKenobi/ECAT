f591bc811dc1862d161aed7e137106fdc5a384fb
Szymon Żogała <szymek.655@gmail.com>
2018-09-24 22:30:03 +0200

    Added

---
+++ ECAT.Core/GlobalEventArgs/FocusedComponentChangedEventArgs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ECAT.Core
+{
+	/// <summary>
+	/// EventArgs for events signaling that focused component changed
+	/// </summary>
+	public class FocusedComponentChangedEventArgs : EventArgs
+    {
+		#region Constructors
+
+		/// <summary>
+		/// Default constructor, requires parameters
+		/// </summary>
+		/// <param name="lostFocus"></param>
+		/// <param name="gettingFocus"></param>
+		public FocusedComponentChangedEventArgs(IBaseComponent lostFocus, IBaseComponent gettingFocus)
+		{
+			LostFocus = lostFocus;
+			GettingFocus = gettingFocus;
+		}
+		
+		#endregion
+
+		#region Public properties
+
+		/// <summary>
+		/// Component that is losing focus or null if no component was focused
+		/// </summary>
+		public IBaseComponent LostFocus { get; }
+
+		/// <summary>
+		/// Component that will get focus or null if no component will be focused
+		/// </summary>
+		public IBaseComponent GettingFocus { get; }
+
+		#endregion
+	}
+}
