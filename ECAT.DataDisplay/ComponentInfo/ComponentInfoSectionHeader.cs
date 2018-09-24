1d982261e644647e6b2b41dfa940c270bfaa02c9
Szymon Żogała <szymek.655@gmail.com>
2018-09-24 22:58:20 +0200

    Added (copy from ECAT.Design project, whose copy will be deprecated)

---
+++ ECAT.DataDisplay/ComponentInfo/ComponentInfoSectionHeader.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using ECAT.Core;
+
+namespace ECAT.DataDisplay
+{
+	public partial class ComponentInfoProvider
+	{
+		/// <summary>
+		/// Standard implementation of <see cref="IComponentInfoSectionHeader"/>
+		/// </summary>
+		private class ComponentInfoSectionHeader : IComponentInfoSectionHeader
+		{
+			#region Constructors
+
+			/// <summary>
+			/// Default constructor
+			/// </summary>
+			public ComponentInfoSectionHeader(int index, string text)
+			{
+				Index = index;
+				Text = text;
+			}
+
+			#endregion
+
+			#region Events
+
+			/// <summary>
+			/// Event fired whenever a property changes its value
+			/// </summary>
+			public event PropertyChangedEventHandler PropertyChanged;
+
+			#endregion
+
+			#region Public properties
+
+			/// <summary>
+			/// Zero-based index
+			/// </summary>
+			public int Index { get; }
+
+			/// <summary>
+			/// Text to display
+			/// </summary>
+			public string Text { get; }
+
+			/// <summary>
+			/// True if this header's section is selected
+			/// </summary>
+			public bool IsSelected { get; set; }
+
+			#endregion
+		}
+	}
+}
