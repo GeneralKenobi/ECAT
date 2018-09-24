854aa68afc8d46dc6dfba2317ec088e8bc8477c0
Szymon Żogała <szymek.655@gmail.com>
2018-09-24 13:11:47 +0200

    Added

---
+++ ECAT.DataDisplay/ComponentInfoDisplay/NestedClasses/Initialization/PowerDisplayInitializer.cs
@@ -0,0 +1,37 @@
+﻿using ECAT.Core;
+using System;
+
+namespace ECAT.DataDisplay
+{
+	public partial class ComponentInfoDisplay
+	{
+		/// <summary>
+		/// Finds all types with <see cref="DisplayPowerInfo"/> attribute and attempts to construct <see cref="InfoSectionDefinition"/>
+		/// based on them and add those to <see cref="ComponentInfoDisplay"/> singleton resolved from <see cref="IoC"/>
+		/// </summary>
+		private class PowerDisplayInitializer : InitializerBase<DisplayPowerInfo>
+		{
+			#region Protected methods
+
+			/// <summary>
+			/// Constructs a <see cref="InfoSectionDefinition"/> based on <paramref name="type"/> and <paramref name="attribute"/>
+			/// </summary>
+			/// <param name="type"></param>
+			/// <param name="attribute"></param>
+			/// <param name="infoSection"></param>
+			/// <returns></returns>
+			protected override bool TryConstructInfoSection(Type type, DisplayPowerInfo attribute, out InfoSectionDefinition infoSection)
+			{
+				infoSection = new InfoSectionDefinition(
+					new PowerInfoResolver(type),
+					new PowerInfoInterpreter(),
+					attribute.SectionIndex);
+
+				// InfoSectionDefinition for power can always be constructed
+				return true;
+			}
+
+			#endregion
+		}
+	}
+}
