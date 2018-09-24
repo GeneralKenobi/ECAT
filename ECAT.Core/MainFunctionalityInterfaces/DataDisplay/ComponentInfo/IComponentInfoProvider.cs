03f33ee03c11d48ecadf73e9353b496ff1507664
Szymon Żogała <szymek.655@gmail.com>
2018-09-24 23:06:03 +0200

    Added

---
+++ ECAT.Core/MainFunctionalityInterfaces/DataDisplay/ComponentInfo/IComponentInfoProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace ECAT.Core
+{
+	/// <summary>
+	/// Interface for a class providing component info to display
+	/// </summary>
+	public interface IComponentInfoProvider
+	{
+		#region Properties
+
+		/// <summary>
+		/// The info to present
+		/// </summary>
+		IComponentInfo Value { get; }
+
+		#endregion
+	}
+}
