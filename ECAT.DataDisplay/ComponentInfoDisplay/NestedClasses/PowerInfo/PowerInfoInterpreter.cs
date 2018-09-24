801a0f15e6a544f5626116177aebe7a7fa22ba9f
Szymon Żogała <szymek.655@gmail.com>
2018-09-24 13:10:19 +0200

    Added

---
+++ ECAT.DataDisplay/ComponentInfoDisplay/NestedClasses/PowerInfo/PowerInfoInterpreter.cs
@@ -0,0 +1,22 @@
+﻿using ECAT.Core;
+
+namespace ECAT.DataDisplay
+{
+	public partial class ComponentInfoDisplay
+	{
+		/// <summary>
+		/// Interprets signals related to 
+		/// </summary>
+		private class PowerInfoInterpreter : GenericSignalInformationInterpreter
+		{
+			#region Constructors
+
+			/// <summary>
+			/// Default constructor
+			/// </summary>
+			public PowerInfoInterpreter() : base(IoC.Resolve<IQuantityNames>().PowerCap, IoC.Resolve<ISIUnits>().PowerShort) { }
+
+			#endregion
+		}
+	}
+}
