96faaed1076e5d55a22e869937a9d0a4061acd36
Szymon Żogała <szymek.655@gmail.com>
2018-09-25 23:14:53 +0200

    Now inherits from DisplayInfo, added constructor, documentation.

--- ECAT.Core/CustomAttributes/ComponentInfoDisplay/DisplayCurrentInfoTransistor.cs
+++ ECAT.Core/CustomAttributes/ComponentInfoDisplay/DisplayCurrentInfoTransistor.cs
@@ -1,10 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ECAT.Core
+﻿namespace ECAT.Core
 {
-    public class DisplayCurrentInfoCustom
+	/// <summary>
+	/// Registers custom current display, for components that have more than 1 meaningful current flow.
+	/// </summary>
+	public class DisplayCurrentInfoCustom : DisplayInfo
     {
-    }
+		#region Constructors
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public DisplayCurrentInfoCustom(int sectionIndex, string header) : base(sectionIndex, header)
+		{
+
+		}
+
+		#endregion
+	}
 }
