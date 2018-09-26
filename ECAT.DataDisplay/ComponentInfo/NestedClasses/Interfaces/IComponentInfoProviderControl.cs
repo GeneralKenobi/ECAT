5b5e2e807065abdb5f8dea6c1defc31bb55113a3
Szymon Żogała <szymek.655@gmail.com>
2018-09-26 13:50:29 +0200

    Added

---
+++ ECAT.DataDisplay/ComponentInfo/NestedClasses/Interfaces/IComponentInfoProviderControl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ECAT.DataDisplay
+{
+	partial class ComponentInfoProvider
+	{
+		/// <summary>
+		/// Interface used to control behaviour of <see cref="Core.IComponentInfoProvider"/>
+		/// </summary>
+		private interface IComponentInfoProviderControl
+		{
+			#region Methods
+
+			/// Adds new info sections to. Each type has to implement <see cref="IBaseComponent"/>
+			/// (otherwise an exception is thrown).
+			void AddInfoSections(IEnumerable<KeyValuePair<Type, IEnumerable<InfoSectionDefinition>>> sections);
+
+			#endregion
+		}
+	}
+}
