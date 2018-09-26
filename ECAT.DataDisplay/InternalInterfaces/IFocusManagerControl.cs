411fbaeb9b8841e55529359f230c79fafaf4e8e6
Szymon Żogała <szymek.655@gmail.com>
2018-09-26 12:38:44 +0200

    Added

---
+++ ECAT.DataDisplay/InternalInterfaces/IFocusManagerControl.cs
@@ -0,0 +1,29 @@
+﻿using ECAT.Core;
+
+namespace ECAT.DataDisplay
+{
+	/// <summary>
+	/// Control interface for functionalities of <see cref="IFocusManager"/>
+	/// </summary>
+	internal interface IFocusManagerControl
+	{
+		#region Methods
+
+		/// <summary>
+		/// Sets focus to <paramref name="component"/>
+		/// </summary>
+		/// <param name="component"></param>
+		/// <exception cref="System.ArgumentNullException"></exception>
+		void SetFocus(IBaseComponent component);
+
+		/// <summary>
+		/// Loses focus of <paramref name="component"/> (<paramref name="component"/> has to match the currently focused component,
+		/// otherwise focus won't be lost).
+		/// </summary>
+		/// <param name="component"></param>
+		/// <exception cref="System.ArgumentNullException"></exception>
+		void LoseFocus(IBaseComponent component);
+
+		#endregion
+	}
+}
