5aaa73e36941c8003c33cd03c980e80b088e4a66
Szymon Żogała <szymek.655@gmail.com>
2018-09-28 11:51:30 +0200

    Added support for voltage drops from ground to terminal

--- ECAT.DataDisplay/ComponentInfo/NestedClasses/VoltageInfo/VoltageInfoResolver.cs
+++ ECAT.DataDisplay/ComponentInfo/NestedClasses/VoltageInfo/VoltageInfoResolver.cs
@@ -14,7 +14,7 @@ namespace ECAT.DataDisplay
 			#region Constructors
 
 			/// <summary>
-			/// Default constructor
+			/// Constructor for two-terminal voltage drop resolver
 			/// </summary>
 			/// <param name="targetType"></param>
 			/// <param name="terminalA"></param>
@@ -37,9 +37,38 @@ namespace ECAT.DataDisplay
 				}
 			}
 
+			/// <summary>
+			/// Constructor for voltage drop from ground to terminal resolver
+			/// </summary>
+			/// <param name="targetType"></param>
+			/// <param name="terminalA"></param>
+			/// <param name="terminalB"></param>
+			/// <exception cref="ArgumentNullException"></exception>
+			/// <exception cref="ArgumentException"></exception>
+			public VoltageInfoResolver(Type targetType, PropertyInfo terminal) : base(targetType)
+			{
+				// Check if given property infos are correct
+				if (IsPropertyInfoValid(terminal ?? throw new ArgumentNullException(nameof(terminal)), targetType))
+				{
+					_TerminalA = null;
+					_TerminalB = terminal;
+				}
+				else
+				{
+					throw new ArgumentException("Property informations doesn't have a proper return type (" + typeof(ITerminal).FullName +
+						") or don't match target type (" + targetType.FullName + ")");
+				}
+			}
+
 			#endregion
 
 			#region Private properties
+
+			/// <summary>
+			/// True if voltage drop should be resolved from <see cref="_TerminalB"/> to ground. False if voltage drop should be
+			/// resolved from <see cref="_TerminalB"/> to <see cref="_TerminalA"/>.
+			/// </summary>
+			private bool _ResolveFromGround => _TerminalA == null;
 
 			/// <summary>
 			/// Property info of first (reference) terminal
@@ -53,7 +82,7 @@ namespace ECAT.DataDisplay
 
 			#endregion
 
-			#region Private properties
+			#region Private methods
 
 			/// <summary>
 			/// Returns true if <paramref name="info"/> is a valid property info that matches the <paramref name="targetType"/>
@@ -75,12 +104,24 @@ namespace ECAT.DataDisplay
 			/// <returns></returns>
 			protected override ISignalInformation GetSignalInformation(IBaseComponent target)
 			{
-				// At this point type is guaranteed to have a property given by both property infos with ITerminal return type
-				var terminalAValue = (ITerminal)_TerminalA.GetValue(target);
+				// At this point type is guaranteed to have a property given by _TerminalB with ITerminal return type
 				var terminalBValue = (ITerminal)_TerminalB.GetValue(target);
 
-				// Get the results from provider and return its return value
-				return IoC.Resolve<ISimulationResultsProvider>().Value.Voltage.Get(terminalAValue.NodeIndex, terminalBValue.NodeIndex);
+				if (_ResolveFromGround)
+				{
+					// Resolve from ground to _TerminalB
+					return IoC.Resolve<ISimulationResultsProvider>().Value.Voltage.Get(terminalBValue.NodeIndex);
+				}
+				else
+				{
+					// Resolve from _TerminalA to _TerminalB
+
+					// At this point type is guaranteed to have a property given by _TerminalA with ITerminal return type
+					var terminalAValue = (ITerminal)_TerminalA.GetValue(target);
+
+					// Get the results from provider and return its return value
+					return IoC.Resolve<ISimulationResultsProvider>().Value.Voltage.Get(terminalAValue.NodeIndex, terminalBValue.NodeIndex);
+				}
 			}
 
 			#endregion
