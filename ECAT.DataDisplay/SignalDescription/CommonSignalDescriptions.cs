8335887d1355e48379a72d3c33ce42b8c5ba0533
Szymon Żogała <szymek.655@gmail.com>
2018-09-30 22:12:59 +0200

    Added

---
+++ ECAT.DataDisplay/SignalDescription/CommonSignalDescriptions.cs
@@ -0,0 +1,30 @@
+﻿using ECAT.Core;
+
+namespace ECAT.DataDisplay
+{
+	/// <summary>
+	/// Contains descriptions of common signals
+	/// </summary>
+	[RegisterAsInstance(typeof(ICommonSignalDescriptions))]
+	internal class CommonSignalDescriptions : ICommonSignalDescriptions
+    {
+		#region Public properties
+
+		/// <summary>
+		/// Description of a voltage signal
+		/// </summary>
+		public ISignalDescription Voltage { get; } = new SignalDescription("volt", "Volt", "V", "voltage", "Voltage");
+
+		/// <summary>
+		/// Description of a current signal
+		/// </summary>
+		public ISignalDescription Current { get; } = new SignalDescription("ampere", "Ampere", "A", "current", "Current");
+
+		/// <summary>
+		/// Description of a power signal
+		/// </summary>
+		public ISignalDescription Power { get; } = new SignalDescription("watt", "Watt", "W", "power", "Power");
+
+		#endregion
+	}
+}
