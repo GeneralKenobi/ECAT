88be78364035111faee7d6721780f88b06971034
Szymon Żogała <szymek.655@gmail.com>
2018-09-30 19:20:46 +0200

    Added

---
+++ ECAT.Core/MainFunctionalityInterfaces/Simulation/Signal/SignalData/ISignalDescription/ISignalDescription.cs
@@ -0,0 +1,37 @@
+﻿namespace ECAT.Core
+{
+	/// <summary>
+	/// Interface for classes providing description of an <see cref="ISignalInformation"/> (name, unit, etc).
+	/// </summary>
+	public interface ISignalDescription
+    {
+		#region Properties
+
+		/// <summary>
+		/// Full name of the unit of this signal (eg. "ampere")
+		/// </summary>
+		string Unit { get; }
+
+		/// <summary>
+		/// Full name of the unit of this signal with first letter capitilized (eg. "Ampere")
+		/// </summary>
+		string UnitCap { get; }
+
+		/// <summary>
+		/// Short name of the unit of this signal (eg. "A")
+		/// </summary>
+		string UnitShort { get; }
+
+		/// <summary>
+		/// Name of the signal (eg. "current")
+		/// </summary>
+		string Name { get; }
+
+		/// <summary>
+		/// Name of the signal with first letter capitilized (eg. "Current")
+		/// </summary>
+		string NameCap { get; }
+
+		#endregion
+	}
+}
