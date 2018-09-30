3e4ca76259d83fb86a16173bc8e7d7da4ded2f54
Szymon Żogała <szymek.655@gmail.com>
2018-09-30 23:07:22 +0200

    Added

---
+++ ECAT.UWP/Design/ComponentInfo/ValueConverters/SignalDataToViewModelConverter.cs
@@ -0,0 +1,50 @@
+﻿using CSharpEnhanced.CoreClasses;
+using ECAT.Core;
+using ECAT.ViewModel;
+using System;
+using Windows.UI.Xaml.Data;
+
+namespace ECAT.UWP
+{
+	/// <summary>
+	/// Converter for <see cref="ComponentInfoUC"/>, converts specific <see cref="ISignalData"/> to viewmodels
+	/// </summary>
+	public class SignalDataToViewModelConverter : IValueConverter
+    {
+		#region Public methods
+
+		/// <summary>
+		/// Converts specific <see cref="ISignalData"/> to viewmodels
+		/// </summary>
+		/// <param name="value"></param>
+		/// <param name="targetType"></param>
+		/// <param name="parameter"></param>
+		/// <param name="language"></param>
+		/// <returns></returns>
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			object result = null;
+
+			TypeSwitch.Construct().
+				LazyCase<IPhasorDomainSignal>((x) => result = new PhasorDomainSignalViewModel(x, IoC.Resolve<ISIUnits>().VoltageShort)).
+				Switch(value);
+
+			return result;
+		}
+
+		/// <summary>
+		/// Not implemented
+		/// </summary>
+		/// <param name="value"></param>
+		/// <param name="targetType"></param>
+		/// <param name="parameter"></param>
+		/// <param name="language"></param>
+		/// <returns></returns>
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+}
