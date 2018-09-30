f60b0004c4120c81e21b8d170c3a7a9af37fea5a
Szymon Żogała <szymek.655@gmail.com>
2018-09-30 21:05:13 +0200

    Added

---
+++ ECAT.Core/MainFunctionalityInterfaces/Simulation/Signal/SignalData/ISignalDescription/ICommonSignalDescriptions.cs
@@ -0,0 +1,27 @@
+﻿namespace ECAT.Core
+{
+	/// <summary>
+	/// Interface for classes containing <see cref="ISignalDescription"/>s for common signals
+	/// </summary>
+	public interface ICommonSignalDescriptions
+    {
+		#region Public properties
+
+		/// <summary>
+		/// Description of a voltage signal
+		/// </summary>
+		ISignalDescription Voltage { get; }
+
+		/// <summary>
+		/// Description of a current signal
+		/// </summary>
+		ISignalDescription Current { get; }
+
+		/// <summary>
+		/// Description of a power signal
+		/// </summary>
+		ISignalDescription Power { get; }
+
+		#endregion
+	}
+}
