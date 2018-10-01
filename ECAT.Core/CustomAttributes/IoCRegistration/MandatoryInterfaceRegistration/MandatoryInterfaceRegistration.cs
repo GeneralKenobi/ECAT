ebc980ffeb3a565e08e871b83fb88a8a884a58ba
Szymon Żogała <szymek.655@gmail.com>
2018-10-01 12:48:20 +0200

    Added

---
+++ ECAT.Core/CustomAttributes/IoCRegistration/MandatoryInterfaceRegistration/MandatoryInterfaceRegistration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace ECAT.Core
+{
+	/// <summary>
+	/// Attribute used to mark interfaces that have to have an implementation registered with <see cref="IoC"/>.
+	/// Using it allows to verify at start-up if all necessary services are registered instead of crashing sometime later when
+	/// some service cannot be resolved.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Interface, AllowMultiple = false, Inherited = false)]
+	public class MandatoryInterfaceRegistration : Attribute { }
+}
