48aeed4de90ef192248ec0856f92584a2f73a00a
Szymon Żogała <szymek.655@gmail.com>
2018-10-05 09:23:08 +0200

    Added

---
+++ ECAT.Simulation/SimulationResults/SimulationResultsTime/SimulationResultsTime.cs
@@ -0,0 +1,29 @@
+﻿using ECAT.Core;
+
+namespace ECAT.Simulation
+{
+	/// <summary>
+	/// Manages results of time domain simulations
+	/// </summary>
+	public class SimulationResultsTime : ISimulationResults
+	{
+		#region Properties
+
+		/// <summary>
+		/// Contains information about power, guaranteed to be not null
+		/// </summary>
+		public IVoltageDB Voltage { get; }
+
+		/// <summary>
+		/// Contains information about power, guaranteed to be not null
+		/// </summary>
+		public ICurrentDB Current { get; }
+
+		/// <summary>
+		/// Contains information about power, guaranteed to be not null
+		/// </summary>
+		public IPowerDB Power { get; }
+
+		#endregion
+	}
+}
