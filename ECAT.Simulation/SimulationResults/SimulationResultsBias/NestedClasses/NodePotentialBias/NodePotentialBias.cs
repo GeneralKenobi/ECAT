43a2168545a1850ce8f9ec31b53d158607e0c663
Szymon Żogała <szymek.655@gmail.com>
2018-10-06 22:52:32 +0200

    Now implements INodePotentialBiasControl (extended INodePotentialBias)

--- ECAT.Simulation/SimulationResults/SimulationResultsBias/NestedClasses/NodePotentialBias/NodePotentialBias.cs
+++ ECAT.Simulation/SimulationResults/SimulationResultsBias/NestedClasses/NodePotentialBias/NodePotentialBias.cs
@@ -11,7 +11,7 @@ namespace ECAT.Simulation
 		/// Class used to store potential on a node after bias simulation
 		/// </summary>
 		[RegisterAsType(typeof(INodePotentialBias))]
-		private class NodePotentialBias : INodePotentialBias
+		private class NodePotentialBias : INodePotentialBiasControl
 		{
 			#region Constructors
 
@@ -24,8 +24,17 @@ namespace ECAT.Simulation
 			public NodePotentialBias(double dc, IDictionary<double, Complex> phasors)
 			{
 				DC = dc;
-				Phasors = phasors ?? throw new ArgumentNullException(nameof(phasors));
+				_Phasors = new Dictionary<double, Complex>(phasors ?? throw new ArgumentNullException(nameof(phasors)));
 			}
+
+			#endregion
+
+			#region Private properties
+
+			/// <summary>
+			/// Backing store for <see cref="Phasors"/>
+			/// </summary>
+			private Dictionary<double, Complex> _Phasors { get; } = new Dictionary<double, Complex>();
 
 			#endregion
 
@@ -35,12 +44,29 @@ namespace ECAT.Simulation
 			/// AC potentials present at the node with respect to ground. Item1 (double) refers
 			/// to the frequency of the source generating the potential and Item2 (Complex) to the value of the potential.
 			/// </summary>		
-			public IDictionary<double, Complex> Phasors { get; } = new Dictionary<double, Complex>();
+			public IDictionary<double, Complex> Phasors => _Phasors;
 
 			/// <summary>
 			/// The DC potential of the node with respect to ground
 			/// </summary>
-			public double DC { get; }
+			public double DC { get; private set; }
+
+			#endregion
+
+			#region Public methods
+
+			/// <summary>
+			/// Used to set the value of <see cref="INodePotentialBias.DC"/> property
+			/// </summary>
+			/// <param name="dc"></param>
+			public void SetDC(double dc) => DC = dc;
+
+			/// <summary>
+			/// Used to add <see cref="KeyValuePair{TKey, TValue}"/> to <see cref="INodePotentialBias.Phasors"/>
+			/// </summary>
+			/// <param name="frequency"></param>
+			/// <param name="value"></param>
+			public void AddPhasor(double frequency, Complex value) => _Phasors.Add(frequency, value);
 
 			#endregion
 		}
