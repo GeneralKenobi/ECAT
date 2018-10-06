1fec5ca092edab361ece0112a92129cdd2787aa5
Szymon Żogała <szymek.655@gmail.com>
2018-10-06 17:07:09 +0200

    Added

---
+++ ECAT.Simulation/Services/INodePotentialBias.cs
@@ -0,0 +1,30 @@
+﻿using ECAT.Core;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace ECAT.Simulation
+{
+	/// <summary>
+	/// Class used to store potential on a node after bias simulation
+	/// </summary>
+	[NecessaryService]
+	[ConstructorDeclaration(new Type[] { typeof(double), typeof(IDictionary<double, Complex>)}, "DC", "AC Phasors")]
+	internal interface INodePotentialBias
+    {
+		#region Properties
+
+		/// <summary>
+		/// AC potentials present at the node with respect to ground. Item1 (double) refers
+		/// to the frequency of the source generating the potential and Item2 (Complex) to the value of the potential.
+		/// </summary>		
+		IDictionary<double, Complex> Phasors { get; }
+
+		/// <summary>
+		/// The DC potential of the node with respect to ground
+		/// </summary>
+		double DC { get; }
+
+		#endregion
+	}
+}
---
+++ ECAT.Simulation/SimulationResults/SimulationResultsBias/NestedClasses/NodePotentialBias/NodePotentialBias.cs
@@ -0,0 +1,48 @@
+﻿using ECAT.Core;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace ECAT.Simulation
+{
+	partial class SimulationResultsBias
+	{
+		/// <summary>
+		/// Class used to store potential on a node after bias simulation
+		/// </summary>
+		[RegisterAsType(typeof(INodePotentialBias))]
+		private class NodePotentialBias : INodePotentialBias
+		{
+			#region Constructors
+
+			/// <summary>
+			/// Default constructor
+			/// </summary>
+			/// <param name="dc"></param>
+			/// <param name="phasors"></param>
+			/// <exception cref="ArgumentNullException"></exception>
+			public NodePotentialBias(double dc, IDictionary<double, Complex> phasors)
+			{
+				DC = dc;
+				Phasors = phasors ?? throw new ArgumentNullException(nameof(phasors));
+			}
+
+			#endregion
+
+			#region Public properties
+
+			/// <summary>
+			/// AC potentials present at the node with respect to ground. Item1 (double) refers
+			/// to the frequency of the source generating the potential and Item2 (Complex) to the value of the potential.
+			/// </summary>		
+			public IDictionary<double, Complex> Phasors { get; } = new Dictionary<double, Complex>();
+
+			/// <summary>
+			/// The DC potential of the node with respect to ground
+			/// </summary>
+			public double DC { get; }
+
+			#endregion
+		}
+	}
+}
