2f3fad52c056a5027913c9a9d9287e9a3b75b0db
Szymon Żogała <szymek.655@gmail.com>
2018-10-09 10:35:50 +0200

    Added

---
+++ ECAT.Core/IoC/NestedClasses/DeclaredConstructorSelector.cs
@@ -0,0 +1,66 @@
+﻿using Autofac.Core.Activators.Reflection;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace ECAT.Core
+{
+	partial class IoC
+	{
+		/// <summary>
+		/// Class used to find constructors on types that match constructor declarations on service that type is registered as.
+		/// </summary>
+		private class DeclaredConstructorFinder : IConstructorFinder
+		{
+			#region Constructors
+
+			/// <summary>
+			/// Default constructor
+			/// </summary>
+			/// <param name="service"></param>
+			/// <exception cref="ArgumentNullException"></exception>
+			public DeclaredConstructorFinder(Type service)
+			{
+				Service = service ?? throw new ArgumentNullException(nameof(service));
+			}
+
+			#endregion
+
+			#region Private properties
+
+			/// <summary>
+			/// Service as which the type is being registered
+			/// </summary>
+			private Type Service { get; }
+
+			#endregion
+
+			#region Public methods
+
+			/// <summary>
+			/// Finds constructors that match those declared using <see cref="ConstructorDeclaration"/> attribute on <see cref="Service"/>
+			/// </summary>
+			/// <param name="targetType"></param>
+			/// <returns></returns>
+			public ConstructorInfo[] FindConstructors(Type targetType)
+			{
+				var declarations = GetConstructorDeclarations(Service);
+
+				// Get all constructors
+				return targetType.GetConstructors().
+				// Filter them, get constructor declarations
+				Where((constructor) => declarations.
+					// Check if any declaration matches the constructor by getting the constructor's parameters
+					Any((declaration) => constructor.GetParameters().
+						// Projecting them to their types
+						Select((parameter) => parameter.ParameterType).
+					// And comparing both sequences
+					SequenceEqual(declaration))).
+				// Finally return the result as an array
+				ToArray();
+			}
+
+			#endregion
+		}
+	}
+}
---
+++ ECAT.Core/IoC/NestedClasses/ExactConstructorSelector.cs
@@ -0,0 +1,54 @@
+﻿using Autofac;
+using Autofac.Core;
+using Autofac.Core.Activators.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ECAT.Core
+{
+	partial class IoC
+	{
+		/// <summary>
+		/// Class used to select constructors for registered services that have parameters exactly matching the provided ones
+		/// (first count is checked, then if all constructor parameters can have value provided by any of the provided parameters)
+		/// </summary>
+		private class ExactConstructorSelector : IConstructorSelector
+		{
+			#region Private properties
+
+			/// <summary>
+			/// Container from which services are resolved
+			/// </summary>
+			private IComponentContext _Context => Container;
+
+			#endregion
+
+			#region Public methods
+
+			/// <summary>
+			/// Selects constructor whose number of parameters matches the number of provided paramters and whose parameter all
+			/// can be supplied value by any of the <paramref name="parameters"/>
+			/// </summary>
+			/// <param name="constructorBindings"></param>
+			/// <param name="parameters"></param>
+			/// <returns></returns>
+			public ConstructorParameterBinding SelectConstructorBinding(ConstructorParameterBinding[] constructorBindings,
+				IEnumerable<Parameter> parameters)
+			{
+				var parametersCount = parameters.Count();
+
+				return constructorBindings.
+					// Get all constructors that have the number of parameters equal to the number of provided parameters
+					Where((constructorBinding) => constructorBinding.TargetConstructor.GetParameters().Count() == parametersCount).
+					// Return the first occurance or defualt (null)
+					FirstOrDefault((constructorBinding) => constructorBinding.TargetConstructor.GetParameters().
+						// Check if all parameters can be supplied value
+						All((constructorParameter) => parameters.
+							// By any of the provided parameters
+							Any((parameter) => parameter.CanSupplyValue(constructorParameter, _Context, out var provider))));
+			}
+
+			#endregion
+		}
+	}
+}
