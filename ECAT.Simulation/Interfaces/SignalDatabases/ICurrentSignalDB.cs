a84afaf3e24bf3ce4ff4ab7e72a7635c5b57e6e8
Szymon Żogała <szymek.655@gmail.com>
2018-10-12 17:33:49 +0200

    Added

---
+++ ECAT.Simulation/Interfaces/SignalDatabases/ICurrentSignalDB.cs
@@ -0,0 +1,47 @@
+﻿using ECAT.Core;
+
+namespace ECAT.Simulation
+{
+	/// <summary>
+	/// Interface for classes capable of computing and caching currents through components, the purpose of the interface is
+	/// to work with other interfaces/classes computing and caching values of voltage, power, etc.
+	/// </summary>
+	internal interface ICurrentSignalDB<T> where T : ISignalData
+	{
+		#region Methods
+
+		/// <summary>
+		/// Gets current flowing through an <see cref="IResistor"/> or null if unsuccessful and stores it in
+		/// <paramref name="current"/>. Returns true on success, false otherwise.
+		/// </summary>
+		/// <param name="voltageBA">If true, voltage used to calculate the current is taken from <see cref="ITwoTerminal.TerminalA"/>
+		/// (reference node) to <see cref="ITwoTerminal.TerminalB"/>, if false the direction is reversed</param>
+		/// <param name="current"></param>
+		/// <returns></returns>
+		bool TryGet(IResistor resistor, out T current, bool voltageBA = true);
+
+		/// <summary>
+		/// Gets current flowing through an <see cref="ICapacitor"/> or null if unsuccessful and stores it in
+		/// <paramref name="current"/>. Returns true on success, false otherwise.
+		/// </summary>
+		/// <param name="capacitor"></param>
+		/// <paramref name="current"></paramref>
+		/// <param name="voltageBA">If true, voltage used to calculate the current is taken from <see cref="ITwoTerminal.TerminalA"/>
+		/// (reference node) to <see cref="ITwoTerminal.TerminalB"/>, if false the direction is reversed</param>
+		/// <returns></returns>
+		bool TryGet(ICapacitor capacitor, out T current, bool voltageBA = true);
+
+		/// <summary>
+		/// Gets current produced by some <see cref="IActiveComponent"/> or null if unsuccessful and stores it in
+		/// <paramref name="current"/>. Returns true on success, false otherwise.
+		/// </summary>
+		/// <param name="activeComponentIndex">Index of the <see cref="IActiveComponent"/> whose current to query</param>
+		/// <paramref name="current"></paramref>
+		/// <param name="reverseDirection">If true, the direction of the current will be reversed (with the respect to
+		/// the normal direction obtained in simulation)</param>
+		/// <returns></returns>
+		bool TryGet(int activeComponentIndex, out T current, bool reverseDirection = false);
+
+		#endregion
+	}	
+}
---
+++ ECAT.Simulation/Interfaces/SignalDatabases/IVoltageSignalDB.cs
@@ -0,0 +1,46 @@
+﻿using ECAT.Core;
+
+namespace ECAT.Simulation
+{
+	/// <summary>
+	/// Interface for classes capable of computing and caching voltages across components, the purpose of the interface is
+	/// to work with other interfaces/classes computing and caching values of current, power, etc.
+	/// </summary>
+	internal interface IVoltageSignalDB<T> where T : ISignalData
+	{
+		#region Methods
+
+		/// <summary>
+		/// Gets voltage drop of a node with respect to ground or null if unsuccessful and assigns it to <paramref name="voltage"/>.
+		/// Returns true on success, false otherwise.
+		/// </summary>
+		/// <param name="nodeIndex"></param>
+		/// <param name="voltage"></param>
+		/// <param name="nodeToGround">If true, voltage drop is calculated from ground to node given by
+		/// <paramref name="nodeIndex"/>, if false it is calculated from node given by <paramref name="nodeIndex"/> to ground</param>
+		/// <returns></returns>
+		bool TryGet(int nodeIndex, out T voltage, bool nodeToGround = true);
+
+		/// <summary>
+		/// Gets voltage drop between two nodes (with node A being treated as the reference node) or null if unsuccessful and
+		/// assigns it to <paramref name="voltage"/>. Returns true on success, false otherwise.
+		/// </summary>
+		/// <param name="nodeAIndex"></param>
+		/// <param name="nodeBIndex"></param>
+		/// <param name="voltage"></param>
+		/// <returns></returns>
+		bool TryGet(int nodeAIndex, int nodeBIndex, out T voltage);
+
+		/// <summary>
+		/// Gets voltage drop across a <see cref="ITwoTerminal"/> component or null if unsuccessful and assigns it to
+		/// <paramref name="voltage"/>. Returns true on success, false otherwise.
+		/// </summary>
+		/// <param name="component"></param>
+		/// <param name="voltageBA">If true, voltage drop is calculated from <see cref="ITwoTerminal.TerminalB"/> to
+		/// <param name="voltage"></param>
+		/// <returns></returns>
+		bool TryGet(ITwoTerminal component, out T voltage, bool voltageBA = true);
+
+		#endregion
+	}
+}
