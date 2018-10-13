74dac4150465e8499fc1a65f33832b0b5613ae4e
Szymon Żogała <szymek.655@gmail.com>
2018-10-13 14:33:19 +0200

    Now also handles currents of active components

--- ECAT.Simulation/SignalCache/CurrentCache.cs
+++ ECAT.Simulation/SignalCache/CurrentCache.cs
@@ -1,6 +1,8 @@
 ﻿using CSharpEnhanced.CoreClasses;
 using ECAT.Core;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ECAT.Simulation
 {
@@ -17,9 +19,42 @@ namespace ECAT.Simulation
 		/// <summary>
 		/// Default constructor
 		/// </summary>
-		public CurrentCache() : base(new CustomEqualityComparer<Tuple<ITwoTerminal, bool>>(
+		/// <param name="activeComponentsCurrents"></param>
+		public CurrentCache(IEnumerable<KeyValuePair<int, TSignal>> activeComponentsCurrents) :
+			base(new CustomEqualityComparer<Tuple<ITwoTerminal, bool>>(
 				// Compare the elements of the Tuples, not tuples themselves
-				(x, y) => x.Item1 == y.Item1 && x.Item2 == y.Item2)) { }
+				(x, y) => x.Item1 == y.Item1 && x.Item2 == y.Item2))
+		{
+			// Create a new dictionary
+			_ActiveComponentsCache = new Dictionary<Tuple<int, bool>, Tuple<TSignal, ISignalInformation>>(
+				// Check if parameter is not null
+				activeComponentsCurrents?.
+				// Project it to a tuple with the int key and bool (false, indicating no direction change) and the value
+				Select((x) => new KeyValuePair<Tuple<int, bool>, TSignal>(new Tuple<int, bool>(x.Key, false), x.Value)).
+				// Concat it with the same currents, this time
+				Concat(activeComponentsCurrents.
+				// Project them to a tuple with the int key and bool (true, indicating direction was reversed) and the value negated
+				Select((x) => new KeyValuePair<Tuple<int, bool>, TSignal>(new Tuple<int, bool>(x.Key, true), CopyAndNegate(x.Value)))).
+				// Finally transform it to a dictionary of required types
+				ToDictionary(
+				// Key stays the same
+				(x) => x.Key,
+				// Value is the signal and information based on it
+				(x) => Tuple.Create(x.Value, IoC.Resolve<ISignalInformation>(x.Value, IoC.Resolve<ICommonSignalDescriptions>().Current)))
+				// If the null check above caught a null value, this operator will result in the exception being thrown
+				?? throw new ArgumentNullException(nameof(activeComponentsCurrents)));
+		}
+
+		#endregion
+
+		#region Protected properties
+
+		/// <summary>
+		/// Cache with currents produced by <see cref="IVoltageSource"/>s, <see cref="IACVoltageSource"/>s and <see cref="IOpAmp"/>s
+		/// For key Item1 is the index of the <see cref="IActiveComponent"/>, Item2 determines whether direction was reversed.
+		/// For value Item1 is the data on which the signal is based, Item2 is information about the signal data.
+		/// </summary>
+		protected IReadOnlyDictionary<Tuple<int, bool>, Tuple<TSignal, ISignalInformation>> _ActiveComponentsCache { get; }
 
 		#endregion
 
@@ -48,6 +83,60 @@ namespace ECAT.Simulation
 		#endregion
 
 		#region Protected methods
+
+		/// <summary>
+		/// Tries to obtain current from the <see cref="_ActiveComponentsCache"/>, if successful assigns it to <paramref name="current"/>
+		/// and returns true, otherwise assigns default value of TSignal to <paramref name="current"/> and returns false
+		/// </summary>
+		/// <param name="activeComponentIndex"></param>
+		/// <param name="current"></param>
+		/// <param name="reverseDirection">True if the direction is assumed to be standard, false if reversed</param>
+		/// <returns></returns>
+		protected bool TryGetActiveComponentCurrent(int activeComponentIndex, out TSignal current, bool reverseDirection)
+		{
+			// Check if the current is in the cache (If it was not provided during object construction, it's not possible to
+			// calculate it now)
+			if (_ActiveComponentsCache.TryGetValue(
+				new Tuple<int, bool>(activeComponentIndex, reverseDirection), out var currentPackage))
+			{
+				// If so, assign it and return success
+				current = currentPackage.Item1;
+				return true;
+			}
+			else
+			{
+				// Otherwise assign default value and return false
+				current = default(TSignal);
+				return false;
+			}
+		}
+
+		/// <summary>
+		/// Tries to obtain current from the <see cref="_ActiveComponentsCache"/>, if successful assigns it to <paramref name="info"/>
+		/// and returns true, otherwise assigns null to <paramref name="info"/> and returns false
+		/// </summary>
+		/// <param name="activeComponentIndex"></param>
+		/// <param name="info"></param>
+		/// <param name="reverseDirection">True if the direction is assumed to be standard, false if reversed</param>
+		/// <returns></returns>
+		protected bool TryGetActiveComponentCurrent(int activeComponentIndex, out ISignalInformation info, bool reverseDirection)
+		{
+			// Check if the current is in the cache (If it was not provided during object construction, it's not possible to
+			// calculate it now)
+			if (_ActiveComponentsCache.TryGetValue(
+				new Tuple<int, bool>(activeComponentIndex, reverseDirection), out var currentPackage))
+			{
+				// If so, assign it and return success
+				info = currentPackage.Item2;
+				return true;
+			}
+			else
+			{
+				// Otherwise assign null and return false
+				info = null;
+				return false;
+			}
+		}
 
 		/// <summary>
 		/// Caches the <paramref name="signal"/> as well as its negated copy (with inverted indexes)
