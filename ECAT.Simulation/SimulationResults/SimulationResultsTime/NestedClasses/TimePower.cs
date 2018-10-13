ac97e5b3099b1c45c93829048ff9af474497f74f
Szymon Żogała <szymek.655@gmail.com>
2018-10-14 00:28:43 +0200

    Finished implementation

--- ECAT.Simulation/SimulationResults/SimulationResultsTime/NestedClasses/TimePower.cs
+++ ECAT.Simulation/SimulationResults/SimulationResultsTime/NestedClasses/TimePower.cs
@@ -1,6 +1,7 @@
-﻿using ECAT.Core;
+﻿using CSharpEnhanced.CoreClasses;
+using CSharpEnhanced.Helpers;
+using ECAT.Core;
 using System;
-using System.Collections.Generic;
 
 namespace ECAT.Simulation
 {
@@ -10,7 +11,7 @@ namespace ECAT.Simulation
 		/// Provides functionality connected with storing, calculating and exposing power information in
 		/// form of <see cref="ISignalInformation"/>
 		/// </summary>
-		private class TimePower : IPowerDB
+		private class TimePower : PowerCache<ITimeDomainSignal>, IPowerDB
 		{
 			#region Constructors
 
@@ -40,153 +41,66 @@ namespace ECAT.Simulation
 			/// </summary>
 			private ICurrentSignalDB<ITimeDomainSignal> _Currents { get; }
 
-			/// <summary>
-			/// Contains already computed <see cref="ISignalInformation"/>s
-			/// </summary>
-			private Dictionary<IBaseComponent, ISignalInformation> _Cache { get; } = new Dictionary<IBaseComponent,ISignalInformation>();
-
 			#endregion
 
 			#region Private methods
 
 			/// <summary>
-			/// Caches the <paramref name="power"/> in <see cref="_Cache"/>
+			/// Tries to constract a power for an <see cref="ITwoTerminal"/>
 			/// </summary>
-			/// <param name="component">Component for which the current flow is considered</param>
+			/// <param name="twoTerminal"></param>
 			/// <param name="power"></param>
-			private void CachePower(IBaseComponent component, ISignalData power) =>
-				_Cache.Add(
-					component,
-					IoC.Resolve<ISignalInformation>(power, IoC.Resolve<ICommonSignalDescriptions>().Power));
-
-			/// <summary>
-			/// Returns true if power for <paramref name="resistor"/> can be obtained from <see cref="_Cache"/>
-			/// </summary>
-			/// <param name="resistor"></param>
 			/// <returns></returns>
-			private bool TryEnablePower(IResistor resistor) =>
-				// Check if the cache already contains an entry, otherwise try to construct it
-				_Cache.ContainsKey(resistor) || TryConstructPower(resistor);
-
-			/// <summary>
-			/// Tries to construct a power for <paramref name="resistor"/>, returns true on success
-			/// </summary>
-			/// <param name="element"></param>
-			/// <returns></returns>
-			private bool TryConstructPower(IResistor resistor)
+			private bool TryConstructPower(ITwoTerminal twoTerminal, out ITimeDomainSignal power)
 			{
-				// Try to get voltage drop across the element
-				if (_VoltageDrops.TryGet(resistor, out var voltageDrop))
+				power = null;
+
+				if (_VoltageDrops.TryGet(twoTerminal, out var voltage))
 				{
-					// If successful, create a new power signal based on it, cache it
-					CachePower(resistor, 
-						IoC.Resolve<ITimeDomainSignal>());
+					ITimeDomainSignal current = null;
 
-					// And return success
-					return true;
-				}
-				else
-				{
-					// Return failure					
-					return false;
-				}
-			}
-
-			/// <summary>
-			/// Returns true if power for <paramref name="voltageSource"/> can be obtained from <see cref="_Cache"/>
-			/// </summary>
-			/// <param name="voltageSource"></param>
-			/// <returns></returns>
-			private bool TryEnablePower(IVoltageSource voltageSource) =>
-				// Check if the cache already contains an entry, otherwise try to construct it
-				_Cache.ContainsKey(voltageSource) || TryConstructPower(voltageSource);
-
-			/// <summary>
-			/// Tries to construct a power for <paramref name="voltageSource"/>, returns true on success
-			/// </summary>
-			/// <param name="element"></param>
-			/// <returns></returns>
-			private bool TryConstructPower(IVoltageSource voltageSource)
-			{
-				// Try to get voltage drop across the element
-				if (_Currents.TryGet(voltageSource.ActiveComponentIndex, out var current))
-				{
-					// If successful, create a new power signal based on it, cache it
-					CachePower(voltageSource, IoC.Resolve<ITimeDomainSignal>());
-						
-
-					// And return success
-					return true;
-				}
-				else
-				{
-					// Return failure					
-					return false;
-				}
-			}
-
-			/// <summary>
-			/// Returns true if power for <paramref name="voltageSource"/> can be obtained from <see cref="_Cache"/>
-			/// </summary>
-			/// <param name="voltageSource"></param>
-			/// <returns></returns>
-			private bool TryEnablePower(IACVoltageSource voltageSource) =>
-				// Check if the cache already contains an entry, otherwise try to construct it
-				_Cache.ContainsKey(voltageSource) || TryConstructPower(voltageSource);
-
-			/// <summary>
-			/// Tries to construct a power for <paramref name="voltageSource"/>, returns true on success
-			/// </summary>
-			/// <param name="element"></param>
-			/// <returns></returns>
-			private bool TryConstructPower(IACVoltageSource voltageSource)
-			{
-				// Try to get voltage drop across the element
-				if (_Currents.TryGet(voltageSource.ActiveComponentIndex, out var current))
-				{
-					// If successful, create a new power signal based on it, cache it
-					CachePower(voltageSource, IoC.Resolve<ITimeDomainSignal>());
+					// Call different method depending on the type of the two terminal
+					TypeSwitch.Construct().
+						LazyCase<IResistor>((x) => _Currents.TryGet(x, out current)).
+						LazyCase<ICapacitor>((x) => _Currents.TryGet(x, out current)).
+						LazyCase<IActiveComponent>((x) => _Currents.TryGet(x.ActiveComponentIndex, out current)).
+						Switch(twoTerminal);
 					
-					// And return success
-					return true;
+					// If both voltage and current were obtained
+					if(current != null)
+					{
+						power = IoC.Resolve<ITimeDomainSignal>(
+							voltage.InstantenousValues.MergeSelect(current.InstantenousValues, (x, y) => x * y),
+							voltage.TimeStep,
+							current.StartTime);
+					}
 				}
-				else
-				{
-					// Return failure					
-					return false;
-				}
+
+				// If power is not null it means that it was successfully constructed
+				return power != null;
 			}
 
-			/// <summary>
-			/// Returns true if power for <paramref name="currentSource"/> can be obtained from <see cref="_Cache"/>
-			/// </summary>
-			/// <param name="currentSource"></param>
-			/// <returns></returns>
-			private bool TryEnablePower(ICurrentSource currentSource) =>
-				// Check if the cache already contains an entry, otherwise try to construct it
-				_Cache.ContainsKey(currentSource) || TryConstructPower(currentSource);
+			#endregion
+
+			#region Protected methods
 
 			/// <summary>
-			/// Tries to construct a power for <paramref name="currentSource"/>, returns true on success
+			/// Tries to construct power for <paramref name="component"/>, on success assigns it to <paramref name="power"/> and
+			/// true, on failure assigns null to <paramref name="power"/> and returns false.
 			/// </summary>
-			/// <param name="element"></param>
+			/// <param name="component"></param>
+			/// <param name="power"></param>
 			/// <returns></returns>
-			private bool TryConstructPower(ICurrentSource currentSource)
+			protected override bool TryConstructPower(IBaseComponent component, out ITimeDomainSignal power)
 			{
-				// Try to get voltage drop across the element
-				if (_VoltageDrops.TryGet(currentSource, out var voltageDrop))
-				{
-					// If successful, create a new power signal based on it, cache it
-					CachePower(currentSource, IoC.Resolve<ITimeDomainSignal>());
+				ITimeDomainSignal constructedPower = null;
 
-					// And return success
-					return true;
-				}
-				else
-				{
-					// Return failure					
-					return false;
-				}
+				TypeSwitch.Construct().
+					LazyCase<ITwoTerminal>((x) => TryConstructPower(x, out constructedPower)).
+					Switch(component);
+
+				power = constructedPower;
+				return power != null;
 			}
 
 			#endregion
@@ -201,7 +115,7 @@ namespace ECAT.Simulation
 			/// <returns></returns>
 			public ISignalInformation Get(IResistor resistor) =>
 				// Check if power can be enabled and if it can be fetched, if so return it, otherwise return null
-				TryEnablePower(resistor) && _Cache.TryGetValue(resistor, out var power) ? power : null;
+				TryEnablePower(resistor) && _Cache.TryGetValue(resistor, out var power) ? power.Item2 : null;
 			
 
 			/// <summary>
@@ -212,7 +126,7 @@ namespace ECAT.Simulation
 			/// <returns></returns>
 			public ISignalInformation Get(ICurrentSource currentSource) =>
 				// Check if power can be enabled and if it can be fetched, if so return it, otherwise return null
-				TryEnablePower(currentSource) && _Cache.TryGetValue(currentSource, out var power) ? power : null;
+				TryEnablePower(currentSource) && _Cache.TryGetValue(currentSource, out var power) ? power.Item2 : null;
 
 			/// <summary>
 			/// Gets information about power on an <see cref="IVoltageSource"/>
@@ -222,7 +136,7 @@ namespace ECAT.Simulation
 			/// <returns></returns>
 			public ISignalInformation Get(IVoltageSource voltageSource) =>
 				// Check if power can be enabled and if it can be fetched, if so return it, otherwise return null
-				TryEnablePower(voltageSource) && _Cache.TryGetValue(voltageSource, out var power) ? power : null;
+				TryEnablePower(voltageSource) && _Cache.TryGetValue(voltageSource, out var power) ? power.Item2 : null;
 
 			/// <summary>
 			/// Gets information about power on an <see cref="IACVoltageSource"/>. If the <paramref name="current"/> is composed of
@@ -235,7 +149,7 @@ namespace ECAT.Simulation
 			/// <returns></returns>
 			public ISignalInformation Get(IACVoltageSource voltageSource) =>
 				// Check if power can be enabled and if it can be fetched, if so return it, otherwise return null
-				TryEnablePower(voltageSource) && _Cache.TryGetValue(voltageSource, out var power) ? power : null;
+				TryEnablePower(voltageSource) && _Cache.TryGetValue(voltageSource, out var power) ? power.Item2 : null;
 
 			#endregion
 		}
