a367d0a8469617674b217bce27f237153e9128ea
Szymon Żogała <szymek.655@gmail.com>
2018-10-14 22:53:03 +0200

    Added

---
+++ ECAT.Simulation/Services/ITimeDomainSignalMutable.cs
@@ -0,0 +1,31 @@
+﻿using ECAT.Core;
+using System;
+using System.Collections.Generic;
+
+namespace ECAT.Simulation
+{
+	/// <summary>
+	/// Extension of <see cref="ITimeDomainSignal"/> that allows for adding/modifying values of public properties of the inherited
+	/// interface
+	/// </summary>
+	[NecessaryService]
+	[ConstructorDeclaration]
+	[ConstructorDeclaration(typeof(ITimeDomainSignal), "Copy constructor")]
+	[ConstructorDeclaration(new Type[] { typeof(int), typeof(double) },"Samples", "Instantenous Values", "Time step", "Start time is equal to 0")]
+	[ConstructorDeclaration(new Type[] { typeof(int), typeof(double), typeof(double) }, "Samples", "Instantenous Values", "Time step", "Start time")]
+	public interface ITimeDomainSignalMutable : ITimeDomainSignal
+    {
+		#region Methods
+
+		/// <summary>
+		/// Used to add <see cref="KeyValuePair{TKey, TValue}"/> to <see cref="INodePotentialBias.Phasors"/>
+		/// </summary>
+		/// <param name="frequency"></param>
+		/// <param name="value"></param>
+		/// <exception cref="ArgumentNullException"></exception>
+		/// <exception cref="ArgumentException"></exception>
+		void AddWaveform(double frequency, IEnumerable<double> instantenousValues);
+
+		#endregion
+	}
+}
---
+++ ECAT.Simulation/Signals/SignalData/TimeDomainSignal/TimeDomainSignalMutable.cs
@@ -0,0 +1,58 @@
+﻿using ECAT.Core;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace ECAT.Simulation
+{
+	/// <summary>
+	/// Extension of <see cref="TimeDomainSignal"/> (and <see cref="Core.ITimeDomainSignal"/>) that allows for mutating values
+	/// </summary>
+	[RegisterAsType(typeof(IPhasorDomainSignalMutable))]
+	public class TimeDomainSignalMutable : TimeDomainSignal, ITimeDomainSignalMutable
+    {
+		#region Constructors
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public TimeDomainSignalMutable() { }
+
+		/// <summary>
+		/// Constructor with parameters, start time is considered 0
+		/// </summary>
+		/// <param name="instantenousValues">Values occuring at specific time moments, can'be be null</param>
+		/// <param name="timeStep">Time step between two subsequent values</param>
+		/// <exception cref="ArgumentNullException"></exception>
+		public TimeDomainSignalMutable(int samples, double timeStep) : base(samples, timeStep) { }
+
+		/// <summary>
+		/// Constructor with parameters
+		/// </summary>
+		/// <param name="instantenousValues">Values occuring at specific time moments, can'be be null</param>
+		/// <param name="timeStep">Time step between two subsequent values</param>
+		/// <param name="startTime">Start time of the signal</param>
+		public TimeDomainSignalMutable(int samples, double timeStep, double startTime) : base(samples, timeStep, startTime) { }
+
+		/// <summary>
+		/// Copy constructor
+		/// </summary>
+		/// <exception cref="ArgumentNullException"></exception>
+		public TimeDomainSignalMutable(ITimeDomainSignal signal) : base(signal) { }
+
+		#endregion
+
+		#region Public methods
+
+		/// <summary>
+		/// Used to add <see cref="KeyValuePair{TKey, TValue}"/> to <see cref="INodePotentialBias.Phasors"/>
+		/// </summary>
+		/// <param name="frequency"></param>
+		/// <param name="value"></param>
+		/// <exception cref="ArgumentNullException"></exception>
+		/// <exception cref="ArgumentException"></exception>
+		public void AddWaveform(double frequency, IEnumerable<double> instantenousValues) =>
+			AddWaveformToComposingWaveforms(frequency, instantenousValues);
+
+		#endregion
+	}
+}
