12f40213fbed4b5793524dc21cb0298d353a4101
Szymon Żogała <szymek.655@gmail.com>
2018-10-16 22:04:59 +0200

    No longer derives from DCVoltageSource

--- ECAT.Design/Components/ACVoltageSource/ACVoltageSource.cs
+++ ECAT.Design/Components/ACVoltageSource/ACVoltageSource.cs
@@ -1,4 +1,5 @@
-﻿using ECAT.Core;
+﻿using System.Numerics;
+using ECAT.Core;
 
 namespace ECAT.Design
 {
@@ -7,7 +8,7 @@ namespace ECAT.Design
 	/// </summary>
 	[DisplayCurrentInfo(sectionIndex: 1)]
 	[DisplayPowerInfo(sectionIndex: 2)]
-	public class ACVoltageSource : DCVoltageSource, IACVoltageSource
+	public class ACVoltageSource : TwoTerminal, IACVoltageSource
 	{
 		#region Constructor
 
@@ -21,7 +22,26 @@ namespace ECAT.Design
 
 		#endregion
 
+		#region Private properties
+
+		/// <summary>
+		/// The admittance of a voltage source (constant value)
+		/// </summary>
+		private Complex _Admittance { get; } = IoC.Resolve<IDefaultValues>().VoltageSourceAdmittance;
+
+		#endregion
+
 		#region Public properties
+
+		/// <summary>
+		/// DC voltage produced by this <see cref="IACVoltageSource"/>
+		/// </summary>
+		public double ProducedDCVoltage { get; set; } = IoC.Resolve<IDefaultValues>().DefaultVoltageSourceProducedVoltage;
+
+		/// <summary>
+		/// Index used to query <see cref="ISimulationResults"/> for produced current
+		/// </summary>
+		public int ActiveComponentIndex { get; set; }
 
 		/// <summary>
 		/// Frequency of the AC voltage produced by this <see cref="ACVoltageSource"/>
@@ -32,6 +52,17 @@ namespace ECAT.Design
 		/// The positive peak value of the produced voltage sine wave
 		/// </summary>
 		public double PeakProducedVoltage { get; set; } = IoC.Resolve<IDefaultValues>().DefaultACVoltageSourceProducedACVoltage;
+
+		#endregion
+
+		#region Protected methods
+
+		/// <summary>
+		/// Returns the admittance of an <see cref="IACVoltageSource"/>
+		/// </summary>
+		/// <param name="frequency"></param>
+		/// <returns></returns>
+		protected override Complex CalculateAdmittance(double frequency) => _Admittance;
 
 		#endregion
 	}
