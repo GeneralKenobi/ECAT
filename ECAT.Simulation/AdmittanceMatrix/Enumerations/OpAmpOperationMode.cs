ab47226129035c9239279e176c72f6c212abdf8b
Szymon Żogała <szymek.655@gmail.com>
2019-02-12 12:16:13 +0100

    Added

---
+++ ECAT.Simulation/AdmittanceMatrix/Enumerations/OpAmpOperationMode.cs
@@ -0,0 +1,23 @@
+﻿namespace ECAT.Simulation
+{
+	/// <summary>
+	/// Possible operation modes for an <see cref="IOpAmp"/>
+	/// </summary>
+	public enum OpAmpOperationMode
+	{
+		/// <summary>
+		/// <see cref="IOpAmp"/> is in active mode - it's an amplifier, output voltage is between supply voltages
+		/// </summary>
+		Active = 0,
+
+		/// <summary>
+		/// Output voltage is constant and equal to positive supply voltage
+		/// </summary>
+		PositiveSaturation = 1,
+
+		/// <summary>
+		/// Output voltage is constant and equal to negative supply voltage
+		/// </summary>
+		NegativeSaturation = 2,
+	}
+}
