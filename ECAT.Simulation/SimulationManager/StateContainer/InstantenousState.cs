f8f09574e488c8ead92edc35dcda1c7a4b7af613
Szymon Żogała <szymek.655@gmail.com>
2019-02-16 13:58:53 +0100

    Added

---
+++ ECAT.Simulation/SimulationManager/StateContainer/InstantenousState.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ECAT.Simulation
+{
+	/// <summary>
+	/// Container for instantenous state of a circuit with respect to one source
+	/// </summary>
+	public class InstantenousState
+	{
+		#region Constructor
+
+		/// <summary>
+		/// Constructor with parameters
+		/// </summary>
+		/// <param name="nodes">Nodes present in this instance</param>
+		/// <param name="activeComponentsIndices">Active components indices present in this instance</param>
+		public InstantenousState(IEnumerable<int> nodeIndices, IEnumerable<int> activeComponentsIndices)
+		{
+			// Make an entry for each node
+			foreach(var node in nodeIndices)
+			{
+				Potentials.Add(node, 0);
+			}
+
+			// Make an entry for each index
+			foreach(var index in activeComponentsIndices)
+			{
+				Currents.Add(index, 0);
+			}
+		}
+
+		/// <summary>
+		/// Constructor with parameters
+		/// </summary>
+		/// <param name="nodes">Nodes present in this instance</param>
+		/// <param name="activeComponentsCount">Number of active components, indices available in this instance will are given by a
+		/// range: 0 to <paramref name="activeComponentsCount"/> - 1</param>
+		public InstantenousState(IEnumerable<int> nodeIndices, int activeComponentsCount) :
+			this(nodeIndices, Enumerable.Range(0, activeComponentsCount)) { }
+
+		#endregion
+
+		#region Private properties
+
+		/// <summary>
+		/// Contains nodes and their potentials
+		/// </summary>
+		public IDictionary<int, double> Potentials { get; } = new Dictionary<int, double>();
+
+		/// <summary>
+		/// Contains indices of active components and their currents
+		/// </summary>
+		public IDictionary<int, double> Currents { get; } = new Dictionary<int, double>();
+
+		#endregion
+
+		#region Public methods
+
+		/// <summary>
+		/// Adds the <paramref name="other"/> state to this instance.
+		/// </summary>
+		/// <param name="other">Object to add to this instance, internal collections' keys have to match, otherwise an exception is thrown</param>
+		public void AddState(InstantenousState other)
+		{
+			// Check if keys of the other instance match internal keys, if not throw an exception
+			if(Potentials.Keys.Intersect(other.Potentials.Keys).Count() != Potentials.Count ||
+				Currents.Keys.Intersect(other.Currents.Keys).Count() != Currents.Count)
+			{
+				throw new ArgumentException(nameof(other) + " has incompatible internal collections (keys don't match)");
+			}
+
+			// Add potentials from the other instance
+			foreach (var key in Potentials.Keys.ToList())
+			{
+				Potentials[key] += other.Potentials[key];
+			}
+
+			// Add currents from the other instance
+			foreach (var key in Currents.Keys.ToList())
+			{
+				Currents[key] += other.Currents[key];
+			}
+		}
+
+		#endregion
+	}
+}
---
+++ ECAT.Simulation/SimulationManager/StateContainer/PartialInstantenousStates.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ECAT.Simulation
+{
+	/// <summary>
+	/// Container for instantenous state of a system
+	/// </summary>
+	public class PartialInstantenousStates
+	{
+		#region Constructor
+
+		/// <summary>
+		/// Constructor with parameters
+		/// </summary>
+		/// <param name="acSourcesCount">Number of AC sources expected in this state</param>
+		/// <param name="nodeIndices">Indices of nodes present in this instance</param>
+		/// <param name="activeComponentsIndices">Active components indices present in this instance</param>
+		public PartialInstantenousStates(int acSourcesCount, IEnumerable<int> nodeIndices, IEnumerable<int> activeComponentsIndices)
+		{
+			// Create container for DC state
+			DCState = new InstantenousState(nodeIndices, activeComponentsIndices);
+
+			// Create array for AC states
+			ACStates = new InstantenousState[acSourcesCount];
+
+			// And initialize each entry in the array
+			for(int i = 0; i < acSourcesCount; ++i)
+			{
+				ACStates[i] = new InstantenousState(nodeIndices, activeComponentsIndices);
+			}
+
+			// Assign the collections to private properties for future use
+			_NodeIndices = nodeIndices;
+			_ActiveComponentsIndices = activeComponentsIndices;
+		}
+
+		/// <summary>
+		/// Constructor with parameters
+		/// </summary>
+		/// <param name="acSourcesCount">Number of AC sources expected in this state</param>
+		/// <param name="nodeIndices">Indices of nodes present in this instance</param>
+		/// <param name="activeComponentsCount">Number of active components, indices available in this instance are given by a
+		/// range: 0 to <paramref name="activeComponentsCount"/> - 1</param>
+		public PartialInstantenousStates(int acSourcesCount, IEnumerable<int> nodeIndices, int activeComponentsCount) :
+			this(acSourcesCount, nodeIndices, Enumerable.Range(0, activeComponentsCount)) { }
+
+		/// <summary>
+		/// Constructor with parameters
+		/// </summary>
+		/// <param name="acSourcesCount">Number of AC sources expected in this state</param>
+		/// <param name="nodesCount">Number of nodes, indices available in this instance are given by a
+		/// range: 0 to <paramref name="nodesCount"/> - 1</param>
+		/// <param name="activeComponentsCount">Number of active components, indices available in this instance are given by a
+		/// range: 0 to <paramref name="activeComponentsCount"/> - 1</param>
+		public PartialInstantenousStates(int acSourcesCount, int nodesCount, int activeComponentsCount) :
+			this(acSourcesCount, Enumerable.Range(0, nodesCount), Enumerable.Range(0, activeComponentsCount)) { }
+
+		/// <summary>
+		/// Constructor with parameters
+		/// </summary>
+		/// <param name="acSourcesCount">Number of AC sources expected in this state</param>
+		/// <param name="nodesCount">Number of nodes, indices available in this instance are given by a
+		/// range: 0 to <paramref name="nodesCount"/> - 1</param>
+		/// <param name="activeComponentsIndices">Active components indices present in this instance</param>
+		public PartialInstantenousStates(int acSourcesCount, int nodesCount, IEnumerable<int> activeComponentsIndices) :
+			this(acSourcesCount, Enumerable.Range(0, nodesCount), activeComponentsIndices) { }
+
+		#endregion
+
+		#region Private properties
+
+		/// <summary>
+		/// Indices of nodes present in this instance
+		/// </summary>
+		private IEnumerable<int> _NodeIndices { get; }
+
+		/// <summary>
+		/// Active components indices present in this instance
+		/// </summary>
+		private IEnumerable<int> _ActiveComponentsIndices { get; }
+
+		#endregion
+
+		#region Public properties
+
+		/// <summary>
+		/// Array holding AC instantenous potentials for each source
+		/// </summary>
+		public InstantenousState[] ACStates { get; }
+
+		/// <summary>
+		/// DC instantenous state of the system
+		/// </summary>
+		public InstantenousState DCState { get; }
+
+		#endregion
+
+		#region Public methods
+
+		/// <summary>
+		/// Combines (adds) all states (for every AC source and DC) and returns it
+		/// </summary>
+		/// <returns></returns>
+		public InstantenousState Combine()
+		{
+			// Create result
+			var result = new InstantenousState(_NodeIndices, _ActiveComponentsIndices);
+
+			// Add every AC state
+			for(int i = 0; i < ACStates.Length; ++i)
+			{
+				result.AddState(ACStates[i]);
+			}
+
+			// Add DC state
+			result.AddState(DCState);
+
+			return result;
+		}
+
+		#endregion
+	}
+}
