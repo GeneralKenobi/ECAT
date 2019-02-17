74766554a86eabb2e48efb5e3b9f7799f19783a6
Szymon Żogała <szymek.655@gmail.com>
2019-02-17 13:30:02 +0100

    Now implements IActiveComponent

--- ECAT.Core/MainFunctionalityInterfaces/Design/Components/CurrentSource/ICurrentSource.cs
+++ ECAT.Core/MainFunctionalityInterfaces/Design/Components/CurrentSource/ICurrentSource.cs
@@ -4,7 +4,7 @@
 	/// Interface for current sources (<see cref="ITwoTerminal.TerminalA"/> corresponds to the negative terminal and
 	/// <see cref="ITwoTerminal.TerminalB"/> corresponds to the positive terminal)
 	/// </summary>
-	public interface ICurrentSource : ITwoTerminal
+	public interface ICurrentSource : ITwoTerminal, IActiveComponent
     {
 		#region Properties
 
