4cc611dfd0248b2088760b39fbc1f9357aad51c6
Szymon Żogała <szymek.655@gmail.com>
2019-02-19 14:32:52 +0100

    Added - it will replace IActiveComponent in some parts of IActiveComponent's functionality

---
+++ ECAT.Core/MainFunctionalityInterfaces/Design/Components/BasicTypes/ISource.cs
@@ -0,0 +1,22 @@
+﻿namespace ECAT.Core
+{
+	/// <summary>
+	/// Interface for all sources
+	/// </summary>
+	public interface ISource
+	{
+		#region Properties
+
+		/// <summary>
+		/// Index used to query <see cref="ISimulationResults"/> for produced current
+		/// </summary>
+		int ActiveComponentIndex { get; set; }
+
+		/// <summary>
+		/// Description of this <see cref="ISource"/>
+		/// </summary>
+		IActiveComponentDescription Description { get; }
+
+		#endregion
+	}
+}
