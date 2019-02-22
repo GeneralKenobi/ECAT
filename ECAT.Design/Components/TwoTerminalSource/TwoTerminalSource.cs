73c4a35a48ff719fead5ad7025a580353f075715
Szymon Żogała <szymek.655@gmail.com>
2019-02-22 20:22:39 +0100

    Added

---
+++ ECAT.Design/Components/TwoTerminalSource/TwoTerminalSource.cs
@@ -0,0 +1,50 @@
+﻿using ECAT.Core;
+
+namespace ECAT.Design
+{
+	/// <summary>
+	/// Base class for <see cref="ITwoTerminalSource"/>s
+	/// </summary>
+	public abstract class TwoTerminalSource : TwoTerminal, ISource
+	{
+		#region Private members
+
+		/// <summary>
+		/// Backing store for <see cref="OutputValue"/>
+		/// </summary>
+		private double mOutputValue;
+
+		#endregion
+
+		#region Protected properties
+
+		/// <summary>
+		/// Backing store for <see cref="Description"/>
+		/// </summary>
+		protected SourceDescription _Description { get; } = new SourceDescription();
+
+		#endregion
+
+		#region Public properties
+
+		/// <summary>
+		/// Output value of the source (voltage, current, etc.)
+		/// </summary>
+		public double OutputValue
+		{
+			get => mOutputValue;
+			set
+			{
+				mOutputValue = value;
+				_Description.OutputValue = value;
+			}
+		}
+
+		/// <summary>
+		/// Description of this <see cref="ISource"/>
+		/// </summary>
+		public ISourceDescription Description => _Description;
+
+		#endregion
+	}
+}
