f55fa5111dbfa76360037fe1c2ef3e11406ca0f3
Szymon Żogała <szymek.655@gmail.com>
2019-02-22 20:31:13 +0100

    Added

---
+++ ECAT.Simulation/AdmittanceMatrix/NodeInfo/OpAmpNodeInfo.cs
@@ -0,0 +1,44 @@
+﻿namespace ECAT.Simulation
+{
+	/// <summary>
+	/// Contains information about node indices for op-amp
+	/// </summary>
+	public class OpAmpNodeInfo
+	{
+		#region Constructor
+
+		/// <summary>
+		/// Constructor with parameters
+		/// </summary>
+		/// <param name="output"></param>
+		/// <param name="nonInvertingInput"></param>
+		/// <param name="invertingInput"></param>
+		public OpAmpNodeInfo(int output, int nonInvertingInput, int invertingInput)
+		{
+			Output = output;
+			NonInvertingInput = nonInvertingInput;
+			InvertingInput = invertingInput;
+		}
+
+		#endregion
+
+		#region Public properties
+
+		/// <summary>
+		/// Output node of the op-amp
+		/// </summary>
+		public int Output { get; }
+
+		/// <summary>
+		/// Non-inverting input of the op-amp (+ terminal)
+		/// </summary>
+		public int NonInvertingInput { get; }
+
+		/// <summary>
+		/// Inverting input of the op-amp (- terminal)
+		/// </summary>
+		public int InvertingInput { get; }
+		
+		#endregion
+	}
+}
---
+++ ECAT.Simulation/AdmittanceMatrix/NodeInfo/TwoTerminalSourceNodeInfo.cs
@@ -0,0 +1,35 @@
+﻿namespace ECAT.Simulation
+{
+	/// <summary>
+	/// Contains information about two terminal sources: node indices for two terminal sources
+	/// </summary>
+	public class TwoTerminalSourceNodeInfo
+	{
+		#region Constructor
+
+		/// <summary>
+		/// Constructor with parameters
+		/// </summary>
+		public TwoTerminalSourceNodeInfo(int positive, int negative)
+		{
+			Positive = positive;
+			Negative = negative;
+		}
+
+		#endregion
+	
+		#region Public properties
+
+		/// <summary>
+		/// Positive terminal of the source (the 'front')
+		/// </summary>
+		public int Positive { get; }
+
+		/// <summary>
+		/// Negative terminal of the source (the 'back')
+		/// </summary>
+		public int Negative { get; }
+
+		#endregion
+	}
+}
