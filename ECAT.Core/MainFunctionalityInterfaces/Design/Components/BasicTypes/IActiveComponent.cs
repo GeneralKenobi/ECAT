2d1ce1d3da9b9ee6556e1fd2d0a3d7149fef5240
Szymon Żogała <szymek.655@gmail.com>
2019-02-22 20:27:14 +0100

    Removed index property, no longer implements ISource. Now its only purpose is to identify active components

--- ECAT.Core/MainFunctionalityInterfaces/Design/Components/BasicTypes/IActiveComponent.cs
+++ ECAT.Core/MainFunctionalityInterfaces/Design/Components/BasicTypes/IActiveComponent.cs
@@ -4,15 +4,8 @@
 	/// Interface for active components that produce current which is calculated during the simulation rather than after simulation using
 	/// component's parameters and voltage drop across the component or taken directly from component's parameters (<see cref="ICurrentSource"/>).
 	/// </summary>
-	public interface IActiveComponent : ISource
+	public interface IActiveComponent
 	{
-		#region Properties
 
-		/// <summary>
-		/// Index used to query <see cref="ISimulationResults"/> for produced current
-		/// </summary>
-		int ActiveComponentIndex { get; set; }
-
-		#endregion
 	}
 }
