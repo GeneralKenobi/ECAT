fe214f4ada8f6a9c7a399ccf57acd58b0c5d555d
Szymon Żogała <szymek.655@gmail.com>
2019-02-24 16:23:18 +0100

    Added, all sources now implement a common interface that has a single property for output value of a source. This view model provides means of binding that edits that value. It also provides a public property with header string for that value.

---
+++ ECAT.ViewModel/ViewModel/DesignViewModules/ComponentEditViewModels/SourceEditViewModel/SourceEditViewModel.cs
@@ -0,0 +1,45 @@
+﻿using ECAT.Core;
+using System;
+
+namespace ECAT.ViewModel
+{
+	/// <summary>
+	/// Edit view model for sources - classes that derive from <see cref="ISource"/>. Provides property to bind to that edits
+	/// <see cref="ISource.OutputValue"/> as well as a header string for the output value.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public abstract class SourceEditViewModel<T> : SpecificComponentEditViewModel<T> where T : ISource
+	{
+		#region Constructor
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		/// <param name="viewModel"></param>
+		/// <param name="outputHeader">Can't be null</param>
+		public SourceEditViewModel(ComponentViewModel viewModel, string outputHeader) : base(viewModel)
+		{
+			OutputHeader = outputHeader ?? throw new ArgumentNullException(nameof(outputHeader));
+		}
+
+		#endregion
+
+		#region Public properties
+
+		/// <summary>
+		/// The header containing information about output quantity
+		/// </summary>
+		public string OutputHeader { get; }
+
+		/// <summary>
+		/// Accessor to the value produced by edited source
+		/// </summary>
+		public double OutputValue
+		{
+			get => _EditedComponent.OutputValue;
+			set => _EditedComponent.OutputValue = value;
+		}
+
+		#endregion
+	}
+}
