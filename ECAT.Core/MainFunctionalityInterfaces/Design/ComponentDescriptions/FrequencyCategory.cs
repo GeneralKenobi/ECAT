447135578ebaa675891f2e6fcc02336222d79db1
Szymon Żogała <szymek.655@gmail.com>
2019-03-01 15:35:32 +0100

    Added

---
+++ ECAT.Core/MainFunctionalityInterfaces/Design/ComponentDescriptions/FrequencyCategory.cs
@@ -0,0 +1,18 @@
+﻿namespace ECAT.Core
+{
+	/// <summary>
+	/// Enum for different categories of frequency description - each category is distinch and, because of that, is usually treated differently 
+	/// </summary>
+	public enum FrequencyCategory
+	{
+		/// <summary>
+		/// Constant - frequency = 0
+		/// </summary>
+		DC = 0,
+
+		/// <summary>
+		/// Variable - frequency > 0
+		/// </summary>
+		AC = 1,
+	}
+}
