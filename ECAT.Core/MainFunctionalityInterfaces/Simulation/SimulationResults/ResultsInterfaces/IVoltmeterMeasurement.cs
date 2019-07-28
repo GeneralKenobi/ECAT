c4acb5f9e407bf7c72d05cd5140dda576aa1eb7f
Szymon Żogała <szymek.655@gmail.com>
2019-07-28 13:27:38 +0200

    Add

---
+++ ECAT.Core/MainFunctionalityInterfaces/Simulation/SimulationResults/ResultsInterfaces/IVoltmeterMeasurement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ECAT.Core
+{
+	/// <summary>
+	/// Interface for a declaration of a voltmeter measurement
+	/// </summary>
+	[NecessaryService]
+	[ConstructorDeclaration(new Type[] { typeof(string), typeof(int), typeof(int) }, "VoltmeterID", "NodeA", "NodeB")]
+	public interface IVoltmeterMeasurement
+	{
+		#region Properties
+
+		/// <summary>
+		/// ID of the voltmeter (used to the user to differentiate between voltmeters)
+		/// </summary>
+		string VoltmeterID { get; }
+
+		/// <summary>
+		/// Reference node of the voltmeter
+		/// </summary>
+		int NodeA { get; }
+
+		/// <summary>
+		/// Non-reference node of the voltmeter
+		/// </summary>
+		int NodeB { get; }
+
+		#endregion
+	}
+}
---
+++ ECAT.Simulation/SimulationResults/VoltmeterMeasurement/VoltmeterMeasurement.cs
@@ -0,0 +1,44 @@
+﻿using ECAT.Core;
+
+namespace ECAT.Simulation
+{
+	/// <summary>
+	/// Standard implementation of <see cref="IVoltmeterMeasurement"/>
+	/// </summary>
+	[RegisterAsType(typeof(IVoltmeterMeasurement))]
+	public class VoltmeterMeasurement : IVoltmeterMeasurement
+	{
+		#region Constructor
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public VoltmeterMeasurement(string voltmeterID, int nodeA, int nodeB)
+		{
+			VoltmeterID = voltmeterID;
+			NodeA = nodeA;
+			NodeB = nodeB;
+		}
+
+		#endregion
+
+		#region Public Properties
+
+		/// <summary>
+		/// ID of the voltmeter (used to the user to differentiate between voltmeters)
+		/// </summary>
+		public string VoltmeterID { get; }
+
+		/// <summary>
+		/// Reference node of the voltmeter
+		/// </summary>
+		public int NodeA { get; }
+
+		/// <summary>
+		/// Non-reference node of the voltmeter
+		/// </summary>
+		public int NodeB { get; }
+
+		#endregion
+	}
+}
